71ba32f5d55c16d9a58a0f0deecec8797dbd7654
Nick Nassar <nassar@pculture.org>
2005-06-14 14:33:34 +0000

    Added yet another namespace for Yahoo Media
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@134 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 86b1f42b9f26fa72227b4469f4273a099f59d5ed

--- tv/portable/feedparser.py
+++ tv/portable/feedparser.py
@@ -278,7 +278,8 @@ class _FeedParserMixin:
                   "http://www.w3.org/1999/xhtml":                         "xhtml",
                   "http://www.w3.org/XML/1998/namespace":                 "xml",
 		  "http://tools.search.yahoo.com/mrss/":                  "media",
-		  "http://docs.yahoo.com/mediaModule":                  "media",
+		  "http://docs.yahoo.com/mediaModule":                    "media",
+                  "http://search.yahoo.com/mrss":                         "media",
 		  "http://participatoryculture.org/RSSModules/dtv/1.0":   "dtv"
 }
 
