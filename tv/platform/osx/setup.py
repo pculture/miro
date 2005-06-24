0ebab0806120565aa3235035bd17ddca00ed5e2f
Nick Nassar <nassar@pculture.org>
2005-06-24 21:57:53 +0000

    Added support for Pyrex extension modules
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@172 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 2e5acea4157d08ea0681c8e932eb4e0d7633896a

--- tv/platform/osx/setup.py
+++ tv/platform/osx/setup.py
@@ -1,14 +1,19 @@
 from distutils.core import setup
+from distutils.extension import Extension
 import py2app
 import os
 import sys
 import shutil
+from Pyrex.Distutils import build_ext
 
 # The name of this platform.
 platform = 'osx'
 
 # Find the top of the source tree and set search path
 root = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), '..', '..')
+
+# GCC3.3 on OS X 10.3.9 doesn't like ".."'s in the path
+root = os.path.normpath(root)
 sys.path[0:0]=['%s/platform/%s' % (root, platform), '%s/platform' % root, '%s/portable' % root]
 
 # Only now may we import things from our own tree
@@ -31,7 +36,13 @@ setup(
 #    ext_modules = [vlchelper.info.getExtension(root)],
     options=dict(
         py2app=py2app_options,
-    )
+    ),
+    ext_modules=[
+        #Add extra_compile_args to change the compile options
+        Extension("database",["%s/portable/database.pyx" % root]),
+        Extension("template",["%s/portable/template.pyx" % root])
+        ],
+    cmdclass = {'build_ext': build_ext}
 )
 
 # NEEDS: Hack: Copy VLC libraries into bundle so they are at the place
