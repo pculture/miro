e194eef68c838709419e782ac53787aeb3490c6c
Luc Heinrich <luc@pculture.org>
2005-07-13 11:24:37 +0000

    Removed hardcoded path to python (/usr/local/bin). Now using the /usr/bin/env command to be more compatible accross different OS installations.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@222 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 4a75cd5be7c1735a4887b1bb428c7a334deb9854

--- tv/platform/osx/launch.py
+++ tv/platform/osx/launch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2.4
 
 import os
 import sys
--- tv/platform/osx/test.sh
+++ tv/platform/osx/test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/bin/python2.4 setup.py py2app --dist-dir . -A && DTV.app/Contents/MacOS/DTV
+/usr/bin/env python2.4 setup.py py2app --dist-dir . -A && open DTV.app
