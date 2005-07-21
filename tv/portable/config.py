76f5b89caae3c6878215569b8f8ecbe03e3c0127
Luc Heinrich <luc@pculture.org>
2005-07-21 17:13:07 +0000

    Normalized indentation spacing.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@256 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 4acdc90bf7fcda47f76efe6f1b137855137ba114

--- tv/portable/config.py
+++ tv/portable/config.py
@@ -10,39 +10,40 @@ configLock = database.globalLock
 def get(key):
     configLock.acquire()
     try:
-	try:
-	    ret = configData[key]
-	except KeyError:
-	    if key == 'DataDirectory':
-		#FIXME add Windows support
-		path = expanduser("~/Movies/DTV")
-		try:
-		    makedirs(join(path,'Incomplete Downloads'))
-		except:
-		    pass
-		configData[key] = path
-		ret = path
-	    elif key == 'FreeSpaceTarget':
-		configData[key] = '2000000000'
-		ret = '2000000000'
-	    elif key == 'DownloadsTarget':
-		configData[key] = 3
-		ret = 3
-	    elif key == 'MaxManualDownloads':
-		configData[key] = 10
-		ret = 10
-	    elif key == 'DefaultTimeUntilExpiration':
-		configData[key] = timedelta(days=7)
-		ret = timedelta(days=7)
-	    else:
-		raise KeyError
+        try:
+            ret = configData[key]
+        except KeyError:
+            if key == 'DataDirectory':
+                #FIXME add Windows support
+                path = expanduser("~/Movies/DTV")
+                try:
+                    makedirs(join(path,'Incomplete Downloads'))
+                except:
+                    pass
+                configData[key] = path
+                ret = path
+            elif key == 'FreeSpaceTarget':
+                configData[key] = '2000000000'
+                ret = '2000000000'
+            elif key == 'DownloadsTarget':
+                configData[key] = 3
+                ret = 3
+            elif key == 'MaxManualDownloads':
+                configData[key] = 10
+                ret = 10
+            elif key == 'DefaultTimeUntilExpiration':
+                configData[key] = timedelta(days=7)
+                ret = timedelta(days=7)
+            else:
+                raise KeyError
     finally:
-	configLock.release()
+        configLock.release()
+        
     return ret
 
 def set(key,value):
     configLock.acquire()
     try:
-	configData[key]=value
+        configData[key]=value
     finally:
-	configLock.release()
+        configLock.release()
