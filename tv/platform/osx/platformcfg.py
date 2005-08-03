9eac94c2710650b0aa941d83a511400684d14910
Luc Heinrich <luc@pculture.org>
2005-08-03 23:31:17 +0000

    Added. This implements OSX specific aspects of preferences storage and folder configurations.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@305 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 5ced3a5ffb8954452e170a9d56b14891aa02214d

---
+++ tv/platform/osx/platformcfg.py
@@ -0,0 +1,45 @@
+from AppKit import NSUserDefaults, NSBundle
+from PyObjCTools import Conversion
+
+import os
+
+import config
+
+def load():
+    domain = NSBundle.mainBundle().bundleIdentifier()
+    plist =  NSUserDefaults.standardUserDefaults().persistentDomainForName_(domain)
+    return Conversion.pythonCollectionFromPropertyList(plist)
+
+def save(data):
+    domain = NSBundle.mainBundle().bundleIdentifier()
+    plist = Conversion.propertyListFromPythonCollection(data)
+    defaults = NSUserDefaults.standardUserDefaults()
+    defaults.setPersistentDomain_forName_(plist, domain)
+    defaults.synchronize()
+
+def get(descriptor):
+    value = None
+
+    if descriptor == config.MOVIES_DIRECTORY:
+        path = os.path.expanduser('~/Movies/DTV')
+        try:
+            os.makedirs(os.path.join(path,'Incomplete Downloads'))
+        except:
+            pass
+        value = path
+
+    elif descriptor == config.SUPPORT_DIRECTORY:
+        path = os.path.expandvars('${APPDATA}')
+        path = os.path.expanduser(path)
+        try:
+            os.makedirs(path)
+        except:
+            pass
+        value = path
+    
+    elif descriptor == config.DB_PATHNAME:
+        path = get(config.SUPPORT_DIRECTORY)
+        path = os.path.join(path, 'tvdump')
+        value = path
+    
+    return value
