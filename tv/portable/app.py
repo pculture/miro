115f0397ce6ac533888bbed087bccd999027a397
Nick Nassar <nassar@pculture.org>
2005-08-08 04:23:47 +0000

    added one more default feed
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@360 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 1c0655524d8a7a9969a2ea47bb49bbe1a78c9c9c

--- tv/portable/app.py
+++ tv/portable/app.py
@@ -124,6 +124,7 @@ class Controller (frontend.Application):
                 feed.UniversalFeed('http://www.rocketboom.com/vlog/quicktime_daily_enclosures.xml')
                 feed.UniversalFeed('http://some-pig.net/videos/rss.php?i=2')
                 feed.UniversalFeed('http://telemusicvision.com/televideos/rss.php?i=3&PHPSESSID=8cfef59e42367759c212d1de5b204f06')
+                feed.UniversalFeed('http://revision3.com/systm/feed/small.h264.mov.torrent.xml')
 
             # Define variables for templates
             # NEEDS: reorganize this, and update templates
