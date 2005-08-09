2e3fe1272c12d0cc6637c925384d02bdf9e21814
Nick Nassar <nassar@pculture.org>
2005-08-09 07:06:45 +0000

    Updated default channels
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@383 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 7bb95269d8eaba9849e9f67ca1a3d7ac334aef27

--- tv/portable/app.py
+++ tv/portable/app.py
@@ -123,8 +123,8 @@ class Controller (frontend.Application):
                 feed.UniversalFeed('http://mediamatters.org/tools/syndication/latest.rss')
                 feed.UniversalFeed('http://www.rocketboom.com/vlog/quicktime_daily_enclosures.xml')
                 feed.UniversalFeed('http://some-pig.net/videos/rss.php?i=2')
-                feed.UniversalFeed('http://telemusicvision.com/televideos/rss.php?i=3&PHPSESSID=8cfef59e42367759c212d1de5b204f06')
-                feed.UniversalFeed('http://revision3.com/systm/feed/small.h264.mov.torrent.xml')
+                feed.UniversalFeed('http://64.207.132.106/tmv/rss.php?i=2')
+                feed.UniversalFeed('http://revision3.com/diggnation/feed/small.mov')
 
             # Define variables for templates
             # NEEDS: reorganize this, and update templates
