0d3adb70b4b92976d92c3e3cc122b13d5504b389
Nick Nassar <nassar@pculture.org>
2005-08-09 07:02:42 +0000

    bug fixes
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@382 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: f17389ba7ad3f1d3993329b247daf35eb4e939b1

--- tv/resources/templates/settings.js
+++ tv/resources/templates/settings.js
@@ -1,13 +1,28 @@
 <script type="text/javascript">
 <!-- // Protect from our XML parser, which doesn't know to protect <script>
 function settingsFormSubmit() {
+     var expire = 'system';
+     var i = 0;
      var url = 'action:changeFeedSettings?';
      url += 'feed='+document.forms['settings']['feed'].value;
-     url += '&automatic='+document.forms['settings']['automatic'].value;
-     url += '&getEverything='+document.forms['settings']['getEverything'].value;
+     if (document.forms['settings'].automatic.checked)
+         url += '&automatic=1';
+     else
+         url += '&automatic=0';
+     for (i=0;i<document.forms['settings'].expire.length;i++)
+         {
+             if (document.forms['settings'].expire[i].checked)
+                 {
+                     expire = document.forms['settings'].expire[i].value;
+                 }
+         }
+     url += '&expire='+expire;
+     if (document.forms['settings'].getEverything.checked)
+         url += '&getEverything=1';
+     else
+         url += '&getEverything=0';
      url += '&maxnew='+document.forms['settings']['maxnew'].value;
      url += '&fallbehind='+document.forms['settings']['fallbehind'].value;
-     url += '&expire='+document.forms['settings']['expire'].value;
      url += '&expireDays='+document.forms['settings']['expireDays'].value;
      url += '&expireHours='+document.forms['settings']['expireHours'].value;
      eventURL(url);
