0f569be48963d169e72ff90f92a831d859d5c0ed
Luc Heinrich <luc@pculture.org>
2005-08-26 23:11:43 +0000

    - checkForUpdates now takes an optional parameter to allow notification when DTV is up to date. - checkForUpdates is now protected with a lock to prevent multiple simultaneous checks. The call does not block if a check is already in progress, it just bails silentlty.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@481 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: f3e8ab78cfbd8883f4d769be682fcaa8e9956da4

--- tv/portable/autoupdate.py
+++ tv/portable/autoupdate.py
@@ -1,25 +1,31 @@
 from downloader import grabURL
-from threading import Thread
+from threading import Thread, Lock
+
+_lock = Lock()
 
 # Pass in a connection to the frontend
 def setDelegate(newDelegate):
     global delegate
     delegate = newDelegate
 
-def checkForUpdates():
-    thread = Thread(target=lambda: _checkForUpdates())
-    thread.setDaemon(False)
-    thread.start()
+def checkForUpdates(notifyIfUpToDate=False):
+    if _lock.acquire(False):
+        thread = Thread(target=lambda: _checkForUpdates(notifyIfUpToDate))
+        thread.setDaemon(False)
+        thread.start()
     
 
-def _checkForUpdates():
+def _checkForUpdates(notifyIfUpToDate):
     info = grabURL('http://www.participatoryculture.org/DTV-version.txt')
-    if not info is None:
+    if info is not None:
         try:
             data = info['file-handle'].read()
             info['file-handle'].close()
             (version, url) = data.split()
             if version != 'beta2005-08-25':
                 delegate.updateAvailable(url)
+            elif notifyIfUpToDate:
+                delegate.dtvIsUpToDate()
         except:
             pass
+    _lock.release()
