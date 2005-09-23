36f74f722487fca962a97ce2fe0b699fbc755e5e
Luc Heinrich <luc@pculture.org>
2005-09-23 15:14:36 +0000

    Added the 'setAutoDownloadableFeed' function which allows to switch the auto-downloadable state individually.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@587 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: e2e0aad289eeffe64c9d0e69bfd9650a733dfc40

--- tv/resources/templates/settings.js
+++ tv/resources/templates/settings.js
@@ -1,31 +1,41 @@
 <script type="text/javascript">
-<!-- // Protect from our XML parser, which doesn't know to protect <script>
-function settingsFormSubmit() {
-     var expire = 'system';
-     var i = 0;
-     var url = 'action:changeFeedSettings?';
-     url += 'feed='+document.forms['settings']['feed'].value;
-     if (document.forms['settings'].automatic.checked)
-         url += '&automatic=1';
-     else
-         url += '&automatic=0';
-     for (i=0;i<document.forms['settings'].expire.length;i++)
-         {
-             if (document.forms['settings'].expire[i].checked)
-                 {
-                     expire = document.forms['settings'].expire[i].value;
-                 }
-         }
-     url += '&expire='+expire;
-     if (document.forms['settings'].getEverything.checked)
-         url += '&getEverything=1';
-     else
-         url += '&getEverything=0';
-     url += '&maxnew='+document.forms['settings']['maxnew'].value;
-     url += '&fallbehind='+document.forms['settings']['fallbehind'].value;
-     url += '&expireDays='+document.forms['settings']['expireDays'].value;
-     url += '&expireHours='+document.forms['settings']['expireHours'].value;
-     eventURL(url);
-}
--->
+    <!-- // Protect from our XML parser, which doesn't know to protect <script>
+    function setAutoDownloadableFeed() {
+        var url = 'action:setAutoDownloadableFeed';
+        url += '?feed=' + document.forms['setAuto']['feed'].value;
+        if (document.forms['setAuto'].automatic.checked)
+            url += '&automatic=1';
+        else
+            url += '&automatic=0';
+        eventURL(url);
+    }
+
+    function settingsFormSubmit() {
+         var expire = 'system';
+         var i = 0;
+         var url = 'action:changeFeedSettings?';
+         url += 'feed='+document.forms['settings']['feed'].value;
+         if (document.forms['settings'].automatic.checked)
+             url += '&automatic=1';
+         else
+             url += '&automatic=0';
+         for (i=0;i<document.forms['settings'].expire.length;i++)
+             {
+                 if (document.forms['settings'].expire[i].checked)
+                     {
+                         expire = document.forms['settings'].expire[i].value;
+                     }
+             }
+         url += '&expire='+expire;
+         if (document.forms['settings'].getEverything.checked)
+             url += '&getEverything=1';
+         else
+             url += '&getEverything=0';
+         url += '&maxnew='+document.forms['settings']['maxnew'].value;
+         url += '&fallbehind='+document.forms['settings']['fallbehind'].value;
+         url += '&expireDays='+document.forms['settings']['expireDays'].value;
+         url += '&expireHours='+document.forms['settings']['expireHours'].value;
+         eventURL(url);
+    }
+    -->
 </script>
