aad1b2c91efce30e740c37755e850a8e62d812d3
Geoff Schmidt <gschmidt@gschmidt.org>
2005-09-28 22:07:01 +0000

    Bam! Building and running DTV on Windows now runs not a test program, but DTV.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@619 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 29c7793021ca991f8aee036ff7c88c2a88e0d9c5

--- tv/platform/windows/DTV.py
+++ tv/platform/windows/DTV.py
@@ -1,8 +1,8 @@
 import resource
 
-#import app
-#app.main()
+#import test.domtest
+#print "Your DTV session has been hijacked for Mozilla testing."
+#test.domtest.main()
 
-import test.domtest
-print "Your DTV session has been hijacked for Mozilla testing."
-test.domtest.main()
+import app
+app.main()
