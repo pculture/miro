50c03cd66a859189719391e388335ea20f344c16
Nick Nassar <nassar@pculture.org>
2005-10-11 00:24:08 +0000

    Test for #605
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@668 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 630bf6db817611b451257831b60cfeb25440030c

--- tv/portable/test/templatetest.py
+++ tv/portable/test/templatetest.py
@@ -73,6 +73,7 @@ class HideTest(unittest.TestCase):
 
 class ViewTest(unittest.TestCase):
     pattern = re.compile("^\n<h1>view test template</h1>\n<span id=\"([^\"]+)\"/>\n", re.S)
+    doublePattern = re.compile("^\n<h1>view test template</h1>\n<span id=\"([^\"]+)\"/>\n<span id=\"([^\"]+)\"/>\n", re.S)
 
     itemPattern = re.compile("^<div id=\"(.*?)\">\n<span>&lt;span&gt;object&lt;/span&gt;</span>\n<span><span>object</span></span>\n<div style=\"display:none\">\nhideIfKey:true\n<span>This is an include</span>\n\n<span>This is a template include</span>\n\n<span>&lt;span&gt;This is a database replace&lt;/span&gt;</span>\n<span><span>This is a database replace</span></span>\n</div>\n<div>\nhideIfNotKey:true\n<span>This is an include</span>\n\n<span>This is a template include</span>\n\n<span>&lt;span&gt;This is a database replace&lt;/span&gt;</span>\n<span><span>This is a database replace</span></span>\n</div>\n<div>\nhideIfKey:false\n<span>This is an include</span>\n\n<span>This is a template include</span>\n\n<span>&lt;span&gt;This is a database replace&lt;/span&gt;</span>\n<span><span>This is a database replace</span></span>\n</div>\n<div style=\"display:none\">\nhideIfNotKey:flase\n<span>This is an include</span>\n\n<span>This is a template include</span>\n\n<span>&lt;span&gt;This is a database replace&lt;/span&gt;</span>\n<span><span>This is a database replace</span></span>\n</div>\n",re.S)
 
@@ -117,6 +118,44 @@ class ViewTest(unittest.TestCase):
         self.assert_(match0)
         self.assert_(match1)
         self.assertNotEqual(match0.group(1),match1.group(1))
+
+    def testTwoViews(self):
+        (text, handle) = fillTemplate("unittest/view-double",{"replace":"<span>This is a database replace</span>","true":True, "false":False, "bool":self.bool, "view":self.everything},self.domHandle)
+        text = HTMLPattern.match(text).group(1)
+        assert(self.doublePattern.match(text)) #span for template inserted
+        id = self.doublePattern.match(text).group(1)
+        id2 = self.doublePattern.match(text).group(2)
+        handle.initialFillIn()
+        self.assertEqual(len(self.domHandle.callList),4)
+        self.assertEqual(self.domHandle.callList[0]['name'],'addItemBefore')
+        self.assertEqual(self.domHandle.callList[1]['name'],'addItemBefore')
+        self.assertEqual(self.domHandle.callList[2]['name'],'addItemBefore')
+        self.assertEqual(self.domHandle.callList[3]['name'],'addItemBefore')
+        self.assertEqual(self.domHandle.callList[0]['id'],id)
+        self.assertEqual(self.domHandle.callList[1]['id'],id)
+        self.assertEqual(self.domHandle.callList[2]['id'],id2)
+        self.assertEqual(self.domHandle.callList[3]['id'],id2)
+        match = []
+        match.append(self.itemPattern.match(self.domHandle.callList[0]['xml']))
+        match.append(self.itemPattern.match(self.domHandle.callList[1]['xml']))
+        match.append(self.itemPattern.match(self.domHandle.callList[2]['xml']))
+        match.append(self.itemPattern.match(self.domHandle.callList[3]['xml']))
+        for x in range(0,4):
+            self.assert_(match[x])
+            for y in range(x+1,4):
+                self.assertNotEqual(match[x].group(1),match[y].group(1))
+        self.x.beginChange()
+        self.x.endChange()
+        self.x.remove()
+        self.assertEqual(len(self.domHandle.callList),8)
+        self.assertEqual(self.domHandle.callList[4]['name'],'changeItem')
+        self.assertEqual(self.domHandle.callList[5]['name'],'changeItem')
+        self.assertEqual(self.domHandle.callList[6]['name'],'removeItem')
+        self.assertEqual(self.domHandle.callList[7]['name'],'removeItem')
+        self.assertEqual(self.domHandle.callList[4]['id'],match[1].group(1))
+        self.assertEqual(self.domHandle.callList[5]['id'],match[3].group(1))
+        self.assertEqual(self.domHandle.callList[6]['id'],match[1].group(1))
+        self.assertEqual(self.domHandle.callList[7]['id'],match[3].group(1))
 
     def testOutOfOrder(self):
         self.domHandle = ChangeDelayedDOMTracker()
