7f34eb82a617308af8f81a421df1a7237d5c3cba
Nick Nassar <nassar@pculture.org>
2005-10-12 00:15:32 +0000

    Bare bones implementation of VideoDisplay using the VLC module
    Depends on API changes in the controller to function. MainFrame.selectDisplay() must be called on this in order for it to actually be displayed.
    
    
    
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@675 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 2bcfee5d8139d4e03c6222671eceabb2c4a7c921

--- tv/platform/windows/frontend_implementation/VideoDisplay.py
+++ tv/platform/windows/frontend_implementation/VideoDisplay.py
@@ -1,5 +1,6 @@
 import app
 import frontend
+import vlc
 
 ###############################################################################
 #### Right-hand pane video display                                         ####
@@ -8,24 +9,55 @@ import frontend
 class VideoDisplay (frontend.NullDisplay, app.VideoDisplayDB):
     "Video player that can be shown in a MainFrame's right-hand pane."
 
-    def __init__(self, firstItemId, view, previousDisplay):
-        app.VideoDisplayDB.__init__(self, firstItemId, view)
+    def __init__(self):
+        app.VideoDisplayDB.__init__(self)
         frontend.NullDisplay.__init__(self)
+        self.vlc = vlc.SimpleVLC()
+        self.vlc.setWindow(self.getHwnd())
+
+    # FIXME: This strange API was inherited from OS X
+    @classmethod
+    def getInstance(self):
+        return VideoDisplay()
+
+    def configure(self, view, firstItemId, previousDisplay):
+        self.setPlaylist(view, firstItemId)
         self.previousDisplay = previousDisplay
 
+
+    def onWMClose(self, hwnd, msg, wparam, lparam):
+	self.unlink()
+	win32gui.PostQuitMessage(0)
+
+    def onWMSize(self, hwnd, msg, wparam, lparam):
+        pass
+
+    def onWMActivate(self, hwnd, msg, wparam, lparam):
+        pass
+
+    def playPause(self):
+        filename = self.cur().getPath()
+        if filename is None:
+            filename= self.getNext().getPath()
+        self.vlc.play(filename)
+
+    def stop(self):
+        self.vlc.stop()
+    
     def onSelected(self, frame):
         # Enable controls
         pass
 
     def onDeselected(self, frame):
         # Disable controls
-        pass
+        self.vlc.stop()
 
     # NEEDS: See OS X for details on how to use VideoDisplayDB to find
     # items to play.
 
 #    def getHwnd(self):
 #        in parent for now
+
     def unlink(self):
         frontend.NullDisplay.unlink(self)
 
