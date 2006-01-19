da4867eb3abb79b05637b33b155e0564e9513202
Nick Nassar <nassar@pculture.org>
2006-01-19 07:56:38 +0000

    Added context click support to XUL frontend
    Still needs to send eventURL to the app somehow...
    
    
    
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@970 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: a455d150bfb60f80f91ccde310566e072185ab9a

--- tv/platform/windows-xul/xul/chrome/content/main.js
+++ tv/platform/windows-xul/xul/chrome/content/main.js
@@ -29,3 +29,59 @@ function addChannel(url) {
     py.QueryInterface(Components.interfaces.pcfIDTVPyBridge);
     py.addChannel(url);
 }
+
+// FIXME: Duplicated from dynamic.js
+function getContextClickMenu(element) {
+    while (1) {
+	if (element.nodeType == 1 && element.getAttribute('t:contextMenu')) {
+	    var ret = element.getAttribute('t:contextMenu');
+	    ret = ret.replace(/\\n/g,"\n");
+	    ret = ret.replace(/\\\\/g,"\\");
+	    return ret;
+	}
+	if (element.parentNode)
+	    element = element.parentNode;
+	else
+	    return "";
+    }
+
+    // Satisfy Mozilla that the function always returns a
+    // value. Otherwise, we get an error if strict mode is enabled,
+    // ultimately preventing us from getting the state change event
+    // indicating that the load succeeded.
+    return "";
+}
+
+//FIXME: Duplicated from dynamic.js
+function eventURL(url) {
+  jsdump('FIXME: eventURL called from XUL land: '+url);
+}
+
+function xulclickhandler(event) {
+  var itemsAdded = 0;
+  var menu = getContextClickMenu(event.target);
+  var popup = document.getElementById('contextPopup');
+  var kids = popup.childNodes;
+  for (var i = 0; i < kids.length; i++) {
+    popup.removeChild(kids[i]);
+  }
+ 
+  menu = menu.split("\n");
+  while (menu.length > 0) {
+    var line = menu.shift().split('|');
+    if (line.length > 1) {
+      var newItem = document.createElement('menuitem');
+      newItem.setAttribute('label',line[1]);
+      newItem.setAttribute('oncommand','eventURL("'+line[0]+'");');
+      popup.appendChild(newItem);
+      itemsAdded++;
+    } else {
+      var newItem = document.createElement('menuseparator');
+      popup.appendChild(newItem);
+    }
+  }
+  return (itemsAdded > 0); // Return false if there are no items in the menu
+                           // This should hide empty menus, but
+                           // apparently doesn't...
+
+}
