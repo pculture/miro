d1c8cd80428efd78d13e9c004b46d970b6bcdb4e
Geoff Schmidt <gschmidt@gschmidt.org>
2006-01-23 00:07:37 +0000

    Remove security exception for files on local disk -- it's no longer necessary now that we use the in-process HTTP server, since we conform to the normal Mozilla 'same origin' security rules.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@979 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: f8c8142bffddcfd63aff99d4a2dc3ea88dcca076

--- tv/platform/windows-xul/xul/defaults/preferences/prefs.js
+++ tv/platform/windows-xul/xul/defaults/preferences/prefs.js
@@ -1,18 +1,6 @@
 pref("toolkit.defaultChromeURI", "chrome://dtv/content/main.xul");
 pref("toolkit.singletonWindowType", "main");
 
-/* Allow files on local disk (such as those we autogenerate) to
-   acquire the UniversalBrowserRead privilege, which is what's
-   necessary to do XMLHttpRequests to arbitrary URLs (such as  our
-   dispatch: handler.)
-
-   NEEDS: careful security audit. Is there a way we can restrict this
-   grant to just the HTML our template code generates?
-*/
-pref("capability.principal.codebase.dtv.granted", "UniversalBrowserRead");
-pref("capability.principal.codebase.dtv.id", "file://");
-pref("capability.principal.codebase.dtv.subjectName", "");
-
 /* debugging prefs */
 pref("browser.dom.window.dump.enabled", true);
 pref("javascript.options.showInConsole", true);
