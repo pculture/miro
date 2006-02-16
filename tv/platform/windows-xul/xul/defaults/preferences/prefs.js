4ff61d2aa25bf2e4d39b6e7da5af7a985f8a196b
Geoff Schmidt <gschmidt@gschmidt.org>
2006-02-16 05:50:13 +0000

    Don't warn user when entering and leaving secure areas (such as the channel guide.) Set Mozilla's user-agent string to include a clause about DTV.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1058 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 27226ec092f8cf4bd7852890a8a194f814e5fe67

--- tv/platform/windows-xul/xul/defaults/preferences/prefs.js
+++ tv/platform/windows-xul/xul/defaults/preferences/prefs.js
@@ -2,6 +2,18 @@ pref("toolkit.defaultChromeURI", "chrome://dtv/content/main.xul");
 pref("toolkit.singletonWindowType", "main");
 pref("extensions.pyxpcom.embedded", true);
 
+pref("security.warn_entering_secure", false);
+pref("security.warn_entering_weak", false);
+pref("security.warn_leaving_secure", false);
+pref("security.warn_submit_insecure", false);
+pref("security.warn_viewing_mixed", false);
+
+/* NEEDS: it'd be nice to have an automated way to get the DTV version
+   number into this */
+pref("general.useragent.vendor", "DTV");
+pref("general.useragent.vendorSub", "0.8.0");
+pref("general.useragent.vendorComment", "http://participatoryculture.org");
+
 /* debugging prefs */
 pref("browser.dom.window.dump.enabled", true);
 pref("javascript.options.showInConsole", true);
