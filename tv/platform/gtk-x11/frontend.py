c84fcd15ad9bd75cef429ff452e1539ed79c0e4f
Ben Dean-Kawamura <ben@pculture.org>
2006-03-29 23:58:13 +0000

    Fixed stupid import of a non-module.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1282 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 8a21346ad73e9180cbfdaba0fb21831c6b5293e3

--- tv/platform/gtk-x11/frontend.py
+++ tv/platform/gtk-x11/frontend.py
@@ -14,7 +14,6 @@ import MozillaBrowser
 
 # Almost everything is split out into files under frontend-implementation.
 from frontend_implementation.Application import Application
-import frontend_implementation.MozillaBrowser
 from frontend_implementation.MainFrame import MainFrame, NullDisplay
 from frontend_implementation.UIBackendDelegate import UIBackendDelegate
 from frontend_implementation.HTMLDisplay import HTMLDisplay, getDTVAPICookie, getDTVAPIURL
