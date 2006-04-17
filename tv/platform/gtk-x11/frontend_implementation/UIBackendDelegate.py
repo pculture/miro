601660c8ec99e2bb2013b91d859fce71a310d46e
Chris Lahey <clahey@pculture.org>
2006-04-17 18:51:07 +0000

    Remove unnecessary debugging code.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1368 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 0bc4512a10969c107742070fa27840faa9fcf175

--- tv/platform/gtk-x11/frontend_implementation/UIBackendDelegate.py
+++ tv/platform/gtk-x11/frontend_implementation/UIBackendDelegate.py
@@ -121,7 +121,6 @@ class UIBackendDelegate:
         message = "But we'll try our best to grab the files.\n- It may take time to list the videos\n- Descriptions may look funny\n\nPlease contact the publishers of %s and ask if they have a DTV-style channel." % url
         buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, "Continue", gtk.RESPONSE_OK)
         response = ShowDialog (summary, message, buttons)
-        print response == gtk.RESPONSE_OK
         if (response == gtk.RESPONSE_OK):
             return True
         else:
