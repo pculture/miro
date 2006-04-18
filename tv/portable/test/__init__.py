49620e993a3709b027562b5e6e555e529c8abffa
Ben Dean-Kawamura <ben@pculture.org>
2006-04-18 16:39:54 +0000

    Unittest fixes.  runtests.sh now runs all the tests.  runtests.sh <modulename> runs only the tests in that module.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1391 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: e4e5ba17adffa6c907fe7023f6537c705914ba23

--- tv/portable/test/__init__.py
+++ tv/portable/test/__init__.py
@@ -5,13 +5,8 @@
 
 import unittest
 
-testModules = [
-    __import__('test.databasetest'),
-    __import__('test.templatetest'),
-    __import__('test.fasttypestest'),
-    __import__('test.schematest'),
-    __import__('test.storedatabasetest'),
-]
-
-for module in testModules:
-    unittest.defaultTestLoader.loadTestsFromModule(module)
+from test.databasetest import *
+from test.templatetest import *
+from test.fasttypestest import *
+from test.schematest import *
+from test.storedatabasetest import *
