a6556b31a93e5604a82c838ac3c4c1a25e85bdaf
Ben Dean-Kawamura <ben@pculture.org>
2006-05-09 16:51:13 +0000

    Reordered how we do things in Democracy_Downloader.  Now we send the ready command before importing download, since the module initialization code sends a GetConfigCommand over the wire.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1574 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 53a9e2ea32e5153bb015b4970325d92d540501c1

--- tv/portable/dl_daemon/Democracy_Downloader.py
+++ tv/portable/dl_daemon/Democracy_Downloader.py
@@ -26,8 +26,10 @@ from dl_daemon import daemon
 
 port = int(os.environ['DEMOCRACY_DOWNLOADER_PORT'])
 server = daemon.DownloaderDaemon(port)
-
-from dl_daemon import download, command
+from dl_daemon import command
+c = command.ReadyCommand(server)
+from dl_daemon import download
 download.startBTDownloader()
 c = command.ReadyCommand(server)
 c.send(block = False, retry = True)
+
