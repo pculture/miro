7399053c270ffc82c3ac5f82e4a51b62661ffaef
Chris Lahey <clahey@pculture.org>
2006-05-22 15:42:34 +0000

    Maybe I shouldn't hardcode my movies directory.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1695 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 3c0281d6623e804efec13f643a2e573fe9d31f91

--- tv/platform/gtk-x11/platformutils.py
+++ tv/platform/gtk-x11/platformutils.py
@@ -3,13 +3,15 @@
 #### movies are stored                                                     ####
 ###############################################################################
 import os
+import config
+import prefs
 
 def getAvailableGBytesForMovies():
     # TODO: windows implementation
-    stream = os.popen ('stat -f /home/clahey/Movies2/Democracy/ -c "%a"')
+    stream = os.popen ('stat -f %s -c "%%a"' % (config.get(prefs.MOVIES_DIRECTORY),))
     free_blocks = int(stream.read())
     stream.close()
-    stream = os.popen ('stat -f /home/clahey/Movies2/Democracy/ -c "%S"')
+    stream = os.popen ('stat -f %s -c "%%S"' % (config.get(prefs.MOVIES_DIRECTORY),))
     block_size = int(stream.read())
     stream.close()
     return free_blocks * block_size / float(1024 * 1024 * 1024)
