235c1fbfbb206a5d116bb66648e57552bf8f249e
Chris Lahey <clahey@pculture.org>
2006-05-22 16:06:11 +0000

    Or we could just call statvfs.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1698 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: a72c5a6d0cdfd3f8605cc5e2e867ddab00f82cfd

--- tv/platform/gtk-x11/platformutils.py
+++ tv/platform/gtk-x11/platformutils.py
@@ -7,11 +7,5 @@ import config
 import prefs
 
 def getAvailableGBytesForMovies():
-    # TODO: windows implementation
-    stream = os.popen ('stat -f %s -c "%%a"' % (config.get(prefs.MOVIES_DIRECTORY),))
-    free_blocks = int(stream.read())
-    stream.close()
-    stream = os.popen ('stat -f %s -c "%%S"' % (config.get(prefs.MOVIES_DIRECTORY),))
-    block_size = int(stream.read())
-    stream.close()
-    return free_blocks * block_size / float(1024 * 1024 * 1024)
+    statinfo = os.statvfs (config.get(prefs.MOVIES_DIRECTORY))
+    return statinfo.f_frsize * statinfo.f_bavail / float(1024 * 1024 * 1024)
