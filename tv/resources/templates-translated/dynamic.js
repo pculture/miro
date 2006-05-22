861e33190ecc6eb57d209f9efeef0e95f22e5115
Ben Dean-Kawamura <ben@pculture.org>
2006-05-22 22:34:47 +0000

    Reverted the templates back before the translation stuff, since they don't compile with it.  Copied the translated ones to the directory templates-translated.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1707 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: ba957f43308100dbfbccef6f5d5f0802a3160b60

---
+++ tv/resources/templates-translated/dynamic.js
@@ -0,0 +1,276 @@
+<script type="text/javascript">
+<!-- // Protect from our XML parser, which doesn't know to protect <script>
+
+///////////////////////////////////////////////////////////////////////////////
+//// Machinery related to dynamic updates in full XUL mode                 ////
+///////////////////////////////////////////////////////////////////////////////
+
+function handleUpdate(event) {
+    r = event.target;
+    eval(r.responseText);
+}
+
+function beginUpdates() {
+    if (getDTVPlatform() == 'xul') {
+        // Under XUL, open a 'push' HTTP connection to the controller to
+        // receive updates. This avoids calling across the Python/XPCOM
+        // boundary, which causes deadlocks sometimes for poorly understood
+        // reasons.
+        //        port = getServerPort();
+        var cookie = getEventCookie();
+        //        url = "http://127.0.0.1:" + port + "/dtv/mutators/" + cookie;
+        var url = "/dtv/mutators/" + cookie;
+
+        var xr = new XMLHttpRequest();
+        /*
+        netscape.security.PrivilegeManager.
+            enablePrivilege("UniversalBrowserRead");
+        */
+        xr.multipart = true;
+        xr.open("GET", url, true);
+        xr.onload = handleUpdate;
+        xr.send(null);
+    }
+}
+     
+///////////////////////////////////////////////////////////////////////////////
+//// For use on your page                                                  ////
+///////////////////////////////////////////////////////////////////////////////
+
+function jsdump(str) {
+    Components.classes['@mozilla.org/consoleservice;1']
+	.getService(Components.interfaces.nsIConsoleService)	
+	.logStringMessage(str);
+}
+
+function getEventCookie() {
+    var elt = document.getElementsByTagName("html")[0];
+    return elt.getAttribute('eventCookie');
+}
+
+function getDTVPlatform() {
+    var elt = document.getElementsByTagName("html")[0];
+    return elt.getAttribute('dtvPlatform');
+}
+
+/*
+// NEEDS: eliminate! just use relative URLs
+function getServerPort() {
+    var elt = document.getElementsByTagName("html")[0];
+    return elt.getAttribute('serverPort');
+}
+*/
+
+// For calling from page Javascript: Cause a URL to be loaded. The
+// assumption is that the application will notice, abort the load, and
+// take some action based on the URL.
+function eventURL(url) {
+    if (getDTVPlatform() == 'xul') {
+    	// XUL strategy: async HTTP request to our in-process HTTP
+        // server.  Since it falls under the "same origin" security
+        // model exemption, no need for complicated preferences
+        // shenanigans - what a nice day!
+        //        url = "http://127.0.0.1:" + getServerPort() + "/dtv/action/" +
+        //            getEventCookie() + "?" + url; NEEDS: remove
+        url = "/dtv/action/" + getEventCookie() + "?" + url;
+    	var req = new XMLHttpRequest();
+        req.open("GET", url, true);
+        req.send(null);
+        // NEEDS: there is another copy of this in main.js.
+    }
+    else if (typeof(window.frontend) == 'undefined') {
+	// Generic strategy: trigger a load, and hope the application
+	// catches it and cancels it without creating a race
+	// condition.
+	document.location.href = url;
+    } else {
+	// OS X WebKit (KHTML) strategy: pass in an Objective C object
+	// through the window object and call a method on it.
+	window.frontend.eventURL(url);
+    }
+
+    return false;
+}    
+
+// Open email client with email about selected video
+// All parameters come in URL encoded
+function recommendItem(title, url, feedURL) {
+    url = URLdecode(url);
+    feedURL = URLdecode(feedURL);
+    body = "You should check out this video:\n";
+    body = body + url + "\n\n";
+    body = body + "I found it while using the Democracy Player, which you can download here:\n";
+    body = body + "http://www.getdemocracy.com\n\n";
+    body = body + "The video was on this channel (click 'subscribe' in Democracy and paste in this address):\n"
+    body = body + feedURL + "\n\n"
+    
+    url = 'mailto:';
+    url = url + "?subject=" + title;
+    url = url + "&body=" + URLencode(body);
+    eventURL(url);
+    
+    return false;
+}
+
+// Start the video player. The playlist will be the items in the view
+// named by viewName. If firstItemId is the id of an item in the view,
+// playback will start on that item; otherwise playback will start on
+// the first item.
+function playViewNamed(viewName, firstItemId) {
+    url = 'action:playViewNamed?';
+    url = url + 'viewName=' + URLencode(viewName);
+    url = url + '&firstItemId=' + URLencode(firstItemId);
+    eventURL(url);
+    return false;
+}
+
+// You can make 'incremental search' text boxes on your page that
+// effectively tie the text box to the 'parameter' argument of setViewFilter,
+// with the other argumens fixed. To do this, add these two attributes to
+// the text box:
+//   onfocus="startEditSearch(this)"
+//   onblur="endEditFilter()"
+// replacing the arguments in parentheses with the desired strings.
+//
+// You'll also need to provide a updateSearchString function at the
+// top of your template to perform the actual update
+
+var editSearchField = null;
+var editSearchOldValue = '';
+var editSearchTimer = null;
+
+function startEditSearch(obj) {
+  editSearchOldValue = obj.value;
+
+  editSearchField = obj;
+  editSearchTimerTick();
+}
+
+function editSearchUpdate() {
+    value = editSearchField.value;
+    if (editSearchOldValue != value) {
+	url = 'action:setSearchString?searchString=' + URLencode(value);
+	eventURL(url);
+	editSearchOldValue = value;
+    }
+}
+
+function editSearchTimerTick() {
+    editSearchUpdate();
+    editSearchTimer = setTimeout(editSearchTimerTick, 50);
+}
+
+function endEditSearch() {
+  clearTimeout(editSearchTimer);
+  editSearchUpdate();
+}
+
+// Internal use: 'URL encode' the given string.
+function URLencode(str) {
+    return encodeURIComponent(str)
+}
+
+function URLdecode(str) {
+  return decodeURIComponent(str)
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//// For calling by host templating code                                   ////
+///////////////////////////////////////////////////////////////////////////////
+
+// For calling by host templating code: Returns items that should
+// appear in the context click menu in the format url|description with
+// one item on each line. Blank lines are separators
+function getContextClickMenu(element) {
+    while (1) {
+	if (element.nodeType == 1 && element.getAttribute('t:contextMenu')) {
+	    ret = element.getAttribute('t:contextMenu');
+	    ret = ret.replace(/\\n/g,"\n");
+	    ret = ret.replace(/\\\\/g,"\\");
+	    return ret;
+	}
+	if (element.parentNode)
+	    element = element.parentNode;
+	else
+	    return "";
+    }
+
+    // Satisfy Mozilla that the function always returns a
+    // value. Otherwise, we get an error if strict mode is enabled,
+    // ultimately preventing us from getting the state change event
+    // indicating that the load succeeded.
+    return "";
+}
+
+// For calling by host templating code: Set CSS styles on the item
+// with the given ID to make it disappear.
+function hideItem(id) {
+    elt = document.getElementById(id);
+    elt.style.display = 'none';
+    forceRedisplay(elt);
+}
+
+// For calling by host templating code: Set CSS styles on the item
+// with the given ID to make it visible if it was previously hidden.
+function showItem(id) {
+    elt = document.getElementById(id);
+    elt.style.display = '';
+    forceRedisplay(elt);
+}
+
+// For calling by host templating code: Replace the item with the
+// given id with the element described by the proided XML.
+function changeItem(id, newXML) {
+    elt = document.getElementById(id);
+    r = document.createRange();
+    r.selectNode(elt);
+    frag = r.createContextualFragment(newXML);
+    elt.parentNode.replaceChild(frag, elt);
+}
+
+// For calling by host templating code: Parse the XML in newXML into a
+// new element, and insert the new element immediately before the item
+// with the given id, such that the newly inserted item has the same
+// parent.
+function addItemBefore(newXML, id) {
+    elt = document.getElementById(id);
+    r = document.createRange();
+    r.selectNode(elt);
+    frag = r.createContextualFragment(newXML);
+    elt.parentNode.insertBefore(frag, elt);
+}    
+
+// For calling by host templating code: Parse the XML in newXML into a
+// new element, and insert the new element as the final child of the
+// item with the given id.
+function addItemAtEnd(newXML, id) {
+    elt = document.getElementById(id);
+    r = document.createRange();
+    r.selectNode(elt);
+    frag = r.createContextualFragment(newXML);
+    elt.insertBefore(frag, null);
+}    
+
+// For calling by host templating code: Remove the item with the given
+// id.
+function removeItem(id) {
+    elt = document.getElementById(id);
+    elt.parentNode.removeChild(elt);
+}    
+
+// Internal use: Sometime if all you do is change the style on a node,
+// Safari doesn't update the view until your mouse is next over the
+// window. Force the issue by making a drastic change in the vicinity
+// of the given element and then reversing it.
+function forceRedisplay(elt) {
+    r = document.createRange();
+    r.selectNode(elt);
+    frag = r.extractContents();
+    r.insertNode(frag);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+-->
+</script>
