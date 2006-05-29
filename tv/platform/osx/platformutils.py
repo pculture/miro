9fdb59ab1041320ba91059c668ebf8cf0d17f54b
Luc Heinrich <luc@pculture.org>
2006-05-29 08:41:24 +0000

    Added threading utilities used to schedule function/method calls on the main GUI thread in various ways.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1770 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: b0a35ec7c3a23cf724f2bc3cc5bef18aa033d932

--- tv/platform/osx/platformutils.py
+++ tv/platform/osx/platformutils.py
@@ -1,5 +1,126 @@
-from Foundation import NSFileManager, NSAutoreleasePool, NSFileSystemFreeSize
+import threading
+import Foundation
+
 import config
+
+###############################################################################
+#### THREADING UTILITIES                                                   ####
+###############################################################################
+
+def warnIfNotOnMainThread(name="(unknown)"):
+    """Displays a warning message if a function is not called on the main GUI 
+    thread.
+    """
+    if threading.currentThread().getName() != 'MainThread':
+        print "WARNING: function %s not on main thread" % name
+
+###############################################################################
+
+def callOnMainThread(func, *args, **kwargs):
+    """Schedules func to be called on the main thread and returns immediately.
+    """
+    _call((func, args, kwargs), waitUntilDone=False)
+
+def callOnMainThreadAndWaitUntilDone(func, *args, **kwargs):
+    """Schedules func to be called on the main thread and wait until the calls 
+    has been effectively performed.
+    """
+    _call((func, args, kwargs), waitUntilDone=True)
+
+def callOnMainThreadAfterDelay(delay, func, *args, **kwargs):
+    """Schedules func to be called on the main thread after the specified delay 
+    (in seconds)
+    """
+    _call((func, args, kwargs), delay=delay)
+
+def callOnMainThreadAndWaitReturnValue(func, *args, **kwargs):
+    """Schedules func to be called on the main thread and wait for its result.
+    """
+    return _call((func, args, kwargs), waitForResult=True)
+
+###############################################################################
+
+def onMainThread(method):
+    """Decorator which specifies that a method should always be called on the 
+    main GUI thread.
+    """
+    def scheduled(self, *args, **kwargs):
+        callOnMainThread(method, self, *args, **kwargs)
+    return scheduled
+    
+def onMainThreadWithReturn(method):
+    """Decorator which specifies that a method should always be called on the 
+    main GUI thread and that the called must wait for its result.
+    """
+    def scheduled(self, *args, **kwargs):
+        return callOnMainThreadAndWaitReturnValue(method, self, *args, **kwargs)
+    return scheduled
+
+###############################################################################
+#### THREADING UTILITIES SUPPORT                                           ####
+###############################################################################
+
+callLock = threading.Lock()
+callEvent = threading.Event()
+callResult = None
+
+def _call(args, delay=0.0, waitUntilDone=False, waitForResult=False):
+    obj = CallerObject.alloc().initWithArgs_(args)
+    try:
+        if waitForResult:
+            return obj.performCallAndWaitReturn()
+        elif delay == 0.0:
+            obj.performCall(waitUntilDone)
+        else:
+            obj.performCallLater(delay)
+    finally:
+        del obj
+
+class CallerObject (Foundation.NSObject):
+    
+    def initWithArgs_(self, args):
+        self = self.init()
+        self.args = args
+        return self
+        
+    def performCall(self, waitUntilDone):
+        self.performSelectorOnMainThread_withObject_waitUntilDone_(self.perform_, self.args, waitUntilDone)
+
+    def performCallLater(self, delay):
+        self.performSelector_withObject_afterDelay_(self.performCall, None, delay)
+        
+    def performCallAndWaitReturn(self):
+        global callLock
+        global callEvent
+        global callResult
+        
+        callLock.acquire()
+        r = None
+        try:
+            callEvent.clear()
+            self.performSelectorOnMainThread_withObject_waitUntilDone_(self.performAndNotify_, self.args, False)
+            callEvent.wait()
+            r = callResult
+        finally:
+            callResult = None
+            callLock.release()
+        return r
+        
+    def perform_(self, (func, args, kwargs)):
+        pool = Foundation.NSAutoreleasePool.alloc().init()
+        try:
+            return func(*args, **kwargs)
+        finally:
+            del pool
+
+    def performAndNotify_(self, (func, args, kwargs)):
+        global callResult
+        pool = Foundation.NSAutoreleasePool.alloc().init()
+        try:
+            callResult = func(*args, **kwargs)
+        finally:
+            del pool
+            callEvent.set()
 
 ###############################################################################
 #### Helper method used to get the free space on the disk where downloaded ####
@@ -7,9 +128,9 @@ import config
 ###############################################################################
 
 def getAvailableGBytesForMovies():
-    pool = NSAutoreleasePool.alloc().init()
-    fm = NSFileManager.defaultManager()
+    pool = Foundation.NSAutoreleasePool.alloc().init()
+    fm = Foundation.NSFileManager.defaultManager()
     info = fm.fileSystemAttributesAtPath_(config.get(config.MOVIES_DIRECTORY))
-    bytesFree = info[NSFileSystemFreeSize]
+    bytesFree = info[Foundation.NSFileSystemFreeSize]
     del pool
-    return bytesFree / (1024 * 1024 * 1024)
+    return bytesFree / float(1024 * 1024 * 1024)
