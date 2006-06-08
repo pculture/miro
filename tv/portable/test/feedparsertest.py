bd86cb812ce6e2a67565323325f713f30c6d3bd8
Chris Lahey <clahey@pculture.org>
2006-06-08 05:18:54 +0000

    Added more tests.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1928 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: dfda3b6d11bbc21c235b444df3b58bb3c5192f2d

--- tv/portable/test/feedparsertest.py
+++ tv/portable/test/feedparsertest.py
@@ -11,12 +11,21 @@ class FeedURLValidationTest(DemocracyTestCase):
         b["url"] = "hello"
         c = feedparser.FeedParserDict ()
         c["href"] = "hi"
+        d = feedparser.FeedParserDict ()
+        d["href"] = "hi"
+        d["what"] = "hello"
         self.assertEqual(a.equal(a), True)
         self.assertEqual(b.equal(b), True)
         self.assertEqual(c.equal(c), True)
+        self.assertEqual(d.equal(d), True)
         self.assertEqual(a.equal(b), True)
+        self.assertEqual(b.equal(a), True)
         self.assertEqual(a.equal(c), False)
+        self.assertEqual(c.equal(a), False)
         self.assertEqual(b.equal(c), False)
-            
+        self.assertEqual(c.equal(b), False)
+        self.assertEqual(a.equal(d), False)
+        self.assertEqual(d.equal(a), False)
+
 if __name__ == "__main__":
     unittest.main()
