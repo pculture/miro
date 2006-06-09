93e0be392d2d6ca7157993dbdcd9ff2cb8636ce0
Ben Dean-Kawamura <ben@pculture.org>
2006-06-09 17:05:10 +0000

    Removed HTTP connection pool limits for the downloader.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@1958 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 179d361a327b8b755fe7154de17c369a48e2a2bf

--- tv/portable/dl_daemon/Democracy_Downloader.py
+++ tv/portable/dl_daemon/Democracy_Downloader.py
@@ -12,6 +12,7 @@ from dl_daemon import daemon
 # This isn't used here, we just want to load it sooner.
 from dl_daemon import download
 import eventloop
+import httpclient
 
 logPath = os.environ.get('DEMOCRACY_DOWNLOADER_LOG')
 if logPath is not None:
@@ -31,5 +32,10 @@ if os.environ.get('DEMOCRACY_DOWNLOADER_FIRST_LAUNCH') != '1':
 
 port = int(os.environ['DEMOCRACY_DOWNLOADER_PORT'])
 server = daemon.DownloaderDaemon(port)
+
+# remove the limits for the connection pool, we limit them ourselves in the
+# downloader code.
+httpclient.HTTPConnectionPool.MAX_CONNECTIONS_PER_SERVER = sys.maxint
+httpclient.HTTPConnectionPool.MAX_CONNECTIONS = sys.maxint
 
 eventloop.startup()
