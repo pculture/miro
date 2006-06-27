009c8566b0c127ffc228706b91da99ab57fd7154
Chris Lahey <clahey@pculture.org>
2006-06-27 17:57:24 +0000

    Moved files to subscribe subdirectory.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2181 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 40e4492ee21d4682a32500e3e28c6e530718410b

--- web/opml.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //header('Content-type: text/xml');
-  header('Content-type: application/x-democracy');
-  header('Content-Disposition: inline; filename="subscribe.democracy"');
-  include "opml-string.php";
-  echo getOPML();
-?>
