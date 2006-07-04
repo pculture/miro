6e643b622e3d3da44481cd7440f522a6296d3946
Ben Dean-Kawamura <ben@pculture.org>
2006-07-04 20:07:38 +0000

    Made absolutePathToURL work more like before.  urllib.pathname2url seems totally busted to me.  I get very different results on windows and linux.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2242 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 9c24bb1e34c4a5025d6263a3759a1c061dbab694

--- tv/portable/util.py
+++ tv/portable/util.py
@@ -108,8 +108,16 @@ def queryRevision(file):
 # that would refer to the same file.
 def absolutePathToFileURL(path):
     if isinstance(path, unicode):
-        path = path.encode('utf-8')
-    return 'file://%s' % urllib.pathname2url(path)
+        path = path.encode("utf-8")
+    parts = string.split(path, os.sep)
+    parts = [urllib.quote(x, ':') for x in parts]
+    if len(parts) > 0 and parts[0] == '':
+        # Don't let "/foo/bar" become "file:////foo/bar", but leave
+        # "c:/foo/bar" becoming "file://c:/foo/bar" -- technically :
+        # should become | (but only in a drive name?) but most
+        # consumers will let us get by with that.
+        parts = parts[1:]
+    return "file:///" + '/'.join(parts)
 
 
 # Shortcut for 'failed' with the exception flag.
