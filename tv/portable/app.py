5738467fd371d07e341760a1df85b0b30caa9c99
Ben Dean-Kawamura <ben@pculture.org>
2006-07-15 21:47:25 +0000

    Fixed our Host: header to work with non-standard ports.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/branches/Democracy-Player-0.8.5@2362 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 6964bd7a115b88db7af826e697eed8f6f68e21ff

--- tv/portable/app.py
+++ tv/portable/app.py
@@ -385,7 +385,6 @@ class VideoRenderer:
     def selectItem(self, anItem):
         url = 'file://%s' % urllib.pathname2url(anItem.getPath())
         if self.canPlayUrl (url):
-            print "DTV: playing %s" % anItem.getPath()
             self.selectUrl (url)
         elif os.path.isdir(anItem.getPath()):
           for filename in os.listdir(anItem.getPath()):
