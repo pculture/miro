a0a3a79707c3bb0d7ca87c346589643b6c6dd43c
Chris Lahey <clahey@pculture.org>
2006-07-25 21:06:30 +0000

    Make build catalogs build the windows xul files.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2474 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: bcaff6450ff85700eb08b6c0cc637370be54d8a7

--- tv/resources/locale/build-catalogs.py
+++ tv/resources/locale/build-catalogs.py
@@ -6,3 +6,7 @@ for pofile in glob.glob ("*.po"):
     lang = pofile[:-3]
     mofile = os.path.join ("%s.mo" % lang)
     os.system ("msgfmt %s -o %s" % (pofile, mofile))
+
+for dtd in glob.glob ("../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd") + glob.glob ("../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd.template"):
+    dtd = os.path.basename (dtd)
+    os.system ("(cd ../../platform/windows-xul/xul/chrome/locale/; intltool-merge --quoted-style -m ../../../../../resources/locale/ en-US/%s %s)" % (dtd, dtd))
