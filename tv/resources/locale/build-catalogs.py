c2b74b99fcddc6d91db7f24d88e0ea76f2d95794
Chris Lahey <clahey@pculture.org>
2006-08-02 14:55:56 +0000

    New build-messages and build-catalogs code to handle OSX.  You also need to run extract-strings before build-messages and inject-strings after build-catalogs, but those need to run on OSX instead of on a box with a trunk version of intltool.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2547 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 4a768e2d073b6b85a6ca1e49748bb4f58216a37d

--- tv/resources/locale/build-catalogs.py
+++ tv/resources/locale/build-catalogs.py
@@ -10,3 +10,16 @@ for pofile in glob.glob ("*.po"):
 for dtd in glob.glob ("../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd") + glob.glob ("../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd.template"):
     dtd = os.path.basename (dtd)
     os.system ("(cd ../../platform/windows-xul/xul/chrome/locale/; intltool-merge --quoted-style -m ../../../../../resources/locale/ en-US/%s %s)" % (dtd, dtd))
+
+
+# Let's use xgettext for this since it supports string tables.
+langs = (("fr", "French"),
+         ("es", "Spanish"),
+         )
+for lang in langs:
+    try:
+        os.makedirs ("../../platform/osx/%s.lproj" % (lang[1],))
+    except:
+        pass
+    # The perl statement removes po entries that have plurals in them since xgettext gives an error on those when writing a stringtable.
+    os.system ( "cat %s.po | perl -e '$/=\"\"; while (<>) {print if !/msgid_plural/;}' | xgettext --force -o ../../platform/osx/%s.lproj/translated.strings --stringtable-output - -L PO" % (lang[0], lang[1]))
--- tv/resources/locale/build-messages.sh
+++ tv/resources/locale/build-messages.sh
@@ -4,4 +4,8 @@ intltool-extract --type=gettext/glade ../../platform/gtk-x11/glade/democracy.gla
 for dtd in ../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd ../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd.template; do
     intltool-extract --type=gettext/quoted $dtd
 done
-xgettext -k_ -kN_ -o messages.pot `find ../../ -name '*.py' -and -not -path '*.svn*' -and -not -path '*build*' -and -not -path '*dist*'` ../../platform/gtk-x11/glade/democracy.glade.h ../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd.h ../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd.template.h
+
+# Strings has to come first because the code that parses strings doesn't handle multiple references to the same string, while the rest of the xgettext parsers do.
+
+xgettext -k_ -kN_ -o messages.pot ../../platform/osx/English.lproj/*.strings `find ../../ -name '*.py' -and -not -path '*.svn*' -and -not -path '*build*' -and -not -path '*dist*'` ../../platform/gtk-x11/glade/democracy.glade.h ../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd.h ../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd.template.h
+
