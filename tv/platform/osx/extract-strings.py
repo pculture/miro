e7e01c758b3eadc68648cc0c8beef2b76dbf6571
Luc Heinrich <luc@pculture.org>
2006-08-09 16:23:12 +0000

    - Updated the extract and inject scripts to work with the new lproj location. - Normalized indentation in extract and inject scripts. - Updated English string files from latest changes in nib files.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2624 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: fe94295937e6a3523ec9255b28805af60b2789f0

--- tv/platform/osx/extract-strings.py
+++ tv/platform/osx/extract-strings.py
@@ -1,6 +1,6 @@
 from glob import glob
 import os
-for nib in glob("English.lproj/*.nib"):
+for nib in glob("Resources/English.lproj/*.nib"):
     name = os.path.basename (nib)[:-4]
-    os.system ("nibtool English.lproj/%s.nib -L -8 > English.lproj/%s.strings" % (name, name))
+    os.system ("nibtool Resources/English.lproj/%s.nib -L -8 > Resources/English.lproj/%s.strings" % (name, name))
     
--- tv/platform/osx/inject-strings.py
+++ tv/platform/osx/inject-strings.py
@@ -1,18 +1,18 @@
 from glob import glob
 import os
-for lproj in glob("*.lproj"):
+for lproj in glob("Resources/*.lproj"):
     lang = os.path.basename(lproj)[:-6]
     if lang == "English":
-	continue
-    if os.path.exists ("%s.lproj/translated.strings" % (lang)):
-	for nib in glob("English.lproj/*.nib"):
-	    name = os.path.basename (nib)[:-4]
-	    exists = os.path.exists ("%s.lproj/%s.nib" % (lang, name))
-	    if exists:
-		nib = "%s.lproj/temp.%s.nib" % (lang, name)
-	    else:
-		nib = "%s.lproj/%s.nib" % (lang, name)
-	    os.system ("nibtool -8 English.lproj/%s.nib -d %s.lproj/translated.strings -W %s" % (name, lang, nib))
-	    if exists:
-		os.system ("mv %s.lproj/temp.%s.nib/* %s.lproj/%s.nib/" % (lang, name, lang, name))
-		os.system ("rmdir %s.lproj/temp.%s.nib" % (lang, name))
+        continue
+    if os.path.exists ("Resources/%s.lproj/translated.strings" % (lang)):
+        for nib in glob("Resources/English.lproj/*.nib"):
+            name = os.path.basename (nib)[:-4]
+            exists = os.path.exists ("Resources/%s.lproj/%s.nib" % (lang, name))
+            if exists:
+                nib = "Resources/%s.lproj/temp.%s.nib" % (lang, name)
+            else:
+                nib = "Resources/%s.lproj/%s.nib" % (lang, name)
+            os.system ("nibtool -8 Resources/English.lproj/%s.nib -d Resources/%s.lproj/translated.strings -W %s" % (name, lang, nib))
+            if exists:
+                os.system ("mv Resources/%s.lproj/temp.%s.nib/* Resources/%s.lproj/%s.nib/" % (lang, name, lang, name))
+                os.system ("rmdir Resources/%s.lproj/temp.%s.nib" % (lang, name))
