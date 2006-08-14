75371d371fc0abee27b3a06d06cba2d9bc2c9719
Chris Lahey <clahey@pculture.org>
2006-08-14 22:46:07 +0000

    Don't use the lang if we don't have chrome for it.
    Set the lang to en-US if there isn't one set.
    
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2689 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: e2606dc1dce7c0c27095a83ad18f45f9b8cb02c1

--- tv/platform/windows-xul/frontend_implementation/Application.py
+++ tv/platform/windows-xul/frontend_implementation/Application.py
@@ -74,14 +74,19 @@ class Application:
         lang = getLocale()
         if lang:
             os.environ["LANGUAGE"] = lang
-            from xpcom import components
-            ps_cls = components.classes["@mozilla.org/preferences-service;1"]
-            print ps_cls
-            ps = ps_cls.getService(components.interfaces.nsIPrefService)
-            print ps
-            branch = ps.getBranch("general.useragent.")
-            branch.setCharPref("locale", lang)
-            locale.setlocale(locale.LC_ALL, '')
+            if not os.path.exists(resource.path(r"..\chrome\locale\%s" % (lang,))):
+                lang = "en-US"
+        else:
+            lang = "en-US"
+
+        from xpcom import components
+        ps_cls = components.classes["@mozilla.org/preferences-service;1"]
+        ps = ps_cls.getService(components.interfaces.nsIPrefService)
+        branch = ps.getBranch("general.useragent.")
+        branch.setCharPref("locale", lang)
+
+        locale.setlocale(locale.LC_ALL, '')
+
         gettext.bindtextdomain("democracyplayer", resource.path("locale"))
         gettext.textdomain("democracyplayer")
         gettext.bind_textdomain_codeset("democracyplayer","UTF-8")
