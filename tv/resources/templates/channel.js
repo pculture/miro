df1fc664523f009daa9c1b607828e02d25a87f80
Ben Dean-Kawamura <ben@pculture.org>
2006-08-18 15:42:19 +0000

    Fix so that new and old items be selected at the same time.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@2783 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 1c11d99f5a5a4f91c65a86e45322b6b46cc0866e

--- tv/resources/templates/channel.js
+++ tv/resources/templates/channel.js
@@ -1,7 +1,8 @@
 function showAllVideos() {
     var showMoreLabel = document.getElementById("show-more-videos-label");
     if(showMoreLabel.style.display != 'none') {
-        toggleLayer('main-newlyavailable-seen');
+        toggleLayer('main-newlyavailable');
+        toggleLayer('main-matching');
         toggleLayer('show-more-videos-label');
         toggleLayer('main-channelbar-available');
     }
