dad1f2aa672151354bc67e3f08bc5432d88601f6
Nick Nassar <nassar@pculture.org>
2006-09-07 18:37:13 +0000

    Solves at least part of the dreaded psyco.bind bug
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/branches/Democracy-Player-0.9.0@3040 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 065037ee406a7fb3541171dfb21e7e6585aae265

--- tv/portable/BitTornado/launchmanycore.py
+++ tv/portable/BitTornado/launchmanycore.py
@@ -3,14 +3,12 @@
 # Written by John Hoffman
 # see LICENSE.txt for license information
 
-from BitTornado import PSYCO
-if PSYCO.psyco:
-    try:
-        import psyco
-        assert psyco.__version__ >= 0x010100f0
-        psyco.full()
-    except:
-        pass
+try:
+    import psyco
+    assert psyco.__version__ >= 0x010100f0
+    psyco.full()
+except:
+    pass
 
 from download_bt1 import BT1Download
 from RawServer import RawServer, UPnP_ERROR
