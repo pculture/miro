ac9f3976eb47602709d6f7d59996129eab32b2f5
Chris Lahey <clahey@pculture.org>
2006-09-13 19:40:57 +0000

    Added autostart option.  Cleaned up dialog UI a bit.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3184 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: f4546c68d066c41adf7b8d9089c3a3a10cc01690

--- tv/platform/gtk-x11/frontend_implementation/startup.py
+++ tv/platform/gtk-x11/frontend_implementation/startup.py
@@ -3,6 +3,7 @@ import MainFrame
 import gtk_queue
 import gtk
 import os
+import shutil
 from threading import Thread
 import util
 from gettext import gettext as _
@@ -41,127 +42,141 @@ class _Search:
         self.finished(found)
 
 def performStartupTasks(terminationCallback):
-        
-    try:
-        widgetTree = MainFrame.WidgetTree(resource.path('democracy.glade'), 'dialog-startup', 'democracyplayer')
-        dialog = widgetTree['dialog-startup']
-        status = {}
-        status['inSearch'] = False
-        status['files'] = None
+    widgetTree = MainFrame.WidgetTree(resource.path('democracy.glade'), 'dialog-startup', 'democracyplayer')
+    dialog = widgetTree['dialog-startup']
+    widgetTree['image-startup-tv'].set_from_file (resource.sharePath('pixmaps/democracyplayer-128x128.png'))
+    dialog.set_icon_from_file (resource.sharePath('pixmaps/democracyplayer-128x128.png'))
+    status = {}
+    status['inSearch'] = False
+    status['files'] = None
+    status['searchSuccess'] = False
+    def overall_cancel(*args):
         status['searchSuccess'] = False
-        def overall_cancel(*args):
-            status['searchSuccess'] = False
-            status['files'] = None
-            count = 0
-            widgetTree['label-startup-search-progress'].set_text (ngettext("%d video found", "%d videos found", count) % (count,))
-            widgetTree['progressbar-startup-search'].set_fraction(0.0)
-            updateUI()
-        widgetTree['button-startup-search-cancel'].connect("clicked", overall_cancel)
+        status['files'] = None
+        count = 0
+        widgetTree['label-startup-search-progress'].set_text (ngettext("%d video found", "%d videos found", count) % (count,))
+        widgetTree['progressbar-startup-search'].set_fraction(0.0)
+        updateUI()
+    widgetTree['button-startup-search-cancel'].connect("clicked", overall_cancel)
 
-        def onSearch():
-            return widgetTree['radiobutton-search-yes'].get_active() and not status['searchSuccess']            
+    def onSearch():
+        return widgetTree['radiobutton-search-yes'].get_active() and not status['searchSuccess']            
 
-        def updateUI(*args):
-            widgetTree['notebook-startup'].set_current_page (step)
+    def updateUI(*args):
+        widgetTree['notebook-startup'].set_current_page (step)
 
-            def set_sensitive(toggle, widget):
-                widgetTree[widget].set_sensitive(widgetTree[toggle].get_active())
-            set_sensitive('radiobutton-search-yes', 'vbox-search-location')
-            set_sensitive('radiobutton-search-custom', 'filechooserbutton-search-custom')
+        def set_sensitive(toggle, widget):
+            widgetTree[widget].set_sensitive(widgetTree[toggle].get_active())
+        set_sensitive('radiobutton-search-yes', 'vbox-search-location')
+        set_sensitive('radiobutton-search-custom', 'filechooserbutton-search-custom')
 
-            def show_hide(widget, value):
-                widgetTree[widget].set_sensitive(value)
-                if value:
-                    widgetTree[widget].show()
-                else:
-                    widgetTree[widget].hide()
+        def show_hide(widget, value):
+            widgetTree[widget].set_sensitive(value)
+            if value:
+                widgetTree[widget].show()
+            else:
+                widgetTree[widget].hide()
 
-            if step == 0:
+        if step == 0:
+            widgetTree['button-startup-back'].set_sensitive(False)
+
+            show_hide ('button-startup-forward', True)
+            show_hide ('button-startup-ok', False)
+            show_hide ('button-startup-search', False)
+        elif step == 1:
+            widgetTree['button-startup-back'].set_sensitive(True)
+
+            show_hide ('button-startup-forward', not onSearch())
+            show_hide ('button-startup-search', onSearch())
+            show_hide ('button-startup-ok', False)
+
+            if status['inSearch']:
+                widgetTree['button-startup-search'].set_sensitive(False)
+                widgetTree['button-startup-forward'].set_sensitive(False)
                 widgetTree['button-startup-back'].set_sensitive(False)
 
-                show_hide ('button-startup-forward', True)
-                show_hide ('button-startup-ok', False)
-                show_hide ('button-startup-search', False)
-            elif step == 1:
-                widgetTree['button-startup-back'].set_sensitive(True)
-
-                show_hide ('button-startup-forward', not onSearch())
-                show_hide ('button-startup-search', onSearch())
-                show_hide ('button-startup-ok', False)
-
-                if status['inSearch']:
-                    widgetTree['button-startup-search'].set_sensitive(False)
-                    widgetTree['button-startup-forward'].set_sensitive(False)
-                    widgetTree['button-startup-back'].set_sensitive(False)
-
-                if status['inSearch'] or status['searchSuccess']:
-                    widgetTree['vbox-startup-search'].set_sensitive(True)
-                    widgetTree['vbox-startup-search-controls'].set_sensitive(False)
-                else:
-                    widgetTree['vbox-startup-search'].set_sensitive(False)
-                    widgetTree['vbox-startup-search-controls'].set_sensitive(True)
-            elif step == 2:
-                widgetTree['button-startup-back'].set_sensitive(True)
-
-                show_hide ('button-startup-forward', False)
-                show_hide ('button-startup-search', False)
-                show_hide ('button-startup-ok', True)
-
-        widgetTree ['radiobutton-search-yes'].connect("toggled", updateUI)
-        widgetTree ['radiobutton-search-custom'].connect("toggled", updateUI)
- 
-        step = 0
-        while (step < 3):
-            # Setup step
-
-            updateUI()
-
-            response = dialog.run()
-            if response == gtk.RESPONSE_NO:
-                step = step - 1
-            elif response == gtk.RESPONSE_YES:
-                if step == 1:
-                    if onSearch():
-                        if widgetTree['radiobutton-search-custom'].get_active():
-                            path = widgetTree['filechooserbutton-search-custom'].get_filename()
-                        else:
-                            path = os.path.expanduser('~')
-                        widgetTree['vbox-startup-search'].show()
-
-                        status['inSearch'] = True
-                        updateUI()
-
-                        search = _Search(widgetTree, path)
-
-                        def cancel(*args):
-                            search.cancelled = True
-                        handler = widgetTree['button-startup-search-cancel'].connect("clicked", cancel)
-
-                        search.run()
-
-                        widgetTree['button-startup-search-cancel'].disconnect(handler)
-
-                        status['inSearch'] = False
-                        updateUI()
-
-                        if not search.cancelled:
-                            status['files'] = search.found
-                            status['searchSuccess'] = True
-                            try:
-                                count = len(status['files'])
-                            except:
-                                count = 0
-                            widgetTree['label-startup-search-progress'].set_text (ngettext("%d video found", "%d videos found", count) % (count,))
-                            widgetTree['progressbar-startup-search'].set_fraction(1.0)
-                        step = step - 1
-                step = step + 1
-                # Handle step
+            if status['inSearch'] or status['searchSuccess']:
+                widgetTree['vbox-startup-search'].set_sensitive(True)
+                widgetTree['vbox-startup-search-controls'].set_sensitive(False)
             else:
-                status['files'] = None
-                return
-    finally:
+                widgetTree['vbox-startup-search'].set_sensitive(False)
+                widgetTree['vbox-startup-search-controls'].set_sensitive(True)
+        elif step == 2:
+            widgetTree['button-startup-back'].set_sensitive(True)
+
+            show_hide ('button-startup-forward', False)
+            show_hide ('button-startup-search', False)
+            show_hide ('button-startup-ok', True)
+
+    widgetTree ['radiobutton-search-yes'].connect("toggled", updateUI)
+    widgetTree ['radiobutton-search-custom'].connect("toggled", updateUI)
+ 
+    step = 0
+    while (step < 3):
+        # Setup step
+
+        updateUI()
+
+        response = dialog.run()
+        if response == gtk.RESPONSE_NO:
+            step = step - 1
+        elif response == gtk.RESPONSE_YES:
+            if step == 1:
+                if onSearch():
+                    if widgetTree['radiobutton-search-custom'].get_active():
+                        path = widgetTree['filechooserbutton-search-custom'].get_filename()
+                    else:
+                        path = os.path.expanduser('~')
+                    widgetTree['vbox-startup-search'].show()
+
+                    status['inSearch'] = True
+                    updateUI()
+
+                    search = _Search(widgetTree, path)
+
+                    def cancel(*args):
+                        search.cancelled = True
+                    handler = widgetTree['button-startup-search-cancel'].connect("clicked", cancel)
+
+                    search.run()
+
+                    widgetTree['button-startup-search-cancel'].disconnect(handler)
+
+                    status['inSearch'] = False
+                    updateUI()
+
+                    if not search.cancelled:
+                        status['files'] = search.found
+                        status['searchSuccess'] = True
+                        try:
+                            count = len(status['files'])
+                        except:
+                            count = 0
+                        widgetTree['label-startup-search-progress'].set_text (ngettext("%d video found", "%d videos found", count) % (count,))
+                        widgetTree['progressbar-startup-search'].set_fraction(1.0)
+                    step = step - 1
+            step = step + 1
+            # Handle step
+        else:
+            status['files'] = None
+            gtk_queue.queue.call_nowait(lambda : terminationCallback(None))
+            return
+    gtk_queue.queue.call_nowait(lambda : terminationCallback(status['files']))
+    if widgetTree['radiobutton-startup-autostart-yes'].get_active():
+        config_home = os.environ.get ('XDG_CONFIG_HOME',
+                                      '~/.config')
+        config_home = os.path.expanduser (config_home)
+        autostart_dir = os.path.join (config_home, "autostart")
+        destination = os.path.join (autostart_dir, "democracyplayer.desktop")
         try:
-            dialog.destroy()
+            os.makedirs(autostart_dir)
         except:
             pass
-        gtk_queue.queue.call_nowait(lambda : terminationCallback(status['files']))
+        try:
+            shutil.copy (resource.sharePath('applications/democracyplayer.desktop'), autostart_dir)
+        except:
+            pass
+    try:
+        dialog.destroy()
+    except:
+        pass
