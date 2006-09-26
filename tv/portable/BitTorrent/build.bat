1fe5f30a9ffc27c1c6eb531a6ce12e3c8db4c548
Ben Dean-Kawamura <ben@pculture.org>
2006-09-26 17:42:12 +0000

    Oops, I should have just checked in the BitTorrent library, not the entire package.  I'm going to delete the directory and restart.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3410 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: e1d545d60b1c622cd37b17d1f3494a4049291320

--- tv/portable/BitTorrent/build.bat
+++
@@ -1,3 +0,0 @@
-del /F /S /Q build dist
-c:\python23\python.exe winsetup.py py2exe
-"C:\Program Files\NSIS\makensis.exe" bittorrent.nsi
--- tv/portable/BitTorrent/osx/Tstate.h
+++
@@ -1,5 +0,0 @@
-#import <python2.3/Python.h>
-@protocol Tstate
-- (PyThreadState *)tstate;
-- (void)setTstate:(PyThreadState *)nstate;
-@end
