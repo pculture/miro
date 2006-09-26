ec9be483e0da93995f1926798e0628e57bed7823
Ben Dean-Kawamura <ben@pculture.org>
2006-09-26 17:45:33 +0000

    Applying Debian's patches to BitTorrent? from the current testing branch (etch). Patch file was bittorrent_3.4.2-8.1.diff.gz.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3412 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 1db4039b5c78ac71afa1b1b56c91c19d3509ec77

--- tv/portable/BitTorrent/zurllib.py
+++ tv/portable/BitTorrent/zurllib.py
@@ -35,8 +35,14 @@ class HTTPContentEncodingHandler(HTTPHandler):
         if DEBUG: 
              pprint.pprint(headers.dict)
         url = fp.url
-        return addinfourldecompress(fp, headers, url)
-
+	resp = addinfourldecompress(fp, headers, url)
+	# As of Python 2.4 http_open response also has 'code' and 'msg'
+        # members, and HTTPErrorProcessor breaks if they don't exist.
+	if 'code' in dir(fp):
+	    resp.code = fp.code
+	if 'msg' in dir(fp):
+	    resp.msg = fp.msg
+	return resp
 
 class addinfourldecompress(addinfourl):
     """Do gzip decompression if necessary. Do addinfourl stuff too."""
