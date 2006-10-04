7a271ade12825f868193ce815d9f1c453d850083
Luc Heinrich <luc@pculture.org>
2006-10-04 13:51:56 +0000

    Clearing #3764, #3765, #3766, #3767, #3768.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3477 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 92b4f321d15d8a14fbb4cfce07d798c68245d4be

--- tv/platform/osx/test/setup-unittests.py
+++ tv/platform/osx/test/setup-unittests.py
@@ -32,8 +32,6 @@ py2app_options = dict(
 setup(
     app=['unittests.py'],
     data_files= resourceFiles,
-# NEEDS XXXX
-#    ext_modules = [vlchelper.info.getExtension(root)],
     options=dict(
         py2app=py2app_options,
     ),
@@ -47,73 +45,3 @@ setup(
         ],
     cmdclass = {'build_ext': build_ext}
 )
-
-# NEEDS: Hack: Copy VLC libraries into bundle so they are at the place
-# that the OS X VLC build process told the dynamic linker told them
-# they would be. There should be a cleaner way to do
-# this. Furthermore, although the rule was taken from VLC's makefile,
-# it probably copies more libraries than we actually need.
-#
-# In an ideal world, VLC would put the actual paths on the local
-# machine into the objects, and py2app would rewrite those paths to be
-# bundle relative during its normal dependency scan.
-#
-# Also copy modules into the bundle. This is a little less than ideal
-# as well.
-#
-# IMPORTANT: Note well that we don't *remove* any files left over from
-# the previous build that disappeared in this one. So, before building
-# a distribution bundle, you should be sure to 'clean'.
-
-VLC_LIBRARY_SUBDIR = 'extras/contrib/vlc-lib'
-BUNDLE_LIB_DIRECTORY = 'Contents/MacOS/lib'
-BUNDLE_MODULE_DIRECTORY = 'Contents/MacOS'
-
-if 'py2app' in sys.argv and False: ## NEEDS XXXX
-    # Create symlinks or copy files?
-    alias = '-A' in sys.argv
-    
-    # NEEDS: We guess where the bundle was built. Horrible.
-    bundleRoot = 'dist/Democracy.app'
-    for i in range(0,len(sys.argv)-1):
-        if sys.argv[i] == '--dist-dir':
-            bundleRoot = '%s/Democracy.app' % sys.argv[i+1]
-
-    # list of (sourcePath, destinationPath) tuples of files to copy or link
-    manifest = []
-
-    # Make list of libraries to copy, and compute their VLC-mangled names.
-    vlcLibDir = "%s/%s" % (vlchelper.info.getVLCRoot(root), VLC_LIBRARY_SUBDIR)
-    bundleLibDir = "%s/%s" % (bundleRoot, BUNDLE_LIB_DIRECTORY)
-    if not os.access(bundleLibDir, os.F_OK):
-        os.makedirs(bundleLibDir)
-    for file in os.listdir(vlcLibDir):
-        src = os.path.abspath('%s/%s' % (vlcLibDir, file))
-        dest = '%s/vlc_%s' % (bundleLibDir, os.path.basename(file))
-        manifest.append((src, dest))
-
-    # Find the modules to copy.
-    bundleModuleDir = "%s/%s" % (bundleRoot, BUNDLE_MODULE_DIRECTORY)
-    for module in vlchelper.info.getModuleList(root):
-        src = os.path.abspath('%s/%s.dylib' % (vlchelper.info.getVLCRoot(root), module))
-        dest = '%s/%s.dylib' % (bundleModuleDir, module)
-        manifest.append((src, dest))
-
-    # Copy or link the files.
-    for (src, dest) in manifest:
-        # Make sure the destination directory exists.
-        if not os.access(os.path.dirname(dest), os.F_OK):
-            os.makedirs(os.path.dirname(dest))
-
-        # Delete the file if it exists.
-        try:
-            os.unlink(dest)
-        except OSError:
-            pass
-
-        # Copy or link the file.
-        if alias:
-            os.symlink(src, dest)
-        else:
-            # NEEDS: frob permissions?
-            shutil.copy(src, dest)
