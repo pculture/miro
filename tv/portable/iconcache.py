pculture/miro

commit a38358ebde69e4bd5ca1c31a982fa4c319d2493c

Put in random ids so that we won't get mozilla caching incorrect file: URLs where the old file has been deleted and replaced with a new file.
git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3708 bce20531-a64c-4166-8316-56baf4a01729

Original sha1: 09da437390067734489bac30a0243a9c4e22a950

tv/portable/iconcache.py
@@ -9,6 +9,7 @@ import config
 import prefs
 import time
 import views
+import random
 
 RUNNING_MAX = 3
 
@@ -179,7 +180,16 @@ class IconCache:
                 return
 
             if (self.filename == None):
-                self.filename = os.path.join(cachedir, info["filename"])
+                # Add a random unique id
+                parts = info["filename"].split('.')
+                uid = "%08d" % (random.randint(0,99999999),)
+                if len(parts) == 1:
+                    self.filename = "%s.%s" % (name, uid)
+                else:
+                    parts[-1:-1] = [uid]
+                    self.filename = '.'.join(parts)
+
+                self.filename = os.path.join(cachedir, self.filename)
                 self.filename = shortenFilename (self.filename)
                 self.filename = nextFreeFilename (self.filename)
                 needsSave = True

commit df28607b5458d847826012d7d310a93722086b49

Fixed exception introduced in r3708.
git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3711 bce20531-a64c-4166-8316-56baf4a01729

Original sha1: 7f76e616617126586855779316bce41954041d73

tv/portable/iconcache.py
@@ -184,10 +184,10 @@ class IconCache:
                 parts = info["filename"].split('.')
                 uid = "%08d" % (random.randint(0,99999999),)
                 if len(parts) == 1:
-                    self.filename = "%s.%s" % (name, uid)
+                    parts.append(uid)
                 else:
                     parts[-1:-1] = [uid]
-                    self.filename = '.'.join(parts)
+                self.filename = '.'.join(parts)
 
                 self.filename = os.path.join(cachedir, self.filename)
                 self.filename = shortenFilename (self.filename)