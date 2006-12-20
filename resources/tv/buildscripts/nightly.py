9cd03c41a026d26ee58e1c736cea059f3fabf70f
Nick Nassar <nassar@pculture.org>
2006-12-20 23:28:46 +0000

    Finished. Need to complete 'clean' in setup.py before this can go into production
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@3872 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 62b849199f10c7573683d64b88e1c7ebffab01b4

--- resources/tv/buildscripts/nightly.py
+++ resources/tv/buildscripts/nightly.py
@@ -7,35 +7,55 @@
 #
 
 import sys
+import shutil
 import os
+import re
 import os.path
 
 orig_dir = os.getcwd()
 repository_url = "https://svn.participatoryculture.org/svn/dtv/trunk/"
 
 def die(error = "An error occured", code = 1):
-    print "ERROR: %s" % error
-    os.chdir(orig_dir)
-    sys.exit(code)
+    try:
+        print "ERROR: %s" % error
+        os.chdir(orig_dir)
+    finally:
+        sys.exit(code)
+
+def chdir_or_die(path):
+    try:
+        os.chdir(path)
+    except:
+        die("Cannot change current directory to %s" % path)
 
 def checkout_and_update(repository, build_dir):
     full_dir = os.path.join(build_dir, repository)
     full_url = repository_url + repository
     if os.path.isdir(repository):
+        print "Updating %s..." % repository
+        sys.stdout.flush()
+        chdir_or_die(full_dir)
         try:
-            os.chdir(full_dir)
             if (os.system("svn --non-interactive update")):
                 die("SVN update of %s failed" % repository)
-            os.chdir(build_dir)
         except:
             die("Error running svn update of %s" % repository)
+        chdir_or_die(build_dir)
     else:
+        print "Checking out %s..." % repository
+        sys.stdout.flush()
+        chdir_or_die(build_dir)
         try:
-            os.chdir(build_dir)
             if (os.system("svn --non-interactive co %s" % full_url)):
                 die("SVN update of %s failed" % repository)
         except:
             die("Error running svn check out of %s" % repository)
+
+def find_installer_name():
+    for filename in os.listdir(os.path.join(platform_build_dir,"dist")):
+        if re.match(installer_filename, filename):
+            return filename
+    return None
 
 # Eventually, this build script will work for all supported platforms
 #
@@ -43,6 +63,7 @@ def checkout_and_update(repository, build_dir):
 platform = "windows-xul"
 repositories = ["tv","dtv-binary-kit"]
 build_command = "bdist_xul"
+installer_filename = "Democracy-[0-9.]+\.exe"
 
 # Find the build directory
 if len(sys.argv) <= 1:
@@ -50,14 +71,12 @@ if len(sys.argv) <= 1:
 
 root_dir = os.path.normpath(sys.argv[1])
 build_dir = os.path.join(root_dir, "build")
+platform_build_dir = os.path.join(build_dir,"tv","platform",platform)
 
 if not os.path.isdir(root_dir):
     die("%s is not a directory" % root_dir)
 
-try:
-    os.chdir(root_dir)
-except:
-    die("Cannot change current directory to %s" % root_dir)
+chdir_or_die(root_dir)
 
 if not os.path.isdir(build_dir):
    try:
@@ -65,10 +84,33 @@ if not os.path.isdir(build_dir):
    except:
        die("Cannot create %s" % build_dir)
 
-try:
-    os.chdir(build_dir)
-except:
-    die("Cannot change current directory to %s" % build_dir)
+chdir_or_die(build_dir)
 
 for repository in repositories:
     checkout_and_update(repository, build_dir)
+
+chdir_or_die(platform_build_dir)
+
+try:
+    if (os.system("python setup.py clean")):
+        die("Clean failed")
+except:
+    die("Cannot clean build directory")
+
+try:
+    if (os.system("python setup.py %s" % build_command)):
+        die("Build failed")
+except:
+    die("Cannot run build")
+
+installer = find_installer_name()
+if installer is None:
+    die("Can't find the installer!")
+
+try:
+    shutil.copyfile(os.path.join(platform_build_dir,"dist",installer),
+                    os.path.join(root_dir, installer))
+except:
+    die("Can't copy the installer!")
+
+chdir_or_die(orig_dir)
