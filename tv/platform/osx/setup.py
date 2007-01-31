3ed9e6dfc8ee38cf3d6caa460efb22a27c798e94
Nick Nassar <nassar@pculture.org>
2007-01-31 18:08:37 +0000

    Rearranged things to use distutils framework. Added "clean"
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4094 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 176e062043d0babf17c246e1e7fdbc0055d4ce4a

--- tv/platform/osx/setup.py
+++ tv/platform/osx/setup.py
@@ -9,6 +9,8 @@ from glob import glob
 from distutils.core import setup
 from Pyrex.Distutils import build_ext
 from distutils.extension import Extension
+from distutils.cmd import Command
+from py2app.build_app import py2app
 
 # Get command line parameters
 
@@ -28,7 +30,6 @@ sys.path[0:0]=[os.path.join(root, 'portable')]
 # Only now may we import things from our own tree
 
 import template_compiler
-template_compiler.compileAllTemplates(root)
 
 # Look for the Boost library in various common places.
 # - we assume that both the library and the include files are installed in the
@@ -114,7 +115,175 @@ else:
 
 # And launch the setup process...
 
-print "Building Democracy Player v%s (%s)" % (conf['appVersion'], conf['appRevision'])
+class clean(Command):
+    user_options = []
+    def initialize_options(self):
+        return None
+    def finalize_options(self):
+        return None
+    def run(self):
+        print "Removing old build directory..."
+        try:
+            shutil.rmtree('build')
+        except:
+            pass
+        print "Removing old dist directory..."
+        try:
+            shutil.rmtree('dist')
+        except:
+            pass
+        print "Removing old app..."
+        try:
+            shutil.rmtree('Democracy.app')
+        except:
+            pass
+        print "Removing old compiled templates..."
+        try:
+            for filename in os.listdir(os.path.join("..","..","portable","compiled_templates")):
+                if not filename.startswith(".svn"):
+                    filename = os.path.join("..","..","portable","compiled_templates",filename)
+                    if os.path.isdir(filename):
+                        shutil.rmtree(filename)
+                    else:
+                        os.remove(filename)
+        except:
+            pass
+
+class mypy2app(py2app):
+    def run(self):
+        global root, bundleRoot, rsrcRoot
+        template_compiler.compileAllTemplates(root)
+        py2app.run(self)
+        # Setup some variables we'll need
+
+        bundleRoot = 'Democracy.app/Contents'
+        execRoot = os.path.join(bundleRoot, 'MacOS')
+        rsrcRoot = os.path.join(bundleRoot, 'Resources')
+        fmwkRoot = os.path.join(bundleRoot, 'Frameworks')
+        prsrcRoot = os.path.join(rsrcRoot, 'resources')
+
+        # Py2App seems to have a bug where alias builds would get
+        # incorrect symlinks to frameworks, so create them manually.
+
+        for fmwk in glob(os.path.join(fmwkRoot, '*.framework')):
+            if os.path.islink(fmwk):
+                dest = os.readlink(fmwk)
+                if not os.path.exists(dest):
+                    print "Fixing incorrect symlink for %s" % os.path.basename(fmwk)
+                    os.remove(fmwk)
+                    os.symlink(os.path.dirname(dest), fmwk)
+
+        # Create a hard link to the main executable with a different
+        # name for the downloader. This is to avoid having 'Democracy'
+        # shown twice in the Activity Monitor since the downloader is
+        # basically Democracy itself, relaunched with a specific
+        # command line parameter.
+
+        print "Creating Downloader hard link."
+
+        srcPath = os.path.join(execRoot, 'Democracy')
+        linkPath = os.path.join(execRoot, 'Downloader')
+
+        if os.path.exists(linkPath):
+            os.remove(linkPath)
+        os.link(srcPath, linkPath)
+
+        # Copy our own portable resources
+
+        print "Copying portable resources to application bundle"
+
+        if forceUpdate and os.path.exists(prsrcRoot):
+            shutil.rmtree(prsrcRoot, True)
+
+        if os.path.exists(prsrcRoot):
+            print "    (all skipped, already bundled)"
+        else:
+            os.mkdir(prsrcRoot)
+            excludedRsrc = ['app.config.template', 'locale', 'testdata']
+            for resource in glob(os.path.join(root, 'resources/*')):
+                rsrcName = os.path.basename(resource)
+                if rsrcName not in excludedRsrc:
+                    if os.path.isdir(resource):
+                        dest = os.path.join(prsrcRoot, rsrcName)
+                        copy = shutil.copytree
+                    else:
+                        dest = os.path.join(prsrcRoot, rsrcName)
+                        copy = shutil.copy
+                    copy(resource, dest)
+                    print "    %s" % dest
+
+        # Install the final app.config file
+
+        print "Copying config file to application bundle"
+        shutil.move(appConfigPath, os.path.join(prsrcRoot, 'app.config'))
+
+        # Copy the gettext MO files in a 'locale' folder inside the
+        # application bundle resources folder. Doing this manually at
+        # this stage instead of automatically through the py2app
+        # options allows to avoid having an intermediate unversioned
+        # 'locale' folder.
+
+        print "Copying gettext MO files to application bundle."
+
+        localeDir = os.path.join (root, 'resources/locale')
+        lclDir = os.path.join(rsrcRoot, 'locale')
+        if forceUpdate and os.path.exists(lclDir):
+            shutil.rmtree(lclDir, True);
+
+        if os.path.exists(lclDir):
+            print "    (all skipped, already bundled)"
+        else:
+            for source in glob(os.path.join(localeDir, '*.mo')):
+                lang = os.path.basename(source)[:-3]
+                dest = os.path.join(lclDir, lang, 'LC_MESSAGES/democracyplayer.mo')
+                os.makedirs(os.path.dirname(dest))
+                shutil.copy2(source, dest)
+                print "    %s" % dest
+
+        # Copy the Quicktime components which we will automatically
+        # install to allow internal playback of a lot of formats and
+        # codecs.
+        print "Copying installable Quicktime components."
+
+        cmpntRoot = os.path.join(bundleRoot, 'Components')
+        if forceUpdate and os.path.exists(cmpntRoot):
+            shutil.rmtree(cmpntRoot, True);
+
+        if os.path.exists(cmpntRoot):
+            print "    (all skipped, already bundled)"
+        else:
+            os.makedirs(cmpntRoot)
+            componentsRoot = os.path.join(os.path.dirname(root), 'dtv-binary-kit-mac/qtcomponents')
+            components = glob(os.path.join(componentsRoot, '**/*.component'))
+            if len(components) == 0:
+                print "    (all skipped, not found in binary kit)"
+            else:
+                for component in components:
+                    componentDest = os.path.join(cmpntRoot, os.path.basename(component))
+                    componentName = os.path.basename(component)
+                    shutil.copytree(component, componentDest)
+                    print "    %s" % componentDest
+        
+        # Check that we haven't left some turds in the application bundle.
+        
+        wipeList = list()
+        for root, dirs, files in os.walk('Democracy.app'):
+            for excluded in ('.svn', 'unittest'):
+                if excluded in dirs:
+                    dirs.remove(excluded)
+                    wipeList.append(os.path.join(root, excluded))
+        
+        if len(wipeList) > 0:
+            print "Wiping out unwanted data from the application bundle."
+            for folder in wipeList:
+                print "    %s" % folder
+                shutil.rmtree(folder)
+        
+        # And we're done...
+        
+        print "------------------------------------------------"
+        
+        print "Building Democracy Player v%s (%s)" % (conf['appVersion'], conf['appRevision'])
 
 py2app_options = dict(
     plist = infoPlist,
@@ -137,132 +306,8 @@ setup(
                   extra_objects=[boostLib],
                   include_dirs=[boostIncludeDir])
         ],
-    cmdclass = dict(build_ext = build_ext)
+    cmdclass = {'build_ext': build_ext,
+	        'clean': clean,
+                'py2app': mypy2app}
 )
 
-# Setup some variables we'll need
-
-bundleRoot = 'Democracy.app/Contents'
-execRoot = os.path.join(bundleRoot, 'MacOS')
-rsrcRoot = os.path.join(bundleRoot, 'Resources')
-fmwkRoot = os.path.join(bundleRoot, 'Frameworks')
-prsrcRoot = os.path.join(rsrcRoot, 'resources')
-
-# Py2App seems to have a bug where alias builds would get incorrect symlinks to
-# frameworks, so create them manually.
-
-for fmwk in glob(os.path.join(fmwkRoot, '*.framework')):
-    if os.path.islink(fmwk):
-        dest = os.readlink(fmwk)
-        if not os.path.exists(dest):
-            print "Fixing incorrect symlink for %s" % os.path.basename(fmwk)
-            os.remove(fmwk)
-            os.symlink(os.path.dirname(dest), fmwk)
-    
-# Create a hard link to the main executable with a different name for the 
-# downloader. This is to avoid having 'Democracy' shown twice in the Activity 
-# Monitor since the downloader is basically Democracy itself, relaunched with a 
-# specific command line parameter.
-
-print "Creating Downloader hard link."
-
-srcPath = os.path.join(execRoot, 'Democracy')
-linkPath = os.path.join(execRoot, 'Downloader')
-
-if os.path.exists(linkPath):
-    os.remove(linkPath)
-os.link(srcPath, linkPath)
-
-# Copy our own portable resources
-
-print "Copying portable resources to application bundle"
-
-if forceUpdate and os.path.exists(prsrcRoot):
-    shutil.rmtree(prsrcRoot, True)
-
-if os.path.exists(prsrcRoot):
-    print "    (all skipped, already bundled)"
-else:
-    os.mkdir(prsrcRoot)
-    excludedRsrc = ['app.config.template', 'locale', 'testdata']
-    for resource in glob(os.path.join(root, 'resources/*')):
-        rsrcName = os.path.basename(resource)
-        if rsrcName not in excludedRsrc:
-            if os.path.isdir(resource):
-                dest = os.path.join(prsrcRoot, rsrcName)
-                copy = shutil.copytree
-            else:
-                dest = os.path.join(prsrcRoot, rsrcName)
-                copy = shutil.copy
-            copy(resource, dest)
-            print "    %s" % dest
-
-# Install the final app.config file
-
-print "Copying config file to application bundle"
-shutil.move(appConfigPath, os.path.join(prsrcRoot, 'app.config'))
-
-# Copy the gettext MO files in a 'locale' folder inside the application bundle 
-# resources folder. Doing this manually at this stage instead of automatically 
-# through the py2app options allows to avoid having an intermediate unversioned 
-# 'locale' folder.
-
-print "Copying gettext MO files to application bundle."
-
-localeDir = os.path.join (root, 'resources/locale')
-lclDir = os.path.join(rsrcRoot, 'locale')
-if forceUpdate and os.path.exists(lclDir):
-    shutil.rmtree(lclDir, True);
-
-if os.path.exists(lclDir):
-    print "    (all skipped, already bundled)"
-else:
-    for source in glob(os.path.join(localeDir, '*.mo')):
-        lang = os.path.basename(source)[:-3]
-        dest = os.path.join(lclDir, lang, 'LC_MESSAGES/democracyplayer.mo')
-        os.makedirs(os.path.dirname(dest))
-        shutil.copy2(source, dest)
-        print "    %s" % dest
-
-# Copy the Quicktime components which we will automatically install to allow
-# internal playback of a lot of formats and codecs.
-
-print "Copying installable Quicktime components."
-
-cmpntRoot = os.path.join(bundleRoot, 'Components')
-if forceUpdate and os.path.exists(cmpntRoot):
-    shutil.rmtree(cmpntRoot, True);
-
-if os.path.exists(cmpntRoot):
-    print "    (all skipped, already bundled)"
-else:
-    os.makedirs(cmpntRoot)
-    componentsRoot = os.path.join(os.path.dirname(root), 'dtv-binary-kit-mac/qtcomponents')
-    components = glob(os.path.join(componentsRoot, '**/*.component'))
-    if len(components) == 0:
-        print "    (all skipped, not found in binary kit)"
-    else:
-        for component in components:
-            componentDest = os.path.join(cmpntRoot, os.path.basename(component))
-            componentName = os.path.basename(component)
-            shutil.copytree(component, componentDest)
-            print "    %s" % componentDest
-
-# Check that we haven't left some turds in the application bundle.
-
-wipeList = list()
-for root, dirs, files in os.walk('Democracy.app'):
-    for excluded in ('.svn', 'unittest'):
-        if excluded in dirs:
-            dirs.remove(excluded)
-            wipeList.append(os.path.join(root, excluded))
-
-if len(wipeList) > 0:
-    print "Wiping out unwanted data from the application bundle."
-    for folder in wipeList:
-        print "    %s" % folder
-        shutil.rmtree(folder)
-
-# And we're done...
-
-print "------------------------------------------------"
