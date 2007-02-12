0dc78255c6cd3cac747bea81eb8883b04895e1ff
Ben Dean-Kawamura <ben@pculture.org>
2007-02-12 18:56:01 +0000

    Adding script to fix the plural problem with ro.po.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4222 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 599bff8f20744d9bda0f97b23fbd9c6bafbb5b02

--- tv/resources/locale/build-catalogs.py
+++ tv/resources/locale/build-catalogs.py
@@ -2,6 +2,8 @@ import glob
 import os.path
 import os
 
+os.system("python fix-ro-plurals.py")
+
 for pofile in glob.glob ("*.po"):
     lang = pofile[:-3]
     mofile = os.path.join ("%s.mo" % lang)
