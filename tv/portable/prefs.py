107f644c1d9db97205678e093a46efc1fc034a15
Chris Lahey <clahey@pculture.org>
2007-03-22 20:36:44 +0000

    Remove this unused pref.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4387 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 80e17810d532648ea3a32a2a6419a85171ec0350

--- tv/portable/prefs.py
+++ tv/portable/prefs.py
@@ -32,7 +32,6 @@ UPLOAD_RATIO                = Pref( key='uploadRatio',           default=2.0,   
 STARTUP_TASKS_DONE          = Pref( key='startupTasksDone',      default=False, platformSpecific=False )
 SINGLE_VIDEO_PLAYBACK_MODE  = Pref( key='singleVideoPlaybackMode',default=False,platformSpecific=False )
 RESUME_VIDEOS_MODE          = Pref( key='resumeVideosMode',      default=True,  platformSpecific=False )
-MY_COLLECTION_DIRS          = Pref( key="CollectionDirs",        default="",    platformSpecific=False )
 
 # Only used on particular platforms.
 XINE_VIZ                    = Pref( key="xineViz",               default=u"goom",platformSpecific=False )
