b8aacc096db901f08ddb002598d5fa5e206b90df
Ben Dean-Kawamura <ben@pculture.org>
2007-04-16 17:27:04 +0000

    Updated the windows translations so I can look at #3579.
    Updated build-catalogs.py so that it only translates the .dtd.template files.
    Deleted the .dtd files from svn.
    
    
    
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4507 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: f32d960e383304188268304f373062c306ab8e3a

--- tv/resources/locale/build-catalogs.py
+++ tv/resources/locale/build-catalogs.py
@@ -9,7 +9,7 @@ for pofile in glob.glob ("*.po"):
     mofile = os.path.join ("%s.mo" % lang)
     os.system ("msgfmt %s -o %s" % (pofile, mofile))
 
-for dtd in glob.glob ("../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd") + glob.glob ("../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd.template"):
+for dtd in glob.glob ("../../platform/windows-xul/xul/chrome/locale/en-US/*.dtd.template"):
     dtd = os.path.basename (dtd)
     os.system ("(cd ../../platform/windows-xul/xul/chrome/locale/; intltool-merge --quoted-style -m ../../../../../resources/locale/ en-US/%s %s)" % (dtd, dtd))
     os.system ("(cd ../../platform/windows-xul/xul/chrome/locale/; perl -pi -e 's/\\\\\\\"/&quot;/g' */%s)" % (dtd,))
