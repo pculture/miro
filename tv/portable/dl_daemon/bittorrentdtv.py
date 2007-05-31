3e14c89b335970fdc38179721c214244179b8ee6
Nick Nassar <nassar@pculture.org>
2007-05-31 18:57:25 +0000

    Fixes #7289
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/branches/Democracy-Player-0.9.6@4646 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: b8485ce89a3a0669fc82bffb6d005652cefe6b08

--- tv/portable/dl_daemon/bittorrentdtv.py
+++ tv/portable/dl_daemon/bittorrentdtv.py
@@ -381,7 +381,7 @@ class TorrentDownload:
             try:
                 fast_resume_data = {
                     'already_got': storagewrapper.get_have_list(),
-                    'mtimes': dict([(f.encode('utf-8'), path.getmtime(f)) for \
+                    'mtimes': dict([(f, path.getmtime(f)) for \
                             f, size in files]),
                 }
                 self.fast_resume_queue.put(bencode(fast_resume_data))
