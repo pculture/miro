b1d93e63afffb1329adc503f58a125ed7c73406d
Nick Nassar <nassar@pculture.org>
2007-06-01 01:57:51 +0000

    Fixes torrents under Windows, rebreaks 'Copy Video URL'
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4648 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 5e84830347c3dcd4b66d6a785fe99658634b2618

--- tv/platform/windows-xul/resources.py
+++ tv/platform/windows-xul/resources.py
@@ -39,9 +39,9 @@ def path(relative_path):
     return abspath.replace("/", "\\")
 
 def url(relative_path):
-    return u"file:///" + urllib.quote(path(relative_path).encode('utf_8'),safe="\\:~")
+    return u"file://" + urllib.quote(path(relative_path).encode('utf_8'),safe="\\:~")
 
 def absoluteUrl(absolute_path):
     """Like url, but without adding the resource directory.
     """
-    return u"file:///" + urllib.quote(absolute_path.encode('utf_8'),safe="\\:~")
+    return u"file://" + urllib.quote(absolute_path.encode('utf_8'),safe="\\:~")
