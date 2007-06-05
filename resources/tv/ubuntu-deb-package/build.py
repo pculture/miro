8fe88d89b9a30b1b4b76f7762e8f082e8e3492c8
Ben Dean-Kawamura <ben@pculture.org>
2007-06-05 18:17:02 +0000

    Added build script to make ubuntu packages.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4695 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 8804cc3f96801840c39ef58a248bfa3c2ac1b40c

---
+++ resources/tv/ubuntu-deb-package/build.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import os, sys, urllib
+from glob import glob
+import shutil
+
+def call(cmd):
+    """Call an external command.  If the command doesn't exit with status 0,
+    or if it outputs to stderr, an exception will be raised.  Returns stdout.
+    """
+    rv = os.system(cmd)
+    if rv != 0:
+        raise OSError("call with %s has return code %s" % (cmd, rv))
+
+def usage():
+    sys.stderr.write("""\
+Usage: build.py [version] [distribution-name] [debian-directory]
+
+For example:
+    build.py 0.9.5 feisty debian/
+""")
+    sys.exit(1)
+
+if len(sys.argv) != 4:
+    usage()
+
+version = sys.argv[1]
+distro = sys.argv[2]
+debian_dir = os.path.normpath(os.path.join(__file__, '..', sys.argv[3]))
+tarball_name = 'Democracy-%s.tar.gz' % version
+tarball_url = 'http://pculture.org/nightlies/' + tarball_name
+user = os.environ['USER']
+
+if not os.path.isdir(debian_dir):
+    usage()
+
+print """\
+Version: %s
+Distribution: %s
+Debian directory: %s
+Tarball URL: %s
+
+Press enter to continue, Ctrl-C to cancel.
+""" % (version, distro, debian_dir, tarball_url)
+raw_input()
+debian_dir = os.path.abspath(debian_dir)
+
+if os.path.exists('build-tmp'):
+    shutil.rmtree('build-tmp')
+os.mkdir('build-tmp')
+os.chdir('build-tmp')
+
+print "downloading tarball"
+call('wget %s' % tarball_url)
+print "extracting files"
+call('tar zxvf %s' %  tarball_name)
+os.rename('Democracy-%s' % version, 'democracyplayer-%s' % version)
+shutil.copytree(debian_dir, 'democracyplayer-%s/debian' % version)
+os.chdir('democracyplayer-%s' % version)
+print "building debs"
+call('dpkg-buildpackage -us -uc -rfakeroot')
+os.chdir('../..')
+
+if os.path.exists(distro):
+    shutil.rmtree(distro)
+os.mkdir(distro)
+call('mv build-tmp/*.deb %s' % distro)
+call('apt-ftparchive packages %s > %s/Packages' % (distro, distro))
+call('gzip %s/Packages' % distro)
+print 'done'
