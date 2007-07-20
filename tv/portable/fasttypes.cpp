ae0fe06b710a180a3f2b713cc1bd9bb7529e339a
Will Kahn-Greene <will.guaraldi@pculture.org>
2007-07-20 18:43:20 +0000

    Added or updated GPL headers for files in portable/ .  Bug #8219 .
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@4966 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 9c580a1393f245313251e8a671827cd76551a945

--- tv/portable/fasttypes.cpp
+++ tv/portable/fasttypes.cpp
@@ -1,8 +1,24 @@
+/*
+ * Miro - an RSS based video player application
+ * Copyright (C) 2005-2007 Participatory Culture Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
 // fasttypes.cpp implements linked list and sorted list types for
 // Python using the STL class libraries.
-
-// This file is part of DTV and licensed under the GNU GPL. See
-// license.txt for details.
 
 // TODO: Finalize and document the Python classes
 
