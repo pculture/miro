926692ce3608c7a3fd28e29a8d9b7742a8fa5cd2
Nick Nassar <nassar@pculture.org>
2007-07-23 18:08:11 +0000

    Fix for detecting OS X version
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/branches/Democracy-Player-0.9.8@4978 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 3a17bf54154c708265abb56ad3cb634d1f6e0adf

--- tv/platform/osx/frontend/QuicktimeRenderer.py
+++ tv/platform/osx/frontend/QuicktimeRenderer.py
@@ -50,7 +50,7 @@ class QuicktimeRenderer (app.VideoRenderer):
 
     def checkComponentCompatibility(self, name):
         if "Perian" in name or "AC3" in name or "A52" in name:
-            if platformutils.getMajorOSVersion <= 7:
+            if platformutils.getMajorOSVersion() <= 7:
                 return False
         return True
 
@@ -133,7 +133,7 @@ class QuicktimeRenderer (app.VideoRenderer):
         (qtmovie, error) = QTMovie.movieWithFile_error_(osfilename)
         if qtmovie is not None:
             movie_data["duration"] = int(movieDuration(qtmovie) * 1000)
-            if platformutils.getMajorOSVersion > 7:
+            if platformutils.getMajorOSVersion() > 7:
                 movie_data["screenshot"] = extractIcon(qtmovie, filename)
             del qtmovie
             return True
