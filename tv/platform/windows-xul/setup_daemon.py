a91a2a02b947b686caeec5ce7b4f0ac8a9c84349
Paul Swartz <pswartz@pculture.org>
2007-08-03 23:38:08 +0000

    get information from the main setup.py file
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@5060 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 4c611bdd93babf9e681f83a18d662bd65fff49a7

--- tv/platform/windows-xul/setup_daemon.py
+++ tv/platform/windows-xul/setup_daemon.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import setup as core_setup
 from distutils.core import setup
 from distutils.extension import Extension
 import py2exe
@@ -22,24 +23,9 @@ root = os.path.normpath(root)
 
 import util
 
-defaultBinaryKitRoot = os.path.join(os.path.dirname(sys.argv[0]), \
-				    '..', '..', '..', 'dtv-binary-kit')
-BINARY_KIT_ROOT = defaultBinaryKitRoot
-BOOST_ROOT = os.path.join(BINARY_KIT_ROOT, 'boost', 'win32')
-BOOST_LIB_PATH = os.path.join(BOOST_ROOT, 'lib')
-# Hack so we don't break things for people who haven't updated their binary kit
-if os.path.exists(os.path.join(BOOST_ROOT, 'include', 'boost-1_33_1')):
-    BOOST_LIB = os.path.join(BOOST_LIB_PATH, 'boost_python-vc71-mt-1_33_1.lib')
-    BOOST_INCLUDE_PATH = os.path.join(BOOST_ROOT, 'include', 'boost-1_33_1')
-    BOOST_RUNTIMES = [
-        os.path.join(BOOST_LIB_PATH, 'boost_python-vc71-mt-1_33_1.dll'),
-        ]
-else:
-    BOOST_LIB = os.path.join(BOOST_LIB_PATH, 'boost_python-vc71-mt-1_33.lib')
-    BOOST_INCLUDE_PATH = os.path.join(BOOST_ROOT, 'include', 'boost-1_33')
-    BOOST_RUNTIMES = [
-        os.path.join(BOOST_LIB_PATH, 'boost_python-vc71-mt-1_33.dll'),
-        ]
+BOOST_LIB = core_setup.BOOST_LIB
+BOOST_INCLUDE_PATH = core_setup.BOOST_INCLUDE_PATH
+BOOST_RUNTIMES = core_setup.BOOST_RUNTIMES
 
 ext_modules=[
     Extension("database", [os.path.join(root, 'portable', 'database.pyx')]),
