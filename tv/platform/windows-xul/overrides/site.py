f1649e5c9f7001a9e8dec5ec99da2a9e998bd0c6
Will Kahn-Greene <will.guaraldi@pculture.org>
2007-09-04 15:01:53 +0000

    Adding GPL headers to more of the windows platform code files.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@5288 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 8f371779643120ebbebec0de0ecfcc6517fd0436

--- tv/platform/windows-xul/overrides/site.py
+++ tv/platform/windows-xul/overrides/site.py
@@ -1,3 +1,20 @@
+# Miro - an RSS based video player application
+# Copyright (C) 2005-2007 Participatory Culture Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
 #import sys
 #class AutoflushingStream:
 #    def __init__(self, stream):
@@ -22,7 +39,7 @@ part left the other incidental initializations alone. One exception to
 the first part of that sentence is that the logic for dealing with
 .pth files has been left alone, just in case.
 
-In any event, this is the very first Python code that runs
+In any event, this is the very first Python code that runs.
 """
 
 import sys
--- tv/platform/windows-xul/xul/components/jsbridge.js
+++ tv/platform/windows-xul/xul/components/jsbridge.js
@@ -1,3 +1,22 @@
+/*
+# Miro - an RSS based video player application
+# Copyright (C) 2005-2007 Participatory Culture Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
 const JSBRIDGE_CONTRACTID = "@participatoryculture.org/dtv/jsbridge;1";
 const JSBRIDGE_CLASSID    = Components.ID("{421AA951-F53D-4499-B362-E432CAE920F4}");
 
