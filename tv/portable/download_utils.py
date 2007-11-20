pculture/miro

commit a7387a43255c870eb4d5bed92c0e08aeef21e09d

Lots of changes for our handling of file:// URLs.  I started this to fix #8936, but I found that on windows especially things needed a lot of work in a lot of places.  I think I fixed a few general areas:
- Create file URLs like this: file:///C:\foo\bar instead of only file:// at
  the start.

- Handle those file URLs when we parse them

- Always call platformutils.unmakeUrlSafe when we parse file URLs



git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@5434 bce20531-a64c-4166-8316-56baf4a01729

Original sha1: 833e0a87f213c9167e514a89e02a4a72071f6a69

tv/portable/download_utils.py
@@ -24,7 +24,7 @@ import filetypes
 import util
 
 from util import checkF, checkU, returnsFilename
-from platformutils import unicodeToFilename
+from platformutils import unicodeToFilename, unmakeURLSafe
 
 URIPattern = re.compile(r'^([^?]*/)?([^/?]*)/*(\?(.*))?$')
 
@@ -60,14 +60,14 @@ def parseURL(url):
         host = host[0:host.rfind(':')]
 
     if scheme == '' and util.chatter:
-        print "WARNING: %s has no scheme" % url
+        print "WARNING: %r has no scheme" % url
 
     if ':' in host:
         host, port = host.split(':')
         try:
             port = int(port)
         except:
-            print "DTV: parseURL: WARNING: invalid port for %s" % url
+            print "DTV: parseURL: WARNING: invalid port for %r" % url
             port = defaultPort(scheme)
     else:
         port = defaultPort(scheme)
@@ -75,14 +75,26 @@ def parseURL(url):
     host = host.lower()
     scheme = scheme.lower()
 
+    path = path.replace('|', ':') 
+    # Windows drive names are often specified as "C|\foo\bar"
+
     if path == '' or path[0] != '/':
         path = '/' + path
+    elif re.match(r'/[a-zA-Z]:', path):
+        # Fix "/C:/foo" paths
+        path = path[1:]
     fullPath = path
     if params:
         fullPath += ';%s' % params
     if query:
         fullPath += '?%s' % query
     return scheme, host, port, fullPath
+
+def getFileURLPath(url):
+    scheme, host, port, path = parseURL(url)
+    if scheme != 'file':
+        raise ValueError("%r is not a file URL" % url)
+    return unmakeURLSafe(path)
 
 # If a filename doesn't have an extension, this tries to find a suitable one
 # based on the HTTP content-type info and add it if one is available.

tv/portable/filetypes.py
@@ -16,7 +16,6 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 import os
-import urlparse
 
 VIDEO_EXTENSIONS = ['.mov', '.wmv', '.mp4', '.m4v', '.ogg', '.anx', '.mpg', '.avi', '.flv', '.mpeg', '.divx', '.xvid', '.rmvb', '.mkv', '.m2v', '.ogm']
 AUDIO_EXTENSIONS = ['.mp3', '.m4a', '.wma', '.mka']
@@ -103,9 +102,10 @@ def _hasVideoType(enclosure):
              enclosure['type'] == u"application/x-shockwave-flash"))
 
 def _hasVideoExtension(enclosure, key):
+    import download_utils
     if key in enclosure:
-        elems = urlparse.urlparse(enclosure[key])
-        return isAllowedFilename(elems[2])
+        elems = download_utils.parseURL(enclosure[key])
+        return isAllowedFilename(elems[3])
     return False
 
 def isFeedContentType(contentType):

tv/platform/windows-xul/resources.py
@@ -56,9 +56,10 @@ def path(relative_path):
     return abspath.replace("/", "\\")
 
 def url(relative_path):
-    return u"file://" + urllib.quote(path(relative_path).encode('utf_8'),safe="\\:~")
+    return absoluteUrl(path(relative_path))
 
 def absoluteUrl(absolute_path):
     """Like url, but without adding the resource directory.
     """
-    return u"file://" + urllib.quote(absolute_path.encode('utf_8'),safe="\\:~")
+    absolute_path = absolute_path.encode('utf_8')
+    return u"file:///" + urllib.quote(absolute_path, safe=":~\\")

tv/portable/app.py
@@ -51,6 +51,7 @@ import guide
 import idlenotifier 
 import eventloop
 import searchengines
+import download_utils
 
 import os
 import re
@@ -1369,8 +1370,8 @@ Are you sure you want to stop watching these %s directories?""") % len(feeds)
             if url == u"":
                 continue
             if url.startswith(u"file://"):
-                filename = url[len(u'file://'):]
-                filename = platformutils.unicodeToFilename(filename)
+                filename = download_utils.getFileURLPath(url)
+                filename = platformutils.osFilenameToFilenameType(filename)
                 eventloop.addIdle (singleclick.openFile,
                     "Open Dropped file", args=(filename,))
             elif url.startswith(u"http:") or url.startswith(u"https:"):
@@ -1576,7 +1577,7 @@ class TemplateDisplay(frontend.HTMLDisplay):
                 controller.setLastVisitedGuideURL(url)
                 return True
             if url.startswith(u'file://'):
-                path = url[len(u"file://"):]
+                path = download_utils.getFileURLPath(url)
                 return os.path.exists(path)
 
             # If we get here, this isn't a DTV URL. We should open it

tv/portable/dl_daemon/download.py
@@ -13,7 +13,7 @@ from copy import copy
 from BitTorrent.bencode import bdecode
 from clock import clock
 from download_utils import cleanFilename, nextFreeFilename, checkFilenameExtension, filterDirectoryName
-from download_utils import filenameFromURL
+from download_utils import filenameFromURL, getFileURLPath
 import eventloop
 import httpclient
 import datetime
@@ -815,8 +815,7 @@ class BTDownloader(BGDownloader):
     def getMetainfo(self):
         if self.metainfo is None:
             if self.url.startswith('file://'):
-                path = self.url[len('file://'):]
-                path = platformutils.unmakeURLSafe(path)
+                path = getFileURLPath(self.url)
                 try:
                     metainfoFile = open(path, 'rb')
                 except IOError:

tv/portable/downloader.py
@@ -24,7 +24,7 @@ import shutil
 
 from database import DDBObject, defaultDatabase
 from dl_daemon import daemon, command
-from download_utils import nextFreeFilename, parseURL, filterDirectoryName
+from download_utils import nextFreeFilename, getFileURLPath, filterDirectoryName
 from util import getTorrentInfoHash, returnsUnicode, checkU, returnsFilename, unicodify, checkF
 from platformutils import FilenameType
 import app
@@ -633,11 +633,7 @@ def getDownloader(item):
     if not channelName:
         channelName = None
     if url.startswith(u'file://'):
-        scheme, host, port, path = parseURL(url)
-        path = platformutils.unmakeURLSafe (path)
-        if re.match(r'/[a-zA-Z]:', path): 
-            # fix windows pathnames (/C:/blah/blah/blah)
-            path = path[1:]
+        path = getFileURLPath(url)
         try:
             getTorrentInfoHash(path)
         except ValueError:

tv/portable/httpclient.py
@@ -40,7 +40,7 @@ from clock import clock
 import httpauth
 import config
 import prefs
-from download_utils import URIPattern, cleanFilename, parseURL, defaultPort
+from download_utils import URIPattern, cleanFilename, parseURL, defaultPort, getFileURLPath
 from xhtmltools import URLEncodeDict, multipartEncode
 import eventloop
 import util
@@ -1825,7 +1825,7 @@ def grabURL(url, callback, errback, headerCallback=None,
     if cookies == None:
         cookies = {}
     if url.startswith("file://"):
-        callback({"body":file(url[7:]).read(),
+        callback({"body":file(getFileURLPath(url)).read(),
                       "updated-url":url,
                       "redirected-url":url,
                       "content-type": defaultMimeType,

tv/portable/iconcache.py
@@ -21,7 +21,7 @@ import threading
 import httpclient
 from fasttypes import LinkedList
 from eventloop import asIdle, addIdle, addTimeout
-from download_utils import nextFreeFilename
+from download_utils import nextFreeFilename, getFileURLPath
 from util import unicodify, call_command
 from platformutils import unicodeToFilename
 import config
@@ -313,7 +313,7 @@ class IconCache:
     def getFilename(self):
         self.dbItem.confirmDBThread()
         if self.url and self.url.startswith (u"file://"):
-            return self.url[len(u"file://"):]
+            return getFileURLPath(self.url)
         elif self.url and self.url.startswith (u"/"):
             return self.url
         else:

commit 9b2a9076444f209043b594fe4fdbd4722fd46f03

r5287 was broken by r5434, this change merges both revisions and therefore allows movies at URLs containing query parameters to be correctly recognized again.
git-svn-id: https://svn.participatoryculture.org/svn/dtv/branches/Democracy-Player-1.0@5776 bce20531-a64c-4166-8316-56baf4a01729

Original sha1: 4c38c84e77bc72eb7095c5f4c928bbe2fdcd1441

tv/portable/download_utils.py
@@ -55,7 +55,7 @@ def defaultPort(scheme):
             print "WARNING: Assuming port 80 for scheme: %s" % scheme
         return 80
 
-def parseURL(url):
+def parseURL(url, split_path=False):
     url = fixFileURLS(url)
     (scheme, host, path, params, query, fragment) = util.unicodify(list(urlparse(url)))
     # Filter invalid URLs with duplicated ports (http://foo.bar:123:123/baz)
@@ -88,11 +88,14 @@ def parseURL(url):
         # Fix "/C:/foo" paths
         path = path[1:]
     fullPath = path
-    if params:
-        fullPath += ';%s' % params
-    if query:
-        fullPath += '?%s' % query
-    return scheme, host, port, fullPath
+    if split_path:
+        return scheme, host, port, fullPath, params, query
+    else:
+        if params:
+            fullPath += ';%s' % params
+        if query:
+            fullPath += '?%s' % query
+        return scheme, host, port, fullPath
 
 def getFileURLPath(url):
     scheme, host, port, path = parseURL(url)

tv/portable/filetypes.py
@@ -104,7 +104,7 @@ def _hasVideoType(enclosure):
 def _hasVideoExtension(enclosure, key):
     import download_utils
     if key in enclosure:
-        elems = download_utils.parseURL(enclosure[key])
+        elems = download_utils.parseURL(enclosure[key], split_path=True)
         return isAllowedFilename(elems[3])
     return False
 