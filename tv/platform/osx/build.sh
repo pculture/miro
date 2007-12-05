15dfba172015e95905dc98c034bc8a3a27d42c2e
Luc Heinrich <luc@pculture.org>
2007-12-05 14:29:02 +0000

    Strip what can be stripped from the built Miro application.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@5811 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: c2d6d90adf801af536b55db70e31fecd7a47861c

--- tv/platform/osx/build.sh
+++ tv/platform/osx/build.sh
@@ -9,4 +9,9 @@ PYTHON=$PYTHON_ROOT/bin/python$PYTHON_VERSION
 
 $PYTHON setup.py py2app -O2 --dist-dir . --force-update $@
 
+find Miro.app -name ".DS_Store" -exec rm {} \;
+find Miro.app -name "info.nib" -exec rm {} \;
+find Miro.app -name "classes.nib" -exec rm {} \;
+find Miro.app -name "*.so" -exec strip -S {} \;
+
 echo Done.
