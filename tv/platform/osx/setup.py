e2f6fbb739764605a3171b76404e5926aca4811a
Luc Heinrich <luc@pculture.org>
2007-12-12 13:13:59 +0000

    When building our python extensions on OS X, we now explicitely specify path to linked libraries (python and boost) for some of them to avoid link errors and warnings.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@5829 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 48268deb9c085d48b10d24e4afb57ab6891a6ae2

--- tv/platform/osx/setup.py
+++ tv/platform/osx/setup.py
@@ -451,14 +451,18 @@ class mypy2app (py2app):
             os.system("ls -la \"%s\"" % imgPath)
 
 # =============================================================================
-# Fix flags for universal binary extensions
+# We're going to explicitely pass these when linking our extensions to:
+# - avoid a linker error if we would specify the libraries using -L and -l
+#   flags since we also specify a isysroot.
+# - avoid the linker warnings which would occur if we would remove the 
+#   isysroot flag.
 # =============================================================================
 
-from distutils import sysconfig
-for syscnf in ["LDFLAGS", "LDSHARED", "BLDSHARED"]:
-	val = sysconfig.get_config_vars()[syscnf]
-	val = val.replace("-isysroot /Developer/SDKs/MacOSX10.4u.sdk", "")
-	sysconfig.get_config_vars()[syscnf] = ' '.join(val.split())
+PYTHON_LIB = os.path.join(SANDBOX_DIR, "Library", "Frameworks", "Python.framework", "Versions", "Current", "Python")
+BOOST_PYTHON_LIB = os.path.join(SANDBOX_DIR, "lib", "libboost_python-%s.dylib" % BOOST_VERSION)
+BOOST_FILESYSTEM_LIB = os.path.join(SANDBOX_DIR, "lib", 'libboost_filesystem-%s.dylib' % BOOST_VERSION)
+BOOST_DATETIME_LIB = os.path.join(SANDBOX_DIR, "lib", 'libboost_date_time-%s.dylib' % BOOST_VERSION)
+BOOST_THREAD_LIB = os.path.join(SANDBOX_DIR, "lib", 'libboost_thread-%s.dylib' % BOOST_VERSION)
 
 # =============================================================================
 # Define the native extensions
@@ -500,13 +504,11 @@ sorts_ext = Extension("sorts", sources=sorts_src)
 
 fasttypes_src = glob(os.path.join(ROOT_DIR, 'portable', 'fasttypes.cpp'))
 fasttypes_inc_dirs = [BOOST_INCLUDE_DIR]
-fasttypes_lib_dirs = [BOOST_LIB_DIR]
-fasttypes_libs = ['boost_python-%s' % BOOST_VERSION]
+fasttypes_extras = [PYTHON_LIB, BOOST_PYTHON_LIB]
 
 fasttypes_ext = Extension("fasttypes", sources=fasttypes_src, 
                                        include_dirs=fasttypes_inc_dirs, 
-                                       library_dirs=fasttypes_lib_dirs, 
-                                       libraries=fasttypes_libs)
+                                       extra_objects=fasttypes_extras)
 
 # -----------------------------------------------------------------------------
 
@@ -524,13 +526,14 @@ libtorrent_inc_dirs = [BOOST_INCLUDE_DIR,
                        os.path.join(PORTABLE_DIR, 'libtorrent', 'include'),
                        os.path.join(PORTABLE_DIR, 'libtorrent', 'include', 'libtorrent')]
 libtorrent_lib_dirs = [BOOST_LIB_DIR]
-libtorrent_libs = ['boost_python-%s' % BOOST_VERSION, 
-                   'boost_filesystem-%s' % BOOST_VERSION, 
-                   'boost_date_time-%s' % BOOST_VERSION, 
-                   'boost_thread-%s' % BOOST_VERSION, 
-                   'z', 
+libtorrent_libs = ['z', 
                    'pthread', 
                    'ssl']
+libtorrent_extras = [PYTHON_LIB,
+                     BOOST_PYTHON_LIB,
+                     BOOST_FILESYSTEM_LIB,
+                     BOOST_DATETIME_LIB,
+                     BOOST_THREAD_LIB]
 libtorrent_compil_args = ["-DHAVE_INCLUDE_LIBTORRENT_ASIO____ASIO_HPP=1", 
                           "-DHAVE_INCLUDE_LIBTORRENT_ASIO_SSL_STREAM_HPP=1", 
                           "-DHAVE_INCLUDE_LIBTORRENT_ASIO_IP_TCP_HPP=1", 
@@ -541,8 +544,8 @@ libtorrent_compil_args = ["-DHAVE_INCLUDE_LIBTORRENT_ASIO____ASIO_HPP=1",
 
 libtorrent_ext = Extension("libtorrent", sources=libtorrent_src, 
                                          include_dirs=libtorrent_inc_dirs,
-                                         library_dirs=libtorrent_lib_dirs, 
                                          libraries=libtorrent_libs, 
+                                         extra_objects=libtorrent_extras,
                                          extra_compile_args=libtorrent_compil_args)
 
 # =============================================================================
