8d7c7404bf248ae5e6f4b90c6542e32415fbbd5a
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-02-08 16:23:39 +0000

    Updated all file license headers adding an exception for openssl linking.  Bug #9523 . I also went through and updated the copyright years for all files that hadn't been updated yet.  I also fixed some Democracy -> Miro issues in the comments.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@6205 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 4cd5c2f2711519d2c17216ccd9ae4c4b1e6eb003

--- tv/platform/windows-xul/setup_daemon.py
+++ tv/platform/windows-xul/setup_daemon.py
@@ -1,5 +1,5 @@
 # Miro - an RSS based video player application
-# Copyright (C) 2005-2007 Participatory Culture Foundation
+# Copyright (C) 2005-2008 Participatory Culture Foundation
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,6 +14,17 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# In addition, as a special exception, the copyright holders give
+# permission to link the code of portions of this program with the OpenSSL
+# library.
+#
+# You must obey the GNU General Public License in all respects for all of
+# the code used other than OpenSSL. If you modify file(s) with this
+# exception, you may extend this exception to your version of the file(s),
+# but you are not obligated to do so. If you do not wish to do so, delete
+# this exception statement from your version. If you delete this exception
+# statement from all source files in the program, then also delete it here.
 
 import os
 import sys
