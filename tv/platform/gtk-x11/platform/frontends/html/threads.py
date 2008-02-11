5ba7db3181feddebf4acca3590c90536eff9cc00
Ben Dean-Kawamura <ben@pculture.org>
2008-02-11 21:02:07 +0000

    Fixed the name of threads.py in the docstring.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@6225 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: cfda242a0f144fec4e93918829a684747c5488f3

--- tv/platform/gtk-x11/platform/frontends/html/threads.py
+++ tv/platform/gtk-x11/platform/frontends/html/threads.py
@@ -26,7 +26,7 @@
 # this exception statement from your version. If you delete this exception
 # statement from all source files in the program, then also delete it here.
 
-"""miro.platform.frontends.html.threading -- Handles calling a function in the
+"""miro.platform.frontends.html.threads -- Handles calling a function in the
 main thread.
 """
 
