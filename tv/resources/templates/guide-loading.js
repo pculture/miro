20866fceb42f2bfa80b2cdb0a5eb5384fa1d46ab
Luc Heinrich <luc@pculture.org>
2008-03-10 15:57:33 +0000

    Fixed guide loading feedback on OS X (#9660).
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@6468 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: dcf7d0922541e7704e931988c0fc550dd0207c5b

---
+++ tv/resources/templates/guide-loading.js
@@ -0,0 +1,38 @@
+<script type="text/javascript">
+    function navBarLoaded() 
+    {
+        var metas = document.getElementsByTagName('meta');
+        var i;
+        for (i=0; i < metas.length; i++)
+        {
+            alert(metas[i]);
+            if (metas[i].getAttribute('name') == "guideURL")
+            {
+                miro_guide_frame.location = metas[i].getAttribute('content');
+                break;
+            }
+        }
+    }
+
+    function guideLoaded() 
+    {
+        if (netscape)
+        {
+            netscape.security.PrivilegeManager.enablePrivilege('UniversalBrowserRead');
+            netscape.security.PrivilegeManager.enablePrivilege('UniversalBrowserWrite');
+        }
+        
+        miro_navigation_frame.document.getElementById('load-indicator').style.display = 'none';
+    }
+
+    function guideUnloaded() 
+    {
+        if (netscape)
+        {
+            netscape.security.PrivilegeManager.enablePrivilege('UniversalBrowserRead');
+            netscape.security.PrivilegeManager.enablePrivilege('UniversalBrowserWrite');
+        }
+        
+        miro_navigation_frame.document.getElementById('load-indicator').style.display = 'block';    
+    }
+</script>
