ada617ea82b5fcb56607c6f952562185401c6edc
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-03-14 21:54:08 +0000

    Updating copyright in Windows About dialog.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@6542 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 6ea13cd5eb0833af28512c6ea07ca32e6ef3c87c

--- tv/platform/windows-xul/xul/chrome/locale/en-US/about.dtd.template.h
+++ tv/platform/windows-xul/xul/chrome/locale/en-US/about.dtd.template.h
@@ -1,4 +1,4 @@
 # 2 "../../platform/windows-xul/xul/chrome/locale/en-US/about.dtd.template"
-char *s = N_("$longAppName version $appVersion ($appRevision); copyright (c) 2005-2007 $publisher.  $longAppName and the $longAppName logo are trademarks of $publisher.");
+char *s = N_("$longAppName version $appVersion ($appRevision); copyright (c) 2005-2008 $publisher.  $longAppName and the $longAppName logo are trademarks of $publisher.");
 # 1 "../../platform/windows-xul/xul/chrome/locale/en-US/about.dtd.template"
 char *s = N_("About $longAppName");
