01acf29ab6b96bf8a2f43a5e86a2bd06d31ce6f3
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-03-19 22:53:08 +0000

    Committing .po files from Launchpad circa 3/19/2008.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@6592 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: a943e766c455ee56caa436b46eeae874194e714c

--- tv/resources/locale/build-catalogs.py
+++ tv/resources/locale/build-catalogs.py
@@ -90,7 +90,9 @@ langs = (
 for lang in langs:
     try:
         os.makedirs ("../../platform/osx/Resources/%s.lproj" % (lang[1],))
-    except:
+    except Exception, e:
+        print "Exception thrown making %s lproj dir: %s" % (lang, repr(e))
         pass
+
     # The perl statement removes po entries that have plurals in them since xgettext gives an error on those when writing a stringtable.
     os.system ( "cat %s.po | perl -e '$/=\"\"; while (<>) {print if !/msgid_plural/;}' | xgettext --force -o ../../platform/osx/Resources/%s.lproj/translated.strings --stringtable-output - -L PO" % (lang[0], lang[1]))
