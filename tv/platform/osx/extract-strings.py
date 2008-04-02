9bb3d4b50283457e62991cab65eb119e8a157a3a
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-04-02 19:37:45 +0000

    Merged revisions 6673 via svnmerge from https://svn.participatoryculture.org/svn/dtv/trunk
    ........
      r6673 | wguaraldi | 2008-04-02 14:43:14 -0400 (Wed, 02 Apr 2008) | 3 lines
      
      Added print statements to extract-strings.py so it's easier to see what it's
      doing.
    ........
    
    
    
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/branches/Miro-1.2@6674 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 523b2c0175035a36c4c54bed2e41e3aebf00e77a

--- tv/platform/osx/extract-strings.py
+++ tv/platform/osx/extract-strings.py
@@ -30,5 +30,7 @@ from glob import glob
 import os
 for nib in glob("Resources/English.lproj/*.nib"):
     name = os.path.basename (nib)[:-4]
+    print "Extracting %s..." % name
     os.system ("nibtool Resources/English.lproj/%s.nib -L -8 > Resources/English.lproj/%s.strings" % (name, name))
     
+print "Done."
