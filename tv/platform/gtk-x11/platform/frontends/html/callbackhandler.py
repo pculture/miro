c3f2fb71796555e201203c82d39ff8eb71f43923
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-05-02 20:03:34 +0000

    Bug #1966.  This adds the "daily" option and additional text to make it clear that you can change the channel checking frequency on a channel-by-channel basis.  Implemented in gtkx11.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@6842 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: e9ab3973ac7bbe8f1e8e844a3924771641beb2c0

--- tv/platform/gtk-x11/platform/frontends/html/callbackhandler.py
+++ tv/platform/gtk-x11/platform/frontends/html/callbackhandler.py
@@ -564,7 +564,7 @@ class CallbackHandler(object):
         AttachFloat   (dialog, widgetTree['spinbutton-padding'], prefs.PRESERVE_X_GB_FREE)
         AttachBoolean (dialog, widgetTree['checkbutton-upload-ratio'], prefs.LIMIT_UPLOAD_RATIO, widgetTree['spinbutton-upload-ratio'])
         AttachFloat   (dialog, widgetTree['spinbutton-upload-ratio'], prefs.UPLOAD_RATIO)
-        AttachCombo   (dialog, widgetTree['combobox-poll'], prefs.CHECK_CHANNELS_EVERY_X_MN, (30, 60, -1))
+        AttachCombo   (dialog, widgetTree['combobox-poll'], prefs.CHECK_CHANNELS_EVERY_X_MN, (30, 60, 1440, -1))
         AttachCombo   (dialog, widgetTree['combobox-auto-setting'], prefs.CHANNEL_AUTO_DEFAULT, ("new", "all", "off"))
         AttachCombo   (dialog, widgetTree['combobox-expiration'], prefs.EXPIRE_AFTER_X_DAYS, (1, 3, 6, 10, 30, -1))
         AttachBooleanRadio (dialog, widgetTree['radiobutton-playback-one'], widgetTree['radiobutton-playback-all'], prefs.SINGLE_VIDEO_PLAYBACK_MODE)
