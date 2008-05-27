pculture/miro

commit ae2a98b810e5d6cca4676f7068f245f1fc0ce78b

Added sidebar section for 'Sites', per #9864.
git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@7049 bce20531-a64c-4166-8316-56baf4a01729

Original sha1: 9f00ea4756a7103a691aaa2dc45515b1e8611f22

tv/portable/tabs.py
@@ -112,7 +112,10 @@ class Tab:
         self.obj = obj
 
         if obj.__class__ == guide.ChannelGuide:
-            self.type = 'guide'
+            if obj.getDefault():
+                self.type = 'guide'
+            else:
+                self.type = 'site'
         elif obj.__class__ == StaticTab: 
             self.type = 'statictab'
         elif obj.__class__ in (feed.Feed, folder.ChannelFolder): 
@@ -136,6 +139,8 @@ class Tab:
                 return 'playlistfolder'
             else:
                 return 'playlist'
+        elif self.type == 'site':
+            return 'site'
         else:
             return ''
 
@@ -185,7 +190,11 @@ class Tab:
 
     def isGuide(self):
         """True if this Tab represents a Channel Guide."""
-        return isinstance(self.obj, guide.ChannelGuide)
+        return isinstance(self.obj, guide.ChannelGuide) and self.obj.getDefault()
+
+    def isSite(self):
+        """True is this Tab represents a Web Site"""
+        return isinstance(self.obj, guide.ChannelGuide) and not self.obj.getDefault()
 
     def isPlaylist(self):
         """True if this Tab represents a Playlist."""
@@ -197,7 +206,7 @@ class Tab:
 
     def feedURL(self):
         """If this Tab represents a Feed or a Guide, the URL. Otherwise None."""
-        if self.isFeed() or self.isGuide():
+        if self.isFeed() or self.isGuide() or self.isSite():
             return self.obj.getURL()
         else:
             return None
@@ -265,7 +274,9 @@ class TabOrder(database.DDBObject, signals.SignalEmitter):
 
     def _initRestore(self):
         signals.SignalEmitter.__init__(self, 'tab-added')
-        if self.type == u'channel':
+        if self.type == u'site':
+            self.tabView = views.siteTabs
+        elif self.type == u'channel':
             self.tabView = views.feedTabs
         elif self.type == u'playlist':
             self.tabView = views.playlistTabs
@@ -351,6 +362,8 @@ def tabIterator():
         yield tab
     for tab in views.staticTabs:
         yield tab
+    for tab in getSingletonDDBObject(views.siteTabOrder).getView():
+        yield tab
     for tab in getSingletonDDBObject(views.channelTabOrder).getView():
         yield tab
     for tab in getSingletonDDBObject(views.playlistTabOrder).getView():
@@ -361,6 +374,8 @@ def getViewForTab(tab):
         return views.guideTabs
     elif tab.type == 'statictab':
         return views.staticTabs
+    if tab.type == 'site':
+        return getSingletonDDBObject(views.siteTabOrder).getView()
     elif tab.type == 'feed':
         return getSingletonDDBObject(views.channelTabOrder).getView()
     elif tab.type == 'playlist':

tv/resources/templates/tablist
@@ -15,6 +15,7 @@
     <t:execOnLoad><![CDATA[
 guideTabs = views.guideTabs
 staticTabs = views.staticTabs
+siteTabs = kargs['siteTabOrder'].getView()
 feedTabs = kargs['channelTabOrder'].getView()
 playlistTabs = kargs['playlistTabOrder'].getView()
 ]]>
@@ -23,91 +24,113 @@ playlistTabs = kargs['playlistTabOrder'].getView()
 <body>
 
 <div id="sidebar">
-  <div class="tab-list-drop-target"
+
+    <div class="tab-list-drop-target"
+         dragdesttype="channel:channelfolder"
+         dragdestdata="channel-START"
+         drageffectchannel="move" 
+	     drageffectchannelfolder="move" >
+        <ul class="tab-list first">
+	        <li t:repeatForView="guideTabs" 
+                t:repeatTemplate="guidetab"
+	            class="***this.getState()***"
+	            selectID="***this.objID()***"
+	            selectViewName="guideTabs"
+	            selectArea='tablist' 
+	            onmousedown="return handleContextMenuSelect(event);" 
+	            onclick="return handleSelect(event);" />
+	        <li t:repeatForView="staticTabs" 
+                t:repeatTemplate="statictab"
+	            class="***this.getState()***"
+	            selectID="***this.objID()***" selectViewName="staticTabs"
+	            selectArea='tablist' 
+	            onclick="return handleSelect(event);" />
+        </ul>
+        <div class="dnd-reorder-indicator circle" />
+        <div class="dnd-reorder-indicator line" />
+    </div>
+
+     <div class="tab-list-drop-target"
           dragdesttype="channel:channelfolder"
           dragdestdata="channel-START"
-          drageffectchannel="move" 
-	  drageffectchannelfolder="move" >
-    <ul class="tab-list first">
-	<li t:repeatForView="guideTabs" 
-	  class="***this.getState()***"
-	  selectID="***this.objID()***" selectViewName="guideTabs"
-	  selectArea='tablist' 
-	  onmousedown="return handleContextMenuSelect(event);" 
-	  onclick="return handleSelect(event);" >
-	      <t:includeTemplate filename="guidetab" />
-	</li>
-	<li t:repeatForView="staticTabs" 
-	  class="***this.getState()***"
-	  selectID="***this.objID()***" selectViewName="staticTabs"
-	  selectArea='tablist' 
-	  onclick="return handleSelect(event);" >
-	    <t:includeTemplate filename="statictab" />
-	</li>
-    </ul>
-    <div class="dnd-reorder-indicator circle" />
-    <div class="dnd-reorder-indicator line" />
-  </div>
-  <div class="tab-list-drop-target"
-	dragdesttype="playlist:playlistfolder"
-	dragdestdata="playlist-START"
-	drageffectplaylist="move" 
-	drageffectplaylistfolder="move" >
-  <h2>CHANNELS</h2>
-    <ul class="tab-list second">
-      <li t:repeatForView="feedTabs" 
-	class="***this.getState()***"
-	selectID="***this.objID()***" selectViewName="feedTabs"
-	selectArea='tablist' 
-	onmousedown="return handleContextMenuSelect(event);" 
-        onclick="return handleSelect(event)">
-        <div id="tab-***this.objID()***">
-	  <div t:showIf="this.isFeed()">
-	      <t:includeTemplate filename="feedtab" />
-	    </div>
-	  <div t:showIf="this.isChannelFolder()">
-	      <t:includeTemplate filename="channelfoldertab" />
-	  </div>
-        </div>
-      </li>
-    </ul>
-    <div class="dnd-reorder-indicator circle" />
-    <div class="dnd-reorder-indicator line" />
-  </div>
-  <div class="tab-list-drop-target"
-	dragdesttype="channel:channelfolder"
-	dragdestdata="channel-END"
-	drageffectchannel="move" 
-	drageffectchannelfolder="move" >
-	<div class="dnd-reorder-indicator show-on-top circle" />
-	<div class="dnd-reorder-indicator show-on-top line" />
-      <h2>PLAYLISTS</h2>
-      <ul class="tab-list third">
-      <li t:repeatForView="playlistTabs" 
-	class="***this.getState()***"
-	selectID="***this.objID()***" selectViewName="playlistTabs"
-	selectArea='tablist' 
-	onmousedown="return handleContextMenuSelect(event);" 
-        onclick="return handleSelect(event);">
-        <div id="tab-***this.objID()***">
-	  <div t:showIf="this.isPlaylist()">
-	      <t:includeTemplate filename="playlisttab" />
-	  </div>
-	  <div t:showIf="this.isPlaylistFolder()">
-              <t:includeTemplate filename="playlistfoldertab" />
-	  </div>
-        </div>
-      </li>
-    </ul>
-  </div>
-  <div class="tab-list-drop-target"
-      dragdesttype="playlist:playlistfolder"
-      dragdestdata="playlist-END"
-      drageffectplaylist="move" 
-      drageffectplaylistfolder="move" >
-      <div class="dnd-reorder-indicator show-on-top circle" />
-      <div class="dnd-reorder-indicator show-on-top line" />
-    </div>
-</div>
+          drageffectchannel="move">
+         <h2>SITES</h2>
+         <ul class="tab-list second">
+             <li t:repeatForView="siteTabs" 
+                 t:repeatTemplate="sitetab"
+                 class="***this.getState()***"
+                 selectID="***this.objID()***"
+                 selectViewName="siteTabs"
+                 selectArea='tablist' 
+                 onmousedown="return handleContextMenuSelect(event);" 
+                 onclick="return handleSelect(event)" />
+         </ul>
+         <div class="dnd-reorder-indicator circle" />
+         <div class="dnd-reorder-indicator line" />
+     </div>
+  
+     <div class="tab-list-drop-target"
+          dragdesttype="playlist:playlistfolder"
+          dragdestdata="playlist-START"
+          drageffectplaylist="move" 
+          drageffectplaylistfolder="move" >
+         <h2>CHANNELS</h2>
+         <ul class="tab-list second">
+             <li t:repeatForView="feedTabs" 
+                 class="***this.getState()***"
+                 selectID="***this.objID()***" selectViewName="feedTabs"
+                 selectArea='tablist' 
+                 onmousedown="return handleContextMenuSelect(event);" 
+                 onclick="return handleSelect(event)">
+                 <div id="tab-***this.objID()***">
+                     <div t:showIf="this.isFeed()">
+                         <t:includeTemplate filename="feedtab" />
+                     </div>
+                     <div t:showIf="this.isChannelFolder()">
+                         <t:includeTemplate filename="channelfoldertab" />
+                     </div>
+                 </div>
+             </li>
+         </ul>
+         <div class="dnd-reorder-indicator circle" />
+         <div class="dnd-reorder-indicator line" />
+     </div>
+  
+     <div class="tab-list-drop-target"
+    	  dragdesttype="channel:channelfolder"
+    	  dragdestdata="channel-END"
+    	  drageffectchannel="move" 
+    	  drageffectchannelfolder="move" >
+          <div class="dnd-reorder-indicator show-on-top circle" />
+          <div class="dnd-reorder-indicator show-on-top line" />
+          <h2>PLAYLISTS</h2>
+          <ul class="tab-list third">
+              <li t:repeatForView="playlistTabs" 
+                  class="***this.getState()***"
+                  selectID="***this.objID()***" selectViewName="playlistTabs"
+                  selectArea='tablist' 
+                  onmousedown="return handleContextMenuSelect(event);" 
+                  onclick="return handleSelect(event);">
+                  <div id="tab-***this.objID()***">
+                      <div t:showIf="this.isPlaylist()">
+                          <t:includeTemplate filename="playlisttab" />
+                      </div>
+                      <div t:showIf="this.isPlaylistFolder()">
+                          <t:includeTemplate filename="playlistfoldertab" />
+                      </div>
+                  </div>
+              </li>
+          </ul>
+      </div>
+  
+      <div class="tab-list-drop-target"
+           dragdesttype="playlist:playlistfolder"
+           dragdestdata="playlist-END"
+           drageffectplaylist="move" 
+           drageffectplaylistfolder="move" >
+           <div class="dnd-reorder-indicator show-on-top circle" />
+           <div class="dnd-reorder-indicator show-on-top line" />
+       </div>
+   </div>
 </body>
 </html>

tv/portable/controller.py
@@ -190,6 +190,16 @@ class Controller:
         for f in views.feeds:
             f.update()
 
+    def renameGuide(self, guide):
+        if guide.getDefault():
+            logging.warning ("attempt to rename default guide")
+            return
+        def callback(dialog):
+            if dialog.choice == dialogs.BUTTON_OK:
+                guide.setTitle(dialog.value)
+        dialogs.TextEntryDialog(_('Rename Site'), _('Enter a new name for the Web Site %s') % guide.getURL(), 
+                dialogs.BUTTON_OK, dialogs.BUTTON_CANCEL).run(callback)
+
     def removeGuide(self, guide):
         if guide.getDefault():
             logging.warning ("attempt to remove default guide")

tv/portable/folder.py
@@ -212,8 +212,7 @@ class PlaylistFolder(FolderBase, playlist.PlaylistMixin):
     def renameTitle(self):
         return _("Rename Playlist Folder")
     def renameText(self):
-        return _("Enter a new name for the playlist folder %s" % 
-                self.getTitle())
+        return _("Enter a new name for the playlist folder %s") % self.getTitle()
     def getTabOrder(self):
         return util.getSingletonDDBObject(views.playlistTabOrder)
     def getChildrenView(self):

tv/portable/frontends/html/contextmenu.py
@@ -196,8 +196,8 @@ def makeGuideContextMenu(guideObj, templateName, view):
             _('Copy URL to clipboard')),
     ]
     if not guideObj.getDefault():
-        i = (lambda: app.controller.removeGuide(guideObj), _('Remove'))
-        menuItems.append(i)
+        menuItems.append((lambda: app.controller.renameGuide(guideObj), _('Rename')))
+        menuItems.append((lambda: app.controller.removeGuide(guideObj), _('Remove')))
     return makeMenu(menuItems)
 
 def makeItemContextMenu(itemObj, templateName, view):

tv/portable/frontends/html/main.py
@@ -203,12 +203,19 @@ class HTMLApplication:
 
         util.print_mem_usage("Post-selection memory check")
 
+        siteTabOrder = util.getSingletonDDBObject(views.siteTabOrder)
+        siteTabOrder.connect('tab-added', self.makeLastTabVisible)
+
         channelTabOrder = util.getSingletonDDBObject(views.channelTabOrder)
-        playlistTabOrder = util.getSingletonDDBObject(views.playlistTabOrder)
         channelTabOrder.connect('tab-added', self.makeLastTabVisible)
+
+        playlistTabOrder = util.getSingletonDDBObject(views.playlistTabOrder)
         playlistTabOrder.connect('tab-added', self.makeLastTabVisible)
+
         self.tabDisplay = templatedisplay.TemplateDisplay('tablist',
-                'default', playlistTabOrder=playlistTabOrder,
+                'default', 
+                siteTabOrder=siteTabOrder,
+                playlistTabOrder=playlistTabOrder,
                 channelTabOrder=channelTabOrder)
         self.frame.selectDisplay(self.tabDisplay, self.frame.channelsDisplay)
 

tv/portable/guide.py
@@ -145,6 +145,9 @@ class ChannelGuide(DDBObject):
     def getDefault(self):
         return self.url == config.get(prefs.CHANNEL_GUIDE_URL)
 
+    def getFolder(self):
+        return None
+
     # For the tabs
     @returnsUnicode
     def getTitle(self):
@@ -152,6 +155,11 @@ class ChannelGuide(DDBObject):
             return self.title
         else:
             return self.getURL()
+
+    def setTitle(self, title):
+        self.confirmDBThread()
+        self.title = title
+        self.signalChange(needsSave=True)
 
     def guideDownloaded(self, info):
         self.updated_url = unicode(info["updated-url"])
@@ -183,7 +191,9 @@ class ChannelGuide(DDBObject):
         #    path = self.iconCache.getFilename()
         #    return resources.absoluteUrl(path)
         #else:
-        return resources.url("images/icon-guide_small.png")
+        if self.getDefault():
+            return resources.url("images/icon-guide_small.png")
+        return resources.url("images/site-icon.png")
 
     def getThumbnailURL(self):
         if self.favicon:

tv/portable/menubar.py
@@ -184,9 +184,9 @@ EditItems = [
 
 ChannelItems = [
     MenuItem(_("Add _Channel"), "NewChannel", (Key("n",MOD),)),
+    MenuItem(_("Add Site..."), "NewGuide", ()),
     MenuItem(_("New Searc_h Channel..."), "NewSearchChannel", ()),
     MenuItem(_("New _Folder..."), "NewChannelFolder", (Key("n",MOD,SHIFT),)),
-    MenuItem(_("Add Channel _Guide..."), "NewGuide", ()),
     Separator(),
     MenuItem(_("Re_name Channel..."), "RenameChannel", (), enabled=False),
     MenuItem(_("_Remove Channel..."), "RemoveChannels", (Key(DELETE),Key(BKSPACE, MOD)), enabled=False,

commit 23a7fcfaa669f34209880d06f37bca03e0b6cc6b

Fixed bugs introduced in r7049.
git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@7051 bce20531-a64c-4166-8316-56baf4a01729

Original sha1: 2f268d4eee5f63b3d2077a41ba535bc229b5f619

tv/portable/tabs.py
@@ -374,7 +374,7 @@ def getViewForTab(tab):
         return views.guideTabs
     elif tab.type == 'statictab':
         return views.staticTabs
-    if tab.type == 'site':
+    elif tab.type == 'site':
         return getSingletonDDBObject(views.siteTabOrder).getView()
     elif tab.type == 'feed':
         return getSingletonDDBObject(views.channelTabOrder).getView()

tv/resources/templates/tablist
@@ -32,19 +32,21 @@ playlistTabs = kargs['playlistTabOrder'].getView()
 	     drageffectchannelfolder="move" >
         <ul class="tab-list first">
 	        <li t:repeatForView="guideTabs" 
-                t:repeatTemplate="guidetab"
 	            class="***this.getState()***"
 	            selectID="***this.objID()***"
 	            selectViewName="guideTabs"
 	            selectArea='tablist' 
 	            onmousedown="return handleContextMenuSelect(event);" 
-	            onclick="return handleSelect(event);" />
+	            onclick="return handleSelect(event);">
+	            <t:includeTemplate filename="guidetab" />
+	        </li>
 	        <li t:repeatForView="staticTabs" 
-                t:repeatTemplate="statictab"
 	            class="***this.getState()***"
 	            selectID="***this.objID()***" selectViewName="staticTabs"
 	            selectArea='tablist' 
-	            onclick="return handleSelect(event);" />
+	            onclick="return handleSelect(event);">
+	            <t:includeTemplate filename="statictab" />
+	        </li>
         </ul>
         <div class="dnd-reorder-indicator circle" />
         <div class="dnd-reorder-indicator line" />
@@ -57,13 +59,14 @@ playlistTabs = kargs['playlistTabOrder'].getView()
          <h2>SITES</h2>
          <ul class="tab-list second">
              <li t:repeatForView="siteTabs" 
-                 t:repeatTemplate="sitetab"
                  class="***this.getState()***"
                  selectID="***this.objID()***"
                  selectViewName="siteTabs"
                  selectArea='tablist' 
                  onmousedown="return handleContextMenuSelect(event);" 
-                 onclick="return handleSelect(event)" />
+                 onclick="return handleSelect(event)">
+ 	            <t:includeTemplate filename="sitetab" />
+             </li>
          </ul>
          <div class="dnd-reorder-indicator circle" />
          <div class="dnd-reorder-indicator line" />