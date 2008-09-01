284031ce2bfe57057d418ad95106b8c3f52d096e
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-09-01 20:38:42 +0000

    Cosmetic cleanup of guide.py.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@7810 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 228ddc9d6bab891ce5092c3c0d46b1598dede0f4

--- tv/portable/guide.py
+++ tv/portable/guide.py
@@ -26,31 +26,24 @@
 # this exception statement from your version. If you delete this exception
 # statement from all source files in the program, then also delete it here.
 
+import re
+import logging
+from HTMLParser import HTMLParser
+from urlparse import urlparse, urljoin
+
 from miro.plat import resources
 from miro.database import DDBObject
-from miro.httpclient import grabURL
-from urlparse import urlparse, urljoin
-from miro.xhtmltools import urlencode
-from copy import copy
-from miro.util import returnsUnicode, unicodify, checkU
-import re
-from miro import app
+from miro.util import returnsUnicode, checkU
 from miro import config
 from miro import indexes
 from miro import prefs
-import threading
-import urllib
-from miro import eventloop
 from miro import views
-import logging
 from miro import httpclient
-from miro.gtcache import gettext as _
-from HTMLParser import HTMLParser,HTMLParseError
 from miro import iconcache
 
 HTMLPattern = re.compile("^.*(<head.*?>.*</body\s*>)", re.S)
 
-def isPartOfGuide(url, guideURL, allowedURLs = None):
+def isPartOfGuide(url, guideURL, allowedURLs=None):
     """Return if url is part of a channel guide where guideURL is the base URL
     for that guide.
     """
@@ -69,8 +62,9 @@ def isPartOfGuide(url, guideURL, allowedURLs = None):
             if isPartOfGuide(url, altURL):
                 return True
         return False
+
 class ChannelGuide(DDBObject):
-    def __init__(self, url, allowedURLs = None):
+    def __init__(self, url, allowedURLs=None):
         checkU(url)
         if allowedURLs is None:
             self.allowedURLs = []
@@ -80,7 +74,7 @@ class ChannelGuide(DDBObject):
         self.updated_url = url
         self.title = None
         self.lastVisitedURL = None
-        self.iconCache = iconcache.IconCache(self, is_vital = True)
+        self.iconCache = iconcache.IconCache(self, is_vital=True)
         self.favicon = None
         self.firstTime = True
         if url:
@@ -98,16 +92,15 @@ class ChannelGuide(DDBObject):
         self.lastVisitedURL = None
         self.historyLocation = None
         self.history = []
-        if (self.iconCache == None):
-            self.iconCache = iconcache.IconCache (self, is_vital = True)
+        if self.iconCache == None:
+            self.iconCache = iconcache.IconCache(self, is_vital=True)
         else:
             self.iconCache.dbItem = self
             self.iconCache.requestUpdate(True)
         self.downloadGuide()
 
-
     def __str__(self):
-        return "Miro Guide <%s>" % (self.url,)
+        return "Miro Guide <%s>" % self.url
 
     def remove(self):
         if self.iconCache is not None:
@@ -129,15 +122,15 @@ class ChannelGuide(DDBObject):
 
     def getLastVisitedURL(self):
         if self.lastVisitedURL is not None:
-            logging.info("First URL is %s"%self.lastVisitedURL)
+            logging.info("First URL is %s", self.lastVisitedURL)
             return self.lastVisitedURL
         else:
             if self.firstTime:
                 self.firstTime = False
-                logging.info("First URL is %s"%self.getFirstURL())
+                logging.info("First URL is %s", self.getFirstURL())
                 return self.getFirstURL()
             else:
-                logging.info("First URL is %s"%self.getURL())
+                logging.info("First URL is %s", self.getURL())
                 return self.getURL()
 
     def getDefault(self):
@@ -177,7 +170,7 @@ class ChannelGuide(DDBObject):
             self.iconCache.requestUpdate(True)
             self.signalChange()
 
-    def guideError (self, error):
+    def guideError(self, error):
         logging.warn("Error downloading guide: %s", error)
 
     def downloadGuide(self):
@@ -185,10 +178,6 @@ class ChannelGuide(DDBObject):
 
     @returnsUnicode
     def getIconURL(self):
-        #if self.iconCache.isValid():
-        #    path = self.iconCache.getFilename()
-        #    return resources.absoluteUrl(path)
-        #else:
         if self.getDefault():
             return resources.url("images/icon-guide_small.png")
         return resources.url("images/site-icon.png")
@@ -239,17 +228,16 @@ class GuideHTMLParser(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
         attrdict = {}
-        for (key, value) in attrs:
+        for key, value in attrs:
             attrdict[key] = value
         if tag == 'title' and self.title == None:
             self.in_title = True
             self.title = u""
         if (tag == 'link' and attrdict.has_key('rel') and
-            attrdict.has_key('type') and attrdict.has_key('href') and
-            'icon' in attrdict['rel'].split(' ') and
-            attrdict['type'].startswith("image/")):
-
-            self.favicon = urljoin(self.baseurl,attrdict['href']).decode('ascii', 'ignore')
+                attrdict.has_key('type') and attrdict.has_key('href') and
+                'icon' in attrdict['rel'].split(' ') and
+                attrdict['type'].startswith("image/")):
+            self.favicon = urljoin(self.baseurl, attrdict['href']).decode('ascii', 'ignore')
 
     def handle_data(self, data):
         if self.in_title:
