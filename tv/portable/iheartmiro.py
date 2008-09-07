cad3a481a26e85c098a44d61afd0c0ef0e310787
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-09-07 21:17:59 +0000

    Cosmetic cleanup of iheartmiro module.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@7887 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 55d8d73ee9948785623c1670bd6db9fdcd31fa3f

--- tv/portable/iheartmiro.py
+++ tv/portable/iheartmiro.py
@@ -1,3 +1,31 @@
+# Miro - an RSS based video player application
+# Copyright (C) 2005-2008 Participatory Culture Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# In addition, as a special exception, the copyright holders give
+# permission to link the code of portions of this program with the OpenSSL
+# library.
+#
+# You must obey the GNU General Public License in all respects for all of
+# the code used other than OpenSSL. If you modify file(s) with this
+# exception, you may extend this exception to your version of the file(s),
+# but you are not obligated to do so. If you do not wish to do so, delete
+# this exception statement from your version. If you delete this exception
+# statement from all source files in the program, then also delete it here.
+
 import os
 import sys
 import tempfile
@@ -9,7 +37,7 @@ from miro import prefs
 from miro import dialogs
 from miro.gtcache import gettext as _
 
-def firefoxExecutable():
+def _firefox_executable():
     if sys.platform == "darwin":
         import AppKit
         return AppKit.NSWorkspace.sharedWorkspace().fullPathForApplication_("Firefox")
@@ -20,13 +48,13 @@ def firefoxExecutable():
                 return poss
     return None
 
-def isBrowserInstalled():
-    return isFirefoxInstalled() or isIE7Installed()
+def _is_firefox_installed():
+    return _firefox_executable() != None
 
-def isFirefoxInstalled():
-    return firefoxExecutable() != None
+def _is_browser_installed():
+    return _is_firefox_installed() or _is_ie7_installed()
 
-def isIE7Installed():
+def _is_ie7_installed():
     if sys.platform != 'win32':
         return False
     import _winreg
@@ -34,35 +62,35 @@ def isIE7Installed():
         key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "Software\Microsoft\Internet Explorer")
     except EnvironmentError:
         return False
-    version, type = _winreg.QueryValueEx(key, "Version")
+    version, type_ = _winreg.QueryValueEx(key, "Version")
     return (version[0] == u'7')
 
-def isIHeartMiroInstalled():
-    return isIHeartMiroInstalledOnFirefox() or isIHeartMiroInstalledOnIE7()
+def _is_iheartmiro_installed():
+    return _is_iheartmiro_installed_on_firefox() or _is_iheartmiro_installed_on_ie7()
 
-def isIHeartMiroInstalledOnFirefox():
-    id = "{216ec66d-214a-43ea-92f0-5373f8405c88}"
-    locations = ["~/.mozilla/firefox/*/extensions/" + id,
-	             "~\\Application Data\\Mozilla\\Firefox\\Profiles\\*\\extensions\\" + id,
-	            "~/Library/Application Support/Firefox/Profiles/*/extensions/" + id ]
+def _is_iheartmiro_installed_on_firefox():
+    id_ = "{216ec66d-214a-43ea-92f0-5373f8405c88}"
+    locations = ["~/.mozilla/firefox/*/extensions/" + id_,
+                 "~\\Application Data\\Mozilla\\Firefox\\Profiles\\*\\extensions\\" + id_,
+                "~/Library/Application Support/Firefox/Profiles/*/extensions/" + id_ ]
     for location in locations:
-        if glob.glob (os.path.expanduser(location)):
+        if glob.glob(os.path.expanduser(location)):
             return True
     return False
 
-def isIHeartMiroInstalledOnIE7():
+def _is_iheartmiro_installed_on_ie7():
     if sys.platform != 'win32':
         return False
     import _winreg
-    id = "CLSID\{EB289F5D-2750-4BFC-91E1-4442686BCDC9}"
+    id_ = "CLSID\{EB289F5D-2750-4BFC-91E1-4442686BCDC9}"
     try:
-        key = _winreg.OpenKey(_winreg.HKEY_CLASSES_ROOT, id)
+        _winreg.OpenKey(_winreg.HKEY_CLASSES_ROOT, id_)
     except EnvironmentError:
         return False
     return True
 
-def installIHeartMiroFirefox():
-    def httpSuccess(info):
+def _install_iheartmiro_firefox():
+    def http_success(info):
         fd, filename = tempfile.mkstemp(suffix=".xpi", prefix="iHeartMiro-", text=False)
         output = os.fdopen(fd, "wb")
         output.write(info["body"])
@@ -71,14 +99,16 @@ def installIHeartMiroFirefox():
             import AppKit
             AppKit.NSWorkspace.sharedWorkspace().openFile_withApplication_andDeactivate_(filename, "Firefox", True)
         else:
-            exe = firefoxExecutable()
+            exe = _firefox_executable()
             os.spawnl(os.P_NOWAIT, exe, exe, filename)
-    def httpFailure(error):
-        print "error: %s" % (error,)
-    httpclient.grabURL("http://www.iheartmiro.org/iHeartMiro-latest.xpi", httpSuccess, httpFailure)
 
-def installIHeartMiroIE7():
-    def httpSuccess(info):
+    def http_failure(error):
+        print "error: %s" % error
+
+    httpclient.grabURL("http://www.iheartmiro.org/iHeartMiro-latest.xpi", http_success, http_failure)
+
+def _install_iheartmiro_ie7():
+    def http_success(info):
         from miro.plat import specialfolders
         filename = os.path.join(specialfolders.appDataDirectory,
                                 u'Participatory Culture Foundation',
@@ -89,36 +119,37 @@ def installIHeartMiroIE7():
         exe = "C:\\windows\\system32\\regsvr32.exe"
         os.spawnl(os.P_NOWAIT, exe, exe, '/s', filename)
         
-    def httpFailure(error):
-        print "error: %s" % (error,)
-    httpclient.grabURL("http://www.iheartmiro.org/IHeartMiro.dll", httpSuccess, httpFailure)
+    def http_failure(error):
+        print "error: %s" % error
 
-# request_count makes it so that the second time you 
-def checkIHeartMiroInstall():
+    httpclient.grabURL("http://www.iheartmiro.org/IHeartMiro.dll", http_success, http_failure)
+
+def check_iheartmiro_install():
+    # request_count makes it so that the second time you
     request_count = config.get(prefs.IHEARTMIRO_REQUEST_COUNT)
 
-#    if request_count >= 2:
-# Comment out these two lines to do testing.
+    # Comment out these two lines to do testing.
     if request_count >= 1:
         return
 
-    if isIHeartMiroInstalled() or not isBrowserInstalled():
+    if _is_iheartmiro_installed() or not _is_browser_installed():
         config.set(prefs.IHEARTMIRO_REQUEST_COUNT, 2)
         return
 
-    if isFirefoxInstalled():
+    if _is_firefox_installed():
         installer = 'Firefox'
-        instructions = """If you click 'Install iHeartMiro' below, Firefox will launch and ask if it's ok.   Just say yes and you're done!   It takes about 7 seconds.  And thanks for your help."""
+        instructions = _("""If you click 'Install iHeartMiro' below, Firefox will launch and ask if it's ok.   Just say yes and you're done!   It takes about 7 seconds.  And thanks for your help.""")
     else:
         installer = 'Internet Explorer 7'
-        instructions = """If you click 'Install iHeartMiro' below, we'll install the extension.  Thanks for your help."""
+        instructions = _("""If you click 'Install iHeartMiro' below, we'll install the extension.  Thanks for your help.""")
+
     def callback(dialog):
         count = request_count
         if dialog.choice == dialogs.BUTTON_INSTALL_IHEARTMIRO:
             if installer == 'Firefox':
-                installIHeartMiroFirefox()
+                _install_iheartmiro_firefox()
             else:
-                installIHeartMiroIE7()
+                _install_iheartmiro_ie7()
 
         if isinstance(dialog, dialogs.CheckboxDialog):
             if not dialog.checkbox_value:
@@ -130,14 +161,19 @@ def checkIHeartMiroInstall():
             count = count + 1
         config.set(prefs.IHEARTMIRO_REQUEST_COUNT, count)
 
-    message = \
-_("""An Effortless Way to Help Miro
-
-Miro is open-source and built by a non-profit.  We need your help to continue our work.
-
-I Heart Miro is a simple %(installer)s extension that gives a referral fee to the Miro organization when you shop at Amazon.  You'll never notice it, it doesn't cost you a thing, and it's easy to uninstall at any time.
-
-%(instructions)s""") % { 'installer': installer, 'instructions': instructions }
+    message = _(
+        "An Effortless Way to Help Miro\n"
+        "\n"
+        "Miro is open-source and built by a non-profit.  We need your help to "
+        "continue our work.\n"
+        "\n"
+        "I Heart Miro is a simple %(installer)s extension that gives a "
+        "referral fee to the Miro organization when you shop at Amazon.  "
+        "You'll never notice it, it doesn't cost you a thing, and it's easy "
+        "to uninstall at any time.\n"
+        "\n"
+        "%(instructions)s"
+    ) % { 'installer': installer, 'instructions': instructions }
 
     title = _("Install iHeartMiro?")
 #    again = _ ("Ask me again later")
