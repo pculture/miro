pculture/miro

commit 85d8dfb53ea31146e8bb63009ed81c801623b72a

Bug #10364.  Implemented update checking on startup and re-wrote parts of autoupdate to be better code.
git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@7855 bce20531-a64c-4166-8316-56baf4a01729

Original sha1: f2abd2f46251000899815166c1a247a53bba9824

tv/portable/autoupdate.py
@@ -26,6 +26,12 @@
 # this exception statement from your version. If you delete this exception
 # statement from all source files in the program, then also delete it here.
 
+"""Checks the AUTOUPDATE_URL to see if there's a more recent version
+of the application.
+
+Call ``check_for_updates``.
+"""
+
 from miro import prefs
 from miro import config
 import logging
@@ -35,66 +41,71 @@ from miro import signals
 
 from miro.httpclient import grabURL
 
-checkInProgress = False
+check_in_progress = False
 
-def checkForUpdates(upToDateCallback=None):
-    """Trigger the version checking process
-    """
-    global checkInProgress
-    if not checkInProgress:
-        checkInProgress = True
+def check_for_updates(up_to_date_callback=None):
+    """Checks the AUTOUPDATE_URL for the recent version."""
+    global check_in_progress
+    if not check_in_progress:
+        check_in_progress = True
         logging.info("Checking for updates...")
         url = config.get(prefs.AUTOUPDATE_URL)
-        updateHandler = lambda data: _handleAppCast(data, upToDateCallback)
-        errorHandler = _handleError
-        grabURL(url, updateHandler, errorHandler)
+        update_handler = lambda data: _handle_app_cast(data, up_to_date_callback)
+        error_handler = _handle_error
+        grabURL(url, update_handler, error_handler)
 
-def _handleError(error):
-    """Error handler
-    """
-    global checkInProgress
-    checkInProgress = False
+def _handle_error(error):
+    """Error handler"""
+    global check_in_progress
+    check_in_progress = False
     logging.warn("HTTP error while checking for updates")
-    eventloop.addTimeout (86400, checkForUpdates, "Check for updates")
+    eventloop.addTimeout(86400, check_for_updates, "Check for updates")
 
-def _handleAppCast(data, upToDateCallback):
+def _handle_app_cast(data, up_to_date_callback):
     """Handle appcast data when it's correctly fetched
     """
     try:
-        try:
-            appcast = feedparser.parse(data['body'])
-            if appcast['bozo'] == '1':
-                return
+        appcast = feedparser.parse(data['body'])
+        if appcast['bozo'] == '1':
+            return
 
-            upToDate = True
-            latest = _getItemForLatest(appcast)
-            if latest is not None:
-                serial = int(config.get(prefs.APP_SERIAL))
-                upToDate = (serial >= _getItemSerial(latest))
-        
-            if not upToDate:
-                logging.info('New update available.')
-                signals.system.updateAvailable(latest)
-            elif upToDateCallback:
-                logging.info('Up to date. Notifying')
-                upToDateCallback()
-            else:
-                logging.info('Up to date.')
-        except:
-            logging.warn("Error while handling appcast data.")
-            import traceback
-            traceback.print_exc()
+        up_to_date = True
+        latest = _get_item_for_latest(appcast)
+        if latest is None:
+            logging.info('No updates for this platform.')
+            # this will go through the finally clause below
+            return
+
+        serial = int(config.get(prefs.APP_SERIAL))
+        up_to_date = (serial >= _get_item_serial(latest))
+    
+        if not up_to_date:
+            logging.info('New update available.')
+            signals.system.updateAvailable(latest)
+        elif up_to_date_callback:
+            logging.info('Up to date.  Notifying callback.')
+            up_to_date_callback()
+        else:
+            logging.info('Up to date.')
+
+    except:
+        logging.exception("Error while handling appcast data.")
+
     finally:
-        global checkInProgress
-        checkInProgress = False
-        eventloop.addTimeout(86400, checkForUpdates, "Check for updates")
+        global check_in_progress
+        check_in_progress = False
+        eventloop.addTimeout(86400, check_for_updates, "Check for updates")
 
-def _getItemForLatest(appcast):
+def _get_item_for_latest(appcast):
     """Filter out non platform items, sort remaining from latest to oldest
     and return the item corresponding to the latest known version.
+
+    If there are no entries for this platform (this happens with Linux), then
+    this returns None.
     """
     platform = config.get(prefs.APP_PLATFORM)
     rejectedItems = list()
+
     for item in appcast['entries']:
         rejectedEnclosures = list()
         for enclosure in item['enclosures']:
@@ -106,16 +117,20 @@ def _getItemForLatest(appcast):
             item['enclosures'].remove(enclosure)
         if len(item['enclosures']) == 0:
             rejectedItems.append(item)
+
     for item in rejectedItems:
         appcast['entries'].remove(item)
 
-    try:
-        appcast['entries'].sort(key=_getItemSerial, reverse=True)
-        return appcast['entries'][0]
-    except:
+    # we've removed all entries that aren't relevant to this platform.
+    # if there aren't any left, we return None and the caller can deal
+    # with things.
+    if not appcast['entries']:
         return None
 
-def _getItemSerial(item):
+    appcast['entries'].sort(key=_get_item_serial, reverse=True)
+    return appcast['entries'][0]
+
+def _get_item_serial(item):
     """Returns the serial of the first enclosure of the passed item
     """
     return int(item['enclosures'][0]['dtv:serial'])

tv/portable/frontends/widgets/application.py
@@ -35,12 +35,14 @@ import traceback
 import urllib
 
 from miro import app
+from miro import autoupdate
 from miro import config
 from miro import prefs
 from miro import feed
 from miro import startup
 from miro import signals
 from miro import messages
+from miro import eventloop
 from miro.gtcache import gettext as _
 from miro.frontends.widgets import dialogs
 from miro.frontends.widgets import newsearchchannel
@@ -81,6 +83,8 @@ class Application:
         app.search_manager = search.SearchManager()
         app.tab_list_manager = tablistmanager.TabListManager()
         self.window = MiroWindow(_("Miro"), self.get_main_window_dimensions())
+
+        # FIXME - first-time startup somewhere around here
 
     def build_window(self):
         app.tab_list_manager.populate_tab_list()
@@ -653,6 +657,7 @@ Are you sure you want to stop watching these %s directories?""") % len(channel_i
     def connect_to_signals(self):
         signals.system.connect('error', self.handleError)
         signals.system.connect('download-complete', self.handleDownloadComplete)
+        signals.system.connect('update-available', self.handle_update_available)
         signals.system.connect('startup-success', self.handleStartupSuccess)
         signals.system.connect('startup-failure', self.handleStartupFailure)
         signals.system.connect('new-dialog', self.handleDialog)
@@ -667,9 +672,16 @@ Are you sure you want to stop watching these %s directories?""") % len(channel_i
 
     def handleStartupSuccess(self, obj):
         call_on_ui_thread(self.startup_ui)
+        eventloop.addTimeout(3, autoupdate.check_for_updates, "Check for updates")
 
     def handleDownloadComplete(self, obj, item):
         print "DOWLOAD COMPLETE"
+
+    def handle_update_available(self, obj, item):
+        print "update available!"
+
+    def handle_up_to_date(self):
+        print "up to date!"
 
     def handleError(self, obj, report):
         # FIXME - I don't want to write the code in dialogs.py yet

commit ccf44dabba48f7f2874564767b29a58c877e23f9

Fixed a problem with python2.4 compatibility that I caused in r7855.
git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@7905 bce20531-a64c-4166-8316-56baf4a01729

Original sha1: 656d181fbdddbaafb9e27d92b86426b57eb181c3

tv/portable/autoupdate.py
@@ -64,32 +64,35 @@ def _handle_error(error):
 def _handle_app_cast(data, up_to_date_callback):
     """Handle appcast data when it's correctly fetched
     """
+    # python 2.4 requires that except and finally clauses be in different
+    # try blocks.
     try:
-        appcast = feedparser.parse(data['body'])
-        if appcast['bozo'] == '1':
-            return
+        try:
+            appcast = feedparser.parse(data['body'])
+            if appcast['bozo'] == '1':
+                return
 
-        up_to_date = True
-        latest = _get_item_for_latest(appcast)
-        if latest is None:
-            logging.info('No updates for this platform.')
-            # this will go through the finally clause below
-            return
+            up_to_date = True
+            latest = _get_item_for_latest(appcast)
+            if latest is None:
+                logging.info('No updates for this platform.')
+                # this will go through the finally clause below
+                return
 
-        serial = int(config.get(prefs.APP_SERIAL))
-        up_to_date = (serial >= _get_item_serial(latest))
+            serial = int(config.get(prefs.APP_SERIAL))
+            up_to_date = (serial >= _get_item_serial(latest))
     
-        if not up_to_date:
-            logging.info('New update available.')
-            signals.system.updateAvailable(latest)
-        elif up_to_date_callback:
-            logging.info('Up to date.  Notifying callback.')
-            up_to_date_callback()
-        else:
-            logging.info('Up to date.')
+            if not up_to_date:
+                logging.info('New update available.')
+                signals.system.updateAvailable(latest)
+            elif up_to_date_callback:
+                logging.info('Up to date.  Notifying callback.')
+                up_to_date_callback()
+            else:
+                logging.info('Up to date.')
 
-    except:
-        logging.exception("Error while handling appcast data.")
+        except:
+            logging.exception("Error while handling appcast data.")
 
     finally:
         global check_in_progress