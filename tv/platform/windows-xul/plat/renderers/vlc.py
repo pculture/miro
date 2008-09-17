576186b97a7c84d3da3c331ad87d95769edaa510
Ben Dean-Kawamura <ben@pculture.org>
2008-09-17 20:10:32 +0000

    Fix for VLC playback now that we start playing from the previous time.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@7981 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 152614fdf3b9f1257fcffa40b7a05f71f890a150

--- tv/platform/windows-xul/plat/renderers/vlc.py
+++ tv/platform/windows-xul/plat/renderers/vlc.py
@@ -99,6 +99,8 @@ class VLCRenderer:
         self.media_player = libvlc.libvlc_media_player_new(self.vlc,
                 self.exc.ref())
         self.exc.check()
+        self.play_from_time = None
+        self.started_playing = False
 
     def set_widget(self, widget):
         widget.connect("realize", self.on_realize)
@@ -126,6 +128,10 @@ class VLCRenderer:
 
     def select_file(self, filename):
         """starts playing the specified file"""
+
+        self.play_from_time = None
+        self.started_playing = False
+
         mrl = 'file://%s' % filename
         media = libvlc.libvlc_media_new(self.vlc, ctypes.c_char_p(mrl),
                 self.exc.ref())
@@ -143,6 +149,10 @@ class VLCRenderer:
     def play(self):
         libvlc.libvlc_media_player_play(self.media_player, self.exc.ref())
         self.exc.check()
+        self.started_playing = True
+        if self.play_from_time is not None:
+            self.set_current_time(self.play_from_time)
+            self.play_from_time = None
 
     def pause(self):
         libvlc.libvlc_media_player_pause(self.media_player, self.exc.ref())
@@ -154,6 +164,8 @@ class VLCRenderer:
 
     def reset(self):
         self.stop()
+        self.play_from_time = None
+        self.started_playing = False
 
     def get_current_time(self):
         time = libvlc.libvlc_media_player_get_time(self.media_player, self.exc.ref())
@@ -166,6 +178,9 @@ class VLCRenderer:
             return time / 1000.0
 
     def set_current_time(self, seconds):
+        if not self.started_playing:
+            self.play_from_time = seconds
+            return
         time = int(seconds * 1000)
         logging.warn('set time: %s %s' % (seconds, time))
         libvlc.libvlc_media_player_set_time(self.media_player,
