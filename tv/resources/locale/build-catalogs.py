f74e6df7b6bc976000ec3f4822a62d47ddc28869
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-10-17 18:21:55 +0000

    Reworked the scripts for syncing translations.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@8305 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 4c80a3781e9b94958bfc39df8f1241aa6b3eaa89

--- tv/resources/locale/build-catalogs.py
+++ tv/resources/locale/build-catalogs.py
@@ -1,10 +1,37 @@
+#!/usr/bin/env python
+
 import glob
 import os.path
 import os
 
-os.system("python fix-ro-plurals.py")
+def fix_names():
+    files = [ mem for mem in os.listdir(".") if mem.endswith(".po") ]
+    for pofile in files:
+        if pofile.startswith("democracyplayer-"):
+             os.system("mv %s %s" % (pofile, pofile.replace("democracyplayer-", "")))
 
-for pofile in glob.glob ("*.po"):
-    lang = pofile[:-3]
-    mofile = os.path.join ("%s.mo" % lang)
-    os.system("msgfmt %s -o %s" % (pofile, mofile))
+def fix_ro_plurals():
+    bad_str = '"Plural-Forms: nplurals=3; plural=((n == 1 ? 0: (((n %\\n"\n"100 > 19) || ((n % 100 == 0) && (n != 0))) ? 2: 1)));\\n"'
+
+    good_str = '"Plural-Forms: nplurals=3; plural=((n == 1 ? 0: (((n % 100 > 19) || ((n % 100 == 0) && (n != 0))) ? 2: 1)));\\n"'
+    ro_path = os.path.normpath(os.path.join(__file__, '..', 'ro.po'))
+    content = open(ro_path).read()
+    if bad_str in content:
+        print 'fixing ro.po'
+        f = open(ro_path, 'wt')
+        f.write(content.replace(bad_str, good_str))
+        f.close()
+
+def build_catalogs():
+    for pofile in glob.glob ("*.po"):
+        lang = pofile[:-3]
+        mofile = os.path.join ("%s.mo" % lang)
+        os.system("msgfmt %s -o %s" % (pofile, mofile))
+
+if __name__ == "__main__":
+    print "FIXING NAMES...."
+    fix_names()
+    print "FIXING RO PLURALS...."
+    fix_ro_plurals()
+    print "BUILDING .mo FILES...."
+    build_catalogs()
