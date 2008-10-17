b74765cdf528d436adadcae1b4719813ae359610
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-10-17 03:08:16 +0000

    Fixed a problem with adding playlists.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@8297 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 1bddcabd328dfb5720c858692c0709a9936065a2

--- tv/portable/messages.py
+++ tv/portable/messages.py
@@ -679,6 +679,7 @@ class PlaylistInfo(object):
         self.name = playlist_obj.get_title()
         self.id = playlist_obj.id
         self.is_folder = isinstance(playlist_obj, PlaylistFolder)
+        self.parent_id = playlist_obj.folder_id
 
 class GuideInfo(object):
     """Tracks the state of a channel guide
