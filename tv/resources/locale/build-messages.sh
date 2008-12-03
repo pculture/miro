5ea28a8064c77981e0b5868a65cb4d0c641f2a41
Will Kahn-Greene <will.guaraldi@pculture.org>
2008-12-03 16:48:50 +0000

    Fixed build-messages.sh to ignore the testdata directory. Updated the messages.pot file.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@8640 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 38d1dd587ea840cfbae2d6ffee394353979340f0

--- tv/resources/locale/build-messages.sh
+++ tv/resources/locale/build-messages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xgettext -k_ -o messages.pot `find ../../ -name '*.py' -and -not -path '*.svn*' -and -not -path '*build*' -and -not -path '*dist*' -and -not -path '*feedparser.py' -and -not -path '*portable/test/*'`
+xgettext -k_ -o messages.pot `find ../../ -name '*.py' -and -not -path '*testdata*' -and -not -path '*.svn*' -and -not -path '*build*' -and -not -path '*dist*' -and -not -path '*feedparser.py' -and -not -path '*portable/test/*'`
