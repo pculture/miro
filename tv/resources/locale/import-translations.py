5b2e4c2347abd9b5c32ec3ef439b29dfd99a8538
Will Kahn-Greene <will.guaraldi@pculture.org>
2009-01-15 19:56:58 +0000

    Bug #11073.  This generalizes the import-translations.py file per Lukasz' suggestions so that it's useful for importing all .po files as well as importing a single .po file.  This makes it easier on translators.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@8939 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 1a50e9a6557fc26ad9cb994a973b5e3fd4076439

--- tv/resources/locale/import-translations.py
+++ tv/resources/locale/import-translations.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python
 """
-This script is for importing a Launchpad export of all available .po
-files.  It:
+This script is for importing a Launchpad export of .po files.
+It handles both the "full export" case and individual .po files.
+It:
 
  * fixes the filenames to what Miro uses,
  * fixes some bugs we've seen in some of the translations, and
  * calls msgfmt on the .po files creating .mo files
 
-This is not a useful script for importing a single .po file.
+It's just a utility script.  If you find it needs additional bits,
+let us know.
 """
 
 import glob
 import os.path
 import os
 
+def get_files():
+    return [mem for mem in os.listdir(".") if mem.endswith(".po")]
+
 def fix_names():
-    files = [ mem for mem in os.listdir(".") if mem.endswith(".po") ]
+    files = get_files()
     for pofile in files:
-        if pofile.startswith("democracyplayer-"):
-             os.system("mv %s %s" % (pofile, pofile.replace("democracyplayer-", "")))
+        newfilename = pofile
+        for repl in ["democracyplayer_", "democracyplayer-"]:
+            newfilename = newfilename.replace(repl, "")
+        if newfilename != pofile:
+             os.system("mv %s %s" % (pofile, newfilename))
 
 def fix_ro_plurals():
     bad_str = '"Plural-Forms: nplurals=3; plural=((n == 1 ? 0: (((n %\\n"\n"100 > 19) || ((n % 100 == 0) && (n != 0))) ? 2: 1)));\\n"'
@@ -32,15 +40,27 @@ def fix_ro_plurals():
         f.write(content.replace(bad_str, good_str))
         f.close()
 
+def fix_pound_pipe():
+    files = get_files()
+    for pofile in files:
+        f = open(pofile, "r")
+        lines = [line for line in f.readlines() if not line.startswith("#|")]
+        f.close()
+        f = open(pofile, "w")
+        f.write("".join(lines))
+        f.close()
+
 def build_catalogs():
-    for pofile in glob.glob ("*.po"):
-        lang = pofile[:-3]
-        mofile = os.path.join ("%s.mo" % lang)
+    files = get_files()
+    for pofile in files:
+        mofile = "%s.mo" % pofile[:-3]
         os.system("msgfmt %s -o %s" % (pofile, mofile))
 
 if __name__ == "__main__":
     print "FIXING NAMES...."
     fix_names()
+    print "FIXING #| issues...."
+    fix_pound_pipe()
     print "FIXING RO PLURALS...."
     fix_ro_plurals()
     print "BUILDING .mo FILES...."
