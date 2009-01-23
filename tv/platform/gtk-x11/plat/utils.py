c571e25e1d21db8d4318c4af32c8dc4741162e5a
Will Kahn-Greene <will.guaraldi@pculture.org>
2009-01-23 21:02:37 +0000

    Changing the 500K limit to 100K for log files on Linux--that seems more reasonable.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@9012 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: f6b8cb674b487cbb021550bc9e3acdaf3e21b12c

--- tv/platform/gtk-x11/plat/utils.py
+++ tv/platform/gtk-x11/plat/utils.py
@@ -115,10 +115,11 @@ def setup_logging(inDownloader=False):
             level = logging.WARN
         logging.basicConfig(level=level,
                             format='%(asctime)s %(levelname)-8s %(message)s')
-        rotater = logging.handlers.RotatingFileHandler(config.get(prefs.LOG_PATHNAME), mode="w", maxBytes=500000, backupCount=5)
+        rotater = logging.handlers.RotatingFileHandler(config.get(prefs.LOG_PATHNAME), mode="w", maxBytes=100000, backupCount=5)
         formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
         rotater.setFormatter(formatter)
         logging.getLogger('').addHandler(rotater)
+        logging.info("===========================================================")
 
 @returnsBinary
 def unicodeToFilename(filename, path=None):
