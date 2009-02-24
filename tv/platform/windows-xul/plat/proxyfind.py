026be777a433798544e438f13ce51a744908ecdd
Ben Dean-Kawamura <ben@pculture.org>
2009-02-24 17:36:05 +0000

    Deal with not being able to load winhttp (#11522)
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@9234 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 3dfe9167b814d138aea814717f2889beec7c84bc

--- tv/platform/windows-xul/plat/proxyfind.py
+++ tv/platform/windows-xul/plat/proxyfind.py
@@ -33,6 +33,8 @@ from ctypes.wintypes import LPWSTR, BOOL
 import logging
 import re
 
+winhttp_loaded = False
+
 class WINHTTP_CURRENT_USER_IE_PROXY_CONFIG(Structure):
     _fields_ = [
         ('fAutoDetect', BOOL),
@@ -48,11 +50,19 @@ class ProxyInfo:
         self.ignore_hosts = []
 
 def get_proxy_info():
+    global winhttp_loaded
     proxy_info = ProxyInfo()
     ie_proxy_info = WINHTTP_CURRENT_USER_IE_PROXY_CONFIG()
-    rv = windll.winhttp.WinHttpGetIEProxyConfigForCurrentUser(
-            byref(ie_proxy_info))
-
+    try:
+        winhttp = windll.winhttp
+    except WindowsError:
+        # We couldn't load the winhttp module.  Maybe the user doesn't have
+        # SP1 or later?  (#11522)
+        rv = None
+    else:
+        winhttp_loaded = True
+        rv = winhttp.WinHttpGetIEProxyConfigForCurrentUser(
+                byref(ie_proxy_info))
     if not rv or ie_proxy_info.lpszProxy is None:
         return proxy_info
     proxy_info.host, proxy_info.port = \
