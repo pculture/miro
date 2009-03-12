c0dd78b6fd9d6d3af5cd5d546aff1990d59955cc
Will Kahn-Greene <will.guaraldi@pculture.org>
2009-03-12 00:03:49 +0000

    Merging r9279 from trunk to Miro-2.0 branch.
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/branches/Miro-2.0@9297 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: 83039d3b82805210d60988f08af479e3fd8d4823

--- tv/portable/frontends/widgets/tablistmanager.py
+++ tv/portable/frontends/widgets/tablistmanager.py
@@ -67,10 +67,10 @@ class TabListManager(object):
             app.menu_manager.handle_static_tab_selection(self.selected_tabs)
         elif self.selected_tab_list.type == 'site':
             app.menu_manager.handle_site_selection(self.selected_tabs)
-            app.item_list_controller_manager.handle_playable_items()
         else:
             raise ValueError("Unknown tab list type: %s" %
                     self.selected_tab_list.type)
+        app.item_list_controller_manager.handle_playable_items()
 
     def which_tablist_has_id(self, feed_id):
         """
