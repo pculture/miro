8cacf5892f00b8a241852d36aa9f55b172117ab1
Will Kahn-Greene <will.guaraldi@pculture.org>
2009-04-14 19:16:46 +0000

    Bug #11617.  Went through and added license/copyright information to all files in Uwe's list.  Thanks Uwe!
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@9411 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: e4f06dcde0b3c942d675e0eedd76ee9d07346261

--- tv/portable/clock.py
+++ tv/portable/clock.py
@@ -1,7 +1,25 @@
-# Originally taken from BitTornado written by John Hoffman
-# see portable/BitTorrent/LICENSE.txt for license information.
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# The Software is provided "AS IS", without warranty of any kind,
+# express or implied, including but not limited to the warranties of
+# merchantability,  fitness for a particular purpose and
+# noninfringement. In no event shall the  authors or copyright holders
+# be liable for any claim, damages or other liability, whether in an
+# action of contract, tort or otherwise, arising from, out of or in
+# connection with the Software or the use or other dealings in the
+# Software.
 #
-# Rewritten by Nick Nassar <nassar@pculture dotorg> to be thread safe.
+# Copyright (C) 2002 John Hoffman
+#               2006 Participatory Culture Foundation
 
 from time import time, clock
 import sys
