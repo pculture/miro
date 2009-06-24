6c1caef5755d842618682814ecfe0276c522fa37
Will Kahn-Greene <will.guaraldi@pculture.org>
2009-06-24 14:24:37 +0000

    Removing a bunch of cruft from SVN that's not Miro related. Many of these things can already be found in their own repositories at https://git.participatoryculture.org/ .
    git-svn-id: https://svn.participatoryculture.org/svn/dtv/trunk@9778 bce20531-a64c-4166-8316-56baf4a01729
    
    Original sha1: be3eca3792ee2431c6c1af5f5aa12452eb96c7db

--- retemplate/computeformals.py
+++
@@ -1,9 +0,0 @@
-import parse
-
-def computeFormals(nodes, soFar=[]):
-    for n in nodes:
-        if isinstance(n, parse.ForNode):
-            soFar = soFar + [n.formal]
-        n.formals = soFar
-        computeFormals(n.allChildren(), soFar)
-            
