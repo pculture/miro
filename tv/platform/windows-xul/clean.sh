8f978148bf2fbf20571da1dcc713a4184c2f2d1d
Will Kahn-Greene <will.guaraldi@pculture.org>
2010-01-12 17:15:56 -0500

    Adding a clean.sh script.
    This script deletes all products of building Miro on Windows.

---
+++ tv/platform/windows-xul/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Last updated January 12, 2010
+
+rm -rf build
+rm -rf dist
+rm -rf plat/frontends/widgets/XULRunnerBrowser/xulrunnerbrowser.cpp
+rm -rf plat/frontends/widgets/XULRunnerBrowser/xulrunnerbrowser.h
+rm -rf ../../portable/__init__.pyc
+rm -rf ../../portable/clock.pyc
+rm -rf ../../portable/filetypes.pyc
+rm -rf ../../portable/frontends/widgets/gtk/pygtkhacks.c
+rm -rf ../../portable/utils.pyc
