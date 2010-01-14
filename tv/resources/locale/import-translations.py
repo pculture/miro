eff38b28abf573012bd63bfca68e9e1624e685f7
Will Kahn-Greene <will.guaraldi@pculture.org>
2010-01-14 15:27:35 -0500

    Updated copyright dates and text files, removed xine from .gitignore.

--- tv/resources/locale/import-translations.py
+++ tv/resources/locale/import-translations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Miro - an RSS based video player application
-# Copyright (C) 2005-2009 Participatory Culture Foundation
+# Copyright (C) 2005-2010 Participatory Culture Foundation
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
