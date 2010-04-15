c587f1fff60ce0eb9121f7288df7c88d1012a540
Will Kahn-Greene <will.guaraldi@pculture.org>
2010-04-15 19:04:28 -0400

    Added helper scripts to repository.
    These are scripts that make it easy to install build and runtime dependencies
    for someone running Miro from source tarball or git.

---
+++ tv/linux/helperscripts/fedora_12.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script installs dependencies for building and running Miro on
+# Fedora 12.
+#
+# You run this sript AT YOUR OWN RISK.  Read through the whole thing
+# before running it!
+#
+# This script must be run as root.
+
+# Last updated:    4/15/2010
+# Last updated by: Will Kahn-Greene
+
+yum groupinstall "Developer Tools" "Developer Libraries"
+
+yum install Pyrex gecko-devel-unstable pygtk2-devel
+
+yum install gnome-python2-gtkmozembed rb_libtorrent rb_libtorrent-python
---
+++ tv/linux/helperscripts/ubuntu_karmic_9_10.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script installs dependencies for building and running Miro on
+# Ubuntu 9.10 (Karmic Koala).
+#
+# You run this sript AT YOUR OWN RISK.  Read through the whole thing
+# before running it!
+#
+# This script must be run with sudo.
+
+# Last updated:    4/15/2010
+# Last updated by: Will Kahn-Greene
+
+aptitude install build-essential git-core pkg-config python-pyrex \
+    xulrunner-1.9.1-dev python-gtk2-dev
+
+aptitude install libtorrent-rasterbar5 python-libtorrent python-gtkmozembed \
+    python-gst0.10 python-gconf \
+    gstreamer0.10-ffmpeg gstreamer0.10-plugins-base gstreamer0.10-plugins-good \
+    gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly
