82cdccc9d3152a9c11c919af166cd7142b432fb1
Will Kahn-Greene <will.guaraldi@pculture.org>
2010-05-07 13:26:10 -0400

    Updated helper scripts with webkit changes.

--- tv/linux/helperscripts/ubuntu_karmic_9_10.sh
+++ tv/linux/helperscripts/ubuntu_karmic_9_10.sh
@@ -8,14 +8,27 @@
 #
 # This script must be run with sudo.
 
-# Last updated:    4/15/2010
+# Last updated:    5/5/2010
 # Last updated by: Will Kahn-Greene
 
-aptitude install build-essential git-core pkg-config python-pyrex \
-    xulrunner-1.9.1-dev python-gtk2-dev
+aptitude install \
+    build-essential \
+    git-core \
+    pkg-config \
+    python-pyrex \
+    python-gtk2-dev
 
-aptitude install libtorrent-rasterbar5 python-libtorrent python-gtkmozembed \
-    python-gst0.10 python-gconf \
-    gstreamer0.10-ffmpeg gstreamer0.10-plugins-base gstreamer0.10-plugins-good \
-    gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly \
-    ffmpeg ffmpeg2theora
+aptitude install \
+    libtorrent-rasterbar5 \
+    python-libtorrent \
+    libwebkit-1.0-2 \
+    python-webkit \
+    python-gst0.10 \
+    python-gconf \
+    gstreamer0.10-ffmpeg \
+    gstreamer0.10-plugins-base \
+    gstreamer0.10-plugins-good \
+    gstreamer0.10-plugins-bad \
+    gstreamer0.10-plugins-ugly \
+    ffmpeg \
+    ffmpeg2theora
--- tv/linux/helperscripts/ubuntu_lucid_10_4.sh
+++ tv/linux/helperscripts/ubuntu_lucid_10_4.sh
@@ -8,14 +8,25 @@
 #
 # This script must be run with sudo.
 
-# Last updated:    4/16/2010
+# Last updated:    5/5/2010
 # Last updated by: Will Kahn-Greene
 
-aptitude install build-essential git-core pkg-config python-pyrex \
-    xulrunner-1.9.2-dev python-gtk2-dev
+aptitude install \
+    build-essential \
+    git-core \
+    pkg-config \
+    python-pyrex \
+    python-gtk2-dev
 
-aptitude install libtorrent-rasterbar5 python-libtorrent python-gtkmozembed \
-    python-gst0.10 python-gconf \
-    gstreamer0.10-ffmpeg gstreamer0.10-plugins-base gstreamer0.10-plugins-good \
-    gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly \
-    ffmpeg ffmpeg2theora
+aptitude install \
+    libtorrent-rasterbar5 \
+    python-libtorrent \
+    python-gst0.10 \
+    python-gconf \
+    gstreamer0.10-ffmpeg \
+    gstreamer0.10-plugins-base \
+    gstreamer0.10-plugins-good \
+    gstreamer0.10-plugins-bad \
+    gstreamer0.10-plugins-ugly \
+    ffmpeg \
+    ffmpeg2theora
