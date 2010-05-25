pculture/miro

commit e98e41f018df71ff133d8e6e9a96d5fe2f6ee39a

Fixed Window menu position on OS X.

tv/osx/plat/frontends/widgets/osxmenus.py
@@ -219,7 +219,7 @@ def populate_menu():
         menus.MenuItem(_("Bring All to Front"), "BringAllToFront"),
     ]
     windowMenu = menus.Menu(_("Window"), "Window", windowMenuItems)
-    menubar.insert(5, windowMenu)
+    menubar.insert(6, windowMenu)
 
     # Help Menu
     helpItem = menubar.get("Help")

commit 75b122c54b8c9bd3cad892260be2a752118beb4d

Fixed crash on playback on OS X (caused by e98e41f0).

tv/osx/plat/frontends/widgets/osxmenus.py
@@ -330,13 +330,13 @@ subtitles_menu_handler = SubtitleChangesHandler.alloc().init()
 
 def on_menu_change(menu_manager):
     main_menu = NSApp().mainMenu()
-    play_pause_menu_item = main_menu.itemAtIndex_(5).submenu().itemAtIndex_(0)
+    play_pause_menu_item = main_menu.itemAtIndex_(6).submenu().itemAtIndex_(0)
     play_pause = _menu_structure.get("PlayPauseItem").state_labels[app.menu_manager.play_pause_state]
     play_pause_menu_item.setTitleWithMnemonic_(play_pause.replace("_", "&"))
 
 def on_playback_change(playback_manager):
     main_menu = NSApp().mainMenu()
-    subtitles_menu_root = main_menu.itemAtIndex_(5).submenu().itemAtIndex_(15)
+    subtitles_menu_root = main_menu.itemAtIndex_(6).submenu().itemAtIndex_(15)
     subtitles_menu = NSMenu.alloc().init()
     subtitles_menu.setAutoenablesItems_(NO)
     subtitles_tracks = None