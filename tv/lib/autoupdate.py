9408f0b83630ff9a24a77f39d12b498d4b4d23ed
Will Kahn-Greene <will.guaraldi@pculture.org>
2010-06-14 15:49:22 -0400

    Getting rid of silly try/try/except/finally block.
    Yay for ditching support for Python 2.4!

--- tv/lib/autoupdate.py
+++ tv/lib/autoupdate.py
@@ -75,38 +75,34 @@ def _handle_error(error):
 def _handle_app_cast(data, up_to_date_callback):
     """Handle appcast data when it's correctly fetched
     """
-    # python 2.4 requires that except and finally clauses be in different
-    # try blocks.
     try:
-        try:
-            appcast = feedparser.parse(data['body'])
-            if appcast['bozo'] == '1':
-                return
+        appcast = feedparser.parse(data['body'])
+        if appcast['bozo'] == '1':
+            return
 
-            up_to_date = True
-            latest = _get_item_for_latest(appcast)
-            if latest is None:
-                logging.info('No updates for this platform.')
-                # this will go through the finally clause below
-                return
+        up_to_date = True
+        latest = _get_item_for_latest(appcast)
+        if latest is None:
+            logging.info('No updates for this platform.')
+            # this will go through the finally clause below
+            return
 
-            serial = int(config.get(prefs.APP_SERIAL))
-            up_to_date = (serial >= _get_item_serial(latest))
+        serial = int(config.get(prefs.APP_SERIAL))
+        up_to_date = (serial >= _get_item_serial(latest))
 
-            if not up_to_date:
-                logging.info('New update available.')
-                signals.system.update_available(latest)
-            elif up_to_date_callback:
-                logging.info('Up to date.  Notifying callback.')
-                up_to_date_callback()
-            else:
-                logging.info('Up to date.')
+        if not up_to_date:
+            logging.info('New update available.')
+            signals.system.update_available(latest)
+        elif up_to_date_callback:
+            logging.info('Up to date.  Notifying callback.')
+            up_to_date_callback()
+        else:
+            logging.info('Up to date.')
 
-        except (SystemExit, KeyboardInterrupt):
-            raise
-        except:
-            logging.exception("Error while handling appcast data.")
-
+    except (SystemExit, KeyboardInterrupt):
+        raise
+    except:
+        logging.exception("Error while handling appcast data.")
     finally:
         global check_in_progress
         check_in_progress = False
