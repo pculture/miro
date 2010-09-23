321829c7ed9ec5408b7b80633e04669f98b7c2d8
Ben Dean-Kawamura <ben@pculture.org>
2010-09-23 12:07:01 -0400

    Fix for status column in list item view.
    Before if you paused an item, then restarted miro, it would just displayed
    "paused".  This didn't let you restart the download without right-clicking and
    was different from what miro displayed before you restarted.

--- tv/lib/frontends/widgets/style.py
+++ tv/lib/frontends/widgets/style.py
@@ -1224,8 +1224,8 @@ class StatusRenderer(ListViewRenderer):
             self.text = ''
 
     def layout(self, layout):
-        if (self.info.state in ('downloading', 'paused') and
-                self.info.download_info.rate > 0):
+        if ((self.info.state == 'downloading' and
+            self.info.download_info.rate > 0) or self.info.state == 'paused'):
             return self.pack_progress_bar(layout)
         else:
             return ListViewRenderer.layout(self, layout)
