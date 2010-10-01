e57fc99105a6c736de3369c884dc935324faba57
Will Kahn-Greene <will.guaraldi@pculture.org>
2010-10-01 11:08:33 -0400

    Minor cleanup for the cli frontend.
    * reduces the logging level
    * fixes some of the error messages
    * adds messages indicating "help" gives help
    * changes empty command line behavior so instead of repeating last
      command, it prints a "Type help for help" message

--- tv/linux/plat/utils.py
+++ tv/linux/plat/utils.py
@@ -97,8 +97,6 @@ def setup_logging(in_downloader=False):
             level = logging.WARN
         elif os.environ.get('MIRO_APP_VERSION', "").endswith("git"):
             level = logging.DEBUG
-        elif os.environ.get('MIRO_FRONTEND') == 'cli':
-            level = logging.WARN
         else:
             level = logging.INFO
         logging.basicConfig(level=level,
@@ -110,10 +108,8 @@ def setup_logging(in_downloader=False):
     else:
         if config.get(prefs.APP_VERSION).endswith("git"):
             level = logging.DEBUG
-        elif options.frontend != 'cli':
-            level = logging.INFO
         else:
-            level = logging.WARN
+            level = logging.INFO
         logging.basicConfig(level=level,
                             format='%(asctime)s %(levelname)-8s %(message)s')
         pathname = config.get(prefs.LOG_PATHNAME)
