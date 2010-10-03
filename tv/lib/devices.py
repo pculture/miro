a6767654f832b04879888389ff5f986bfaff236b
Paul Swartz <paulswartz@gmail.com>
2010-10-02 23:24:16 -0400

    detect devices based on USB IDs, rather than on the Gio name

--- tv/lib/devices.py
+++ tv/lib/devices.py
@@ -6,17 +6,28 @@ from miro import messages
 from miro.plat import resources
 
 class DeviceInfo(object):
-    def __init__(self, name, parser):
-        self.io_name = name
-        self.name = parser.get(name, 'name')
-        self.video_conversion = parser.get(name, 'video_conversion')
-        self.video_path = parser.get(name, 'video_path')
-        self.audio_conversion = parser.get(name, 'audio_conversion')
-        self.audio_path = parser.get(name, 'audio_path')
-        self.audio_types = parser.get(name, 'audio_types').split()
-        self.mount_instructions = parser.get(
-            name, 'mount_instructions').replace('\\n', '\n')
+    def __init__(self, section, parser):
+        self.name = section
+        self.vendor_id = int(self._get(section, parser, 'vendor_id'), 16)
+        self.product_id = [int(id, 16) for id in
+                           self._get(section, parser, 'product_id').split()]
+        self.video_conversion = self._get(section, parser, 'video_conversion')
+        self.video_path = self._get(section, parser, 'video_path')
+        self.audio_conversion = self._get(section, parser, 'audio_conversion')
+        self.audio_path = self._get(section, parser, 'audio_path')
+        self.audio_types = self._get(section, parser, 'audio_types').split()
+        self.mount_instructions = self._get(
+            section, parser, 'mount_instructions').replace('\\n', '\n')
 
+    def _get(self, section, parser, name):
+        try:
+            return parser.get(section, name)
+        except KeyError:
+            pass
+        try:
+            return parser.get('DEFAULT', name)
+        except KeyError:
+            return None
 class DeviceManager(object):
     def __init__(self):
         self.device_map = {}
@@ -29,10 +40,11 @@ class DeviceManager(object):
             parser.readfp(open(device_desc))
             for section in parser.sections():
                 info = DeviceInfo(section, parser)
-                self.device_map[section] = info
+                for product_id in info.product_id:
+                    self.device_map[(info.vendor_id, product_id)] = info
 
-    def get_device(self, name):
-        return self.device_map[name]
+    def get_device(self, vendor_id, product_id):
+        return self.device_map[(vendor_id, product_id)]
 
 device_manager = DeviceManager()
 
--- tv/linux/plat/devicetracker.py
+++ tv/linux/plat/devicetracker.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import subprocess
 
 import gio
 
@@ -9,6 +10,7 @@ from miro import messages
 
 class DeviceTracker(object):
     def __init__(self):
+        self._usb_info_cache = {}
         self._disconnecting = {}
 
     def start_tracking(self):
@@ -20,10 +22,22 @@ class DeviceTracker(object):
         for drive in volume_monitor.get_connected_drives():
             self._drive_connected(volume_monitor, drive)
 
+    def _get_usb_info(self, id):
+        if id not in self._usb_info_cache:
+            udev_info = subprocess.Popen(
+                ['udevadm', 'info', '--name', id,
+                 '--query', 'property'],
+                stdout=subprocess.PIPE).communicate()[0]
+            d = dict(line.split('=', 1) for
+                     line in udev_info.split('\n') if line)
+            self._usb_info_cache[id] = (int(d['ID_VENDOR_ID'], 16),
+                                        int(d['ID_MODEL_ID'], 16))
+        return self._usb_info_cache[id]
+
     def _get_device_info(self, drive):
-        name = drive.get_name()
-        device_info = devices.device_manager.get_device(name)
         id = drive.get_identifier('unix-device')
+        device_info = devices.device_manager.get_device(
+            *self._get_usb_info(id))
         mount_path = size = remaining = None
         volumes = drive.get_volumes()
         if volumes:
@@ -68,6 +82,7 @@ class DeviceTracker(object):
 
     def _drive_disconnected_timeout(self, info):
         del self._disconnecting[info.id]
+        del self._usb_info_cache[info.id]
         devices.device_disconnected(info)
 
 tracker = DeviceTracker()
