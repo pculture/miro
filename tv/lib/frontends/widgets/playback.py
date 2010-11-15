a9b65b00c1c210e66d6640e9b29d8f4b5c2c0fa9
Will Kahn-Greene <will.guaraldi@pculture.org>
2010-11-15 13:28:11 -0500

    Fixes playback.

--- tv/lib/frontends/widgets/playback.py
+++ tv/lib/frontends/widgets/playback.py
@@ -521,7 +521,7 @@ class PlaybackManager (signals.SignalEmitter):
 
     def _on_ready_to_play(self, obj):
         self.open_finished = True
-        if not self.playlist[self.postion].item_watched:
+        if not self.playlist[self.position].item_viewed:
             self.schedule_mark_as_watched(self.playlist[self.position].id)
         if isinstance(self.player, widgetset.VideoPlayer):
             self.player.select_subtitle_encoding(self.initial_subtitle_encoding)
