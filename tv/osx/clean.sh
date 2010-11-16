ef2168077c0f755905a1520286fe195baef36538
Will Kahn-Greene <will.guaraldi@pculture.org>
2010-11-15 20:13:38 -0500

    Adding a clean script to OSX-land.

---
+++ tv/osx/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Last updated January 12, 2010
+
+rm -rf build
+rm -rf Miro.app
+rm -rf ../lib/__init__.pyc
+rm -rf ../lib/clock.pyc
+rm -rf ../lib/filetypes.pyc
+rm -rf ../lib/util.pyc
