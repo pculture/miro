pculture/miro

commit a3173bbfa1676b98532b5cb35827c1794d754119

Don't shadow built-ins.

tv/lib/frontends/widgets/playlist.py
@@ -53,13 +53,13 @@ class DropHandler(signals.SignalEmitter):
     def allowed_types(self):
         return ('downloaded-item',)
 
-    def validate_drop(self, table_view, model, type, source_actions, parent,
+    def validate_drop(self, table_view, model, typ, source_actions, parent,
             position):
         if position != -1 and type == 'downloaded-item':
             return widgetset.DRAG_ACTION_MOVE
         return widgetset.DRAG_ACTION_NONE
 
-    def accept_drop(self, table_view, model, type, source_actions, parent,
+    def accept_drop(self, table_view, model, typ, source_actions, parent,
             position, data):
         dragged_ids = set([int(id) for id in data.split('-')])
         if position >= 0:

tv/lib/frontends/widgets/application.py
@@ -105,9 +105,9 @@ class Application:
         self.unwatched_count = 0
         app.frontend_config_watcher = config.ConfigWatcher(call_on_ui_thread)
 
-    def exception_handler(self, type, value, traceback):
+    def exception_handler(self, typ, value, traceback):
         report = crashreport.format_crash_report("in frontend thread",
-            exc_info=(type, value, traceback), details=None)
+            exc_info=(typ, value, traceback), details=None)
         self.handle_crash_report(report)
 
     def startup(self):
@@ -1352,14 +1352,14 @@ class FrontendStatesStore(object):
         self.sort_states = message.sort_states
         self.active_filters = message.active_filters
 
-    def _key(self, type, id):
-        return '%s:%s' % (type, id)
+    def _key(self, typ, id_):
+        return '%s:%s' % (typ, id_)
 
-    def query_list_view(self, type, id):
-        return self._key(type, id) in self.current_displays
+    def query_list_view(self, typ, id_):
+        return self._key(typ, id_) in self.current_displays
 
-    def query_sort_state(self, type, id):
-        key = self._key(type, id)
+    def query_sort_state(self, typ, id_):
+        key = self._key(typ, id_)
         if key in self.sort_states:
             state = self.sort_states[key]
             if state.startswith('-'):
@@ -1371,29 +1371,29 @@ class FrontendStatesStore(object):
             return itemlist.SORT_KEY_MAP[sort_key](ascending)
         return None
 
-    def query_filters(self, type, id):
-        return self.active_filters.get(self._key(type, id), [])
+    def query_filters(self, typ, id_):
+        return self.active_filters.get(self._key(typ, id_), [])
 
     def set_filters(self, type, id, filters):
         self.active_filters[self._key(type, id)] = filters
         self.save_state()
 
-    def set_sort_state(self, type, id, sorter):
+    def set_sort_state(self, typ, id_, sorter):
         # we have a ItemSort object and we need to create a string that will
         # represent it.  Use the sort key, with '-' prepended if the sort is
         # descending (for example: "date", "-name", "-size", ...)
         state = sorter.KEY
         if not sorter.is_ascending():
             state = '-' + state
-        self.sort_states[self._key(type, id)] = state
+        self.sort_states[self._key(typ, id_)] = state
         self.save_state()
 
-    def set_list_view(self, type, id):
-        self.current_displays.add(self._key(type, id))
+    def set_list_view(self, typ, id_):
+        self.current_displays.add(self._key(typ, id_))
         self.save_state()
 
-    def set_std_view(self, type, id):
-        self.current_displays.discard(self._key(type, id))
+    def set_std_view(self, typ, id_):
+        self.current_displays.discard(self._key(typ, id_))
         self.save_state()
 
     def save_state(self):

tv/lib/frontends/widgets/itemlistcontroller.py
@@ -81,14 +81,14 @@ class ItemListController(object):
     
     :attribute widget: Widget used to display this controller
     """
-    def __init__(self, type, id):
+    def __init__(self, typ, id_):
         """Construct a ItemListController.
 
         type and id are the same as in the constructor to
         messages.TrackItems
         """
-        self.type = type
-        self.id = id
+        self.type = typ
+        self.id = id_
         self.current_item_view = None
         self._search_text = ''
         self._init_widget()

tv/lib/frontends/widgets/playback.py
@@ -430,11 +430,11 @@ class PlaybackManager (signals.SignalEmitter):
         def _handle_unsuccessful_sniff():
             self._finish_setup_player(item_info, "unplayable", volume)
         if item_info.media_type_checked:
-            type = item_info.file_type
-            if type == 'other':
+            typ = item_info.file_type
+            if typ == 'other':
                 # the backend and frontend use different names for this
-                type = 'unplayable'
-            self._finish_setup_player(item_info, type, volume)
+                typ = 'unplayable'
+            self._finish_setup_player(item_info, typ, volume)
         else:
             widgetset.get_item_type(item_info, _handle_successful_sniff, _handle_unsuccessful_sniff)
     

tv/lib/frontends/widgets/prefpanel.py
@@ -634,8 +634,8 @@ class DiskSpacePanel(PanelBuilder):
         note = widgetset.Label(_('GB'))
         attach_boolean(cbx, prefs.PRESERVE_DISK_SPACE, (limit,))
 
-        def set_library_filter(self, type, filter):
-            self.library[type] = filter
+        def set_library_filter(self, typ, filter):
+            self.library[typ] = filter
         attach_float(limit, prefs.PRESERVE_X_GB_FREE, create_float_checker(min=0.0))
 
         grid.pack(cbx)

tv/lib/frontends/widgets/search.py
@@ -89,17 +89,17 @@ class InlineSearchMemory(object):
     def __init__(self):
         self._memory = {}
 
-    def set_search(self, type, id, text):
-        self._memory[(type, id)] = text
+    def set_search(self, typ, id_, text):
+        self._memory[(typ, id_)] = text
 
-    def get_search(self, type, id):
-        return self._memory.get((type, id), '')
+    def get_search(self, typ, id_):
+        return self._memory.get((typ, id_), '')
 
-    def forget_search(self, type, id):
+    def forget_search(self, typ, id_):
         # We should call this when channels/playlists get deleted to
         # free up the memory, but it's so small that it's not worth
         # worrying about.
         try:
-            del self._memory[(type, id)]
+            del self._memory[(typ, id_)]
         except KeyError:
             pass

tv/lib/frontends/widgets/tablist.py
@@ -609,8 +609,8 @@ class TabList(signals.SignalEmitter, TabBlinkerMixin):
             iter = self.view.append_tab(info)
         self.iter_map[info.id] = iter
 
-    def set_folder_expanded(self, id, expanded):
-        self.view.set_row_expanded(self.iter_map[id], expanded)
+    def set_folder_expanded(self, id_, expanded):
+        self.view.set_row_expanded(self.iter_map[id_], expanded)
 
     def update(self, info):
         self.init_info(info)

tv/lib/frontends/widgets/watchedfolders.py
@@ -71,13 +71,13 @@ class WatchedFolderManager(signals.SignalEmitter):
             self.model.remove(iter)
         self.emit('changed')
 
-    def change_visible(self, id, visible):
+    def change_visible(self, id_, visible):
         """Change if a watched folder is visible or not."""
-        messages.SetWatchedFolderVisible(id, visible).send_to_backend()
+        messages.SetWatchedFolderVisible(id_, visible).send_to_backend()
 
-    def remove(self, id):
+    def remove(self, id_):
         """Remove a watched folder."""
-        messages.DeleteWatchedFolder(id).send_to_backend()
+        messages.DeleteWatchedFolder(id_).send_to_backend()
 
     def add(self, path):
         """Add a new watched folder.  It will be initially visible."""

commit 416d488a041306cfa19d3494fb5924aff5b1462a

fix #15475 can't reorder playlist (broken since a3173bbf)

tv/lib/frontends/widgets/playlist.py
@@ -55,7 +55,7 @@ class DropHandler(signals.SignalEmitter):
 
     def validate_drop(self, table_view, model, typ, source_actions, parent,
             position):
-        if position != -1 and type == 'downloaded-item':
+        if position != -1 and typ == 'downloaded-item':
             return widgetset.DRAG_ACTION_MOVE
         return widgetset.DRAG_ACTION_NONE
 