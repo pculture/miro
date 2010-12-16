pculture/miro

commit 8e9307cca43350d7cfafc8775c26db99bda5af88

Bug #11890: remove single_feed code from codebase
I left the code in schema79.py on the off-chance that someone is upgrading from
that far in the past.  Everything else was remove, and I dropped the table in
an upgrade.

tv/lib/item.py
@@ -618,7 +618,7 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin, ItemBase):
             "not isContainerItem AND "
             "(deleted IS NULL or not deleted) AND "
             "(is_file_item OR rd.main_item_id=item.id) AND "
-            "(feed.origURL IS NULL OR feed.origURL!= 'dtv:singleFeed') AND "
+            "feed.origURL IS NULL AND "
             "item.file_type='video'",
             joins={'feed': 'item.feed_id=feed.id',
                    'remote_downloader as rd': 'item.downloader_id=rd.id'})
@@ -629,7 +629,7 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin, ItemBase):
             "not isContainerItem AND "
             "(deleted IS NULL or not deleted) AND "
             "(is_file_item OR rd.main_item_id=item.id) AND "
-            "(feed.origURL IS NULL OR feed.origURL!= 'dtv:singleFeed') AND "
+            "feed.origURL IS NULL AND "
             "item.file_type='audio'",
             joins={'feed': 'item.feed_id=feed.id',
                    'remote_downloader as rd': 'item.downloader_id=rd.id'})
@@ -637,9 +637,9 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin, ItemBase):
     @classmethod
     def watchable_other_view(cls):
         return cls.make_view(
-            "(deleted IS NULL OR not deleted) AND "
+            "(deleted IS NULL OR nrot deleted) AND "
             "(is_file_item OR rd.id IS NOT NULL) AND "
-            "(parent_id IS NOT NULL or feed.origURL != 'dtv:singleFeed') AND "
+            "parent_id IS NOT NULL AND "
             "item.file_type='other'",
             joins={'feed': 'item.feed_id=feed.id',
                    'remote_downloader as rd': 'rd.main_item_id=item.id'})
@@ -1969,8 +1969,7 @@ class FileItem(Item):
         else:
             # external item that the user deleted in Miro
             url = self.get_feed_url()
-            if ((url.startswith("dtv:manualFeed")
-                 or url.startswith("dtv:singleFeed"))):
+            if url.startswith("dtv:manualFeed"):
                 self.remove()
             else:
                 self.make_deleted()

tv/lib/databaseupgrade.py
@@ -2887,3 +2887,7 @@ def upgrade124(cursor):
             (repr(list_view_displays), repr(all_active_filters),
             repr(sort_states)))
     cursor.execute("DROP TABLE widgets_frontend_state")
+
+def upgrade125(cursor):
+    """Remove old dtv:singleFeed table."""
+    cursor.execute("DROP TABLE single_feed_impl")

tv/lib/devices.py
@@ -477,7 +477,12 @@ def write_database(mount, database):
         os.makedirs(os.path.join(mount, '.miro'))
     except OSError:
         pass
-    json.dump(database, file(os.path.join(mount, '.miro', 'json'), 'wb'))
+    try:
+        json.dump(database, file(os.path.join(mount, '.miro', 'json'), 'wb'))
+    except IOError:
+        # couldn't write to the device
+        # XXX throw up an error?
+        pass
 
 def device_connected(info):
     """

tv/lib/feed.py
@@ -413,7 +413,7 @@ class Feed(DDBObject, iconcache.IconCacheOwnerMixin):
             for klass in (FeedImpl, RSSFeedImpl, SavedSearchFeedImpl,
                     ScraperFeedImpl, SearchFeedImpl, DirectoryFeedImpl,
                     DirectoryWatchFeedImpl, SearchDownloadsFeedImpl,
-                    ManualFeedImpl, SingleFeedImpl):
+                    ManualFeedImpl):
                 try:
                     self._actualFeed = klass.get_by_id(self.feed_impl_id)
                     self._actualFeed.ufeed = self
@@ -814,9 +814,6 @@ class Feed(DDBObject, iconcache.IconCacheOwnerMixin):
             self.visible = False
         elif self.origURL == u"dtv:manualFeed":
             newFeed = ManualFeedImpl(self)
-            self.visible = False
-        elif self.origURL == u"dtv:singleFeed":
-            newFeed = SingleFeedImpl(self)
             self.visible = False
         elif SEARCH_URL_MATCH_RE.match(self.origURL):
             newFeed = SavedSearchFeedImpl(self.origURL, self)
@@ -2252,20 +2249,6 @@ class ManualFeedImpl(FeedImpl):
     @returns_unicode
     def get_title(self):
         return _('Local Files')
-
-class SingleFeedImpl(FeedImpl):
-    """Single Video that is playing that has been added by the user
-    opening them with democracy.
-    """
-    def setup_new(self, ufeed):
-        FeedImpl.setup_new(self, url=u'dtv:singleFeed', ufeed=ufeed,
-                title=None)
-        self.ufeed.expire = u'never'
-        self.set_update_frequency(-1)
-
-    @returns_unicode
-    def get_title(self):
-        return _('Playing File')
 
 LINK_PATTERN = re.compile("<(a|embed)\s[^>]*(href|src)\s*=\s*\"([^\"]*)\"[^>]*>(.*?)</a(.*)", re.S)
 IMG_PATTERN = re.compile(".*<img\s.*?src\s*=\s*\"(.*?)\".*?>", re.S)

tv/lib/messagehandler.py
@@ -1563,6 +1563,7 @@ New ids: %s""", playlist_item_ids, message.item_ids)
                               [message.item.id]).send_to_frontend()
 
     def handle_device_eject(self, message):
+        devices.write_database(message.device.mount, message.device.database)
         app.device_tracker.eject(message.device)
 
     @staticmethod

tv/lib/models.py
@@ -43,15 +43,14 @@ def initialize():
     global RemoteDownloader, Feed, FeedImpl, RSSFeedImpl
     global SavedSearchFeedImpl, ScraperFeedImpl, SearchFeedImpl
     global DirectoryWatchFeedImpl, DirectoryFeedImpl, SearchDownloadsFeedImpl
-    global ManualFeedImpl, SingleFeedImpl, ChannelFolder, PlaylistFolder
+    global ManualFeedImpl, ChannelFolder, PlaylistFolder
     global PlaylistFolderItemMap, ChannelGuide, Item, FileItem, IconCache
     global SavedPlaylist, PlaylistItemMap, TabOrder, ThemeHistory
 
     from miro.downloader import RemoteDownloader
     from miro.feed import Feed, FeedImpl, RSSFeedImpl, SavedSearchFeedImpl, \
             ScraperFeedImpl, SearchFeedImpl, DirectoryWatchFeedImpl, \
-            DirectoryFeedImpl, SearchDownloadsFeedImpl, ManualFeedImpl, \
-            SingleFeedImpl
+            DirectoryFeedImpl, SearchDownloadsFeedImpl, ManualFeedImpl
     from miro.folder import ChannelFolder, PlaylistFolder, \
             PlaylistFolderItemMap
     from miro.guide import ChannelGuide

tv/lib/schema.py
@@ -355,7 +355,7 @@ from miro.feed import (Feed, FeedImpl, RSSFeedImpl, SavedSearchFeedImpl,
                        ScraperFeedImpl)
 from miro.feed import (SearchFeedImpl, DirectoryWatchFeedImpl,
                        DirectoryFeedImpl, SearchDownloadsFeedImpl)
-from miro.feed import ManualFeedImpl, SingleFeedImpl
+from miro.feed import ManualFeedImpl
 from miro.folder import ChannelFolder, PlaylistFolder, PlaylistFolderItemMap
 from miro.guide import ChannelGuide
 from miro.item import Item, FileItem
@@ -562,11 +562,6 @@ class ManualFeedImplSchema(FeedImplSchema):
     table_name = 'manual_feed_impl'
     # no addition fields over FeedImplSchema
 
-class SingleFeedImplSchema(FeedImplSchema):
-    klass = SingleFeedImpl
-    table_name = 'single_feed_impl'
-    # no addition fields over FeedImplSchema
-
 class RemoteDownloaderSchema(DDBObjectSchema):
     klass = RemoteDownloader
     table_name = 'remote_downloader'
@@ -708,7 +703,7 @@ class DBLogEntrySchema(DDBObjectSchema):
     def handle_malformed_list_view_displays(row):
         return []
 
-VERSION = 124
+VERSION = 125
 object_schemas = [
     IconCacheSchema, ItemSchema, FeedSchema,
     FeedImplSchema, RSSFeedImplSchema, SavedSearchFeedImplSchema,
@@ -716,7 +711,6 @@ object_schemas = [
     SearchFeedImplSchema, DirectoryFeedImplSchema, DirectoryWatchFeedImplSchema,
     SearchDownloadsFeedImplSchema, RemoteDownloaderSchema,
     ChannelGuideSchema, ManualFeedImplSchema,
-    SingleFeedImplSchema,
     PlaylistSchema, ChannelFolderSchema, PlaylistFolderSchema,
     PlaylistItemMapSchema, PlaylistFolderItemMapSchema,
     TabOrderSchema, ThemeHistorySchema, DisplayStateSchema,

tv/lib/schemav79.py
@@ -463,6 +463,9 @@ class ManualFeedImplSchema(FeedImplSchema):
     # no addition fields over FeedImplSchema
 
 class SingleFeedImplSchema(FeedImplSchema):
+    """
+    NB: This type of feed no longer exists as of version 125.
+    """
     klass = None
     classString = 'single-feed-impl'
     # no addition fields over FeedImplSchema

commit 866d08a7812aa5ac0b8bb4c6f323b872c22fc6ec

fix the stuff that broke in 8e9307c

tv/lib/item.py
@@ -618,7 +618,6 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin, ItemBase):
             "not isContainerItem AND "
             "(deleted IS NULL or not deleted) AND "
             "(is_file_item OR rd.main_item_id=item.id) AND "
-            "feed.origURL IS NULL AND "
             "item.file_type='video'",
             joins={'feed': 'item.feed_id=feed.id',
                    'remote_downloader as rd': 'item.downloader_id=rd.id'})
@@ -629,7 +628,6 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin, ItemBase):
             "not isContainerItem AND "
             "(deleted IS NULL or not deleted) AND "
             "(is_file_item OR rd.main_item_id=item.id) AND "
-            "feed.origURL IS NULL AND "
             "item.file_type='audio'",
             joins={'feed': 'item.feed_id=feed.id',
                    'remote_downloader as rd': 'item.downloader_id=rd.id'})
@@ -637,9 +635,8 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin, ItemBase):
     @classmethod
     def watchable_other_view(cls):
         return cls.make_view(
-            "(deleted IS NULL OR nrot deleted) AND "
+            "(deleted IS NULL OR not deleted) AND "
             "(is_file_item OR rd.id IS NOT NULL) AND "
-            "parent_id IS NOT NULL AND "
             "item.file_type='other'",
             joins={'feed': 'item.feed_id=feed.id',
                    'remote_downloader as rd': 'rd.main_item_id=item.id'})