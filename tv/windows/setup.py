022a795e0f42222c42117c882319295aea43cc9d
Will Kahn-Greene <will.guaraldi@pculture.org>
2010-12-22 16:16:20 -0500

    Bug #15586.  Fixes missing CREDITS on Windows.

--- tv/windows/setup.py
+++ tv/windows/setup.py
@@ -270,6 +270,8 @@ for dir in ('searchengines', 'images', 'conversions', 'devices'):
     source_dir = os.path.join(resources_dir, dir)
     data_files.extend(find_data_files(dest_dir, source_dir))
 
+data_files.append(('resources', [os.path.join(root_dir, 'CREDITS')]))
+
 locale_temp_dir = os.path.join(os.path.dirname(__file__), "build", "locale")
 
 def copy_locale_files():
