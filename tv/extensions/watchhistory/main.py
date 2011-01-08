1f2db443b62e5c51e37fdb93edee23b7d7806180
Will Kahn-Greene <will.guaraldi@pculture.org>
2011-01-08 11:20:28 -0500

    Updated copyright, tweaked README.

--- tv/extensions/watchhistory/main.py
+++ tv/extensions/watchhistory/main.py
@@ -1,3 +1,32 @@
+# Miro - an RSS based video player application
+# Copyright (C) 2010, 2011
+# Participatory Culture Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# In addition, as a special exception, the copyright holders give
+# permission to link the code of portions of this program with the OpenSSL
+# library.
+#
+# You must obey the GNU General Public License in all respects for all of
+# the code used other than OpenSSL. If you modify file(s) with this
+# exception, you may extend this exception to your version of the file(s),
+# but you are not obligated to do so. If you do not wish to do so, delete
+# this exception statement from your version. If you delete this exception
+# statement from all source files in the program, then also delete it here.
+
 import logging
 import os
 import time
@@ -21,7 +50,7 @@ class WatchHistory():
         logfile = os.path.join(api.get_support_directory(), "watched.csv")
         fp = open(logfile, "ab")
         self.csv_writer = csv.writer(fp)
-        
+
         # connect to will-play
         app.playback_manager.connect('selecting-file', self.handle_select_file)
         app.playback_manager.connect('will-play', self.handle_will_play)
