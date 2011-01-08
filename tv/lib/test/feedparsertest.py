7f7c045bead906ae2eebaf439557c91d1c9b9533
Will Kahn-Greene <will.guaraldi@pculture.org>
2011-01-07 19:44:09 -0500

    Added feedparser regression tests and test generator.

--- tv/lib/test/feedparsertest.py
+++ tv/lib/test/feedparsertest.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import unittest
+import pprint
 
 from miro import feedparserutil
 from miro.item import FeedParserValues
@@ -11,9 +12,21 @@ FPTESTINPUT = resources.path("testdata/feedparsertests/feeds")
 FPTESTOUTPUT = resources.path("testdata/feedparsertests/output")
 
 def _parse_feed(inputfile):
+    """Runs feedparserutil.parse on the inputfile and returns a dict
+    of feedparser stuff.
+
+    :param inputfile: the name of the input file in the FPTESTINPUT
+        directory.
+
+    :returns: the feedparser dict
+
+    :raises AssertionError: if the feedparser has problems and the
+        feedparser dict has 'bozo_exception' in it.
+    """
     fn = os.path.join(FPTESTINPUT, inputfile)
     d = feedparserutil.parse(fn)
-    assert "bozo_exception" not in d, ("Feed didn't parse: %s" % d["bozo_exception"])
+    assert "bozo_exception" not in d, ("Feed didn't parse: %s %s" %
+                                       (fn, d["bozo_exception"]))
     return d
 
 class FeedParserDictTest(MiroTestCase):
@@ -67,6 +80,21 @@ class FeedParserTest(MiroTestCase):
         # this should kick up a KeyError and NOT a TypeError
         self.assertRaises(KeyError, lambda: d['url'])
 
+# this creates a separate test method in FeedParserTest for each test
+# in the FPTESTINPUT directory.  makes it easier to debug tests that
+# go awry, makes it easier to see test progress from the command line
+# (lots of little tests rather than one big test), and increases the
+# test count appropriately.
+for mem in os.listdir(FPTESTINPUT):
+    def _test(self):
+        d = _parse_feed(mem)
+        fp = open(os.path.join(FPTESTOUTPUT, "%s.output" % mem), "r")
+        output = fp.read()
+        fp.close()
+        self.assertEquals(pprint.pformat(d), output)
+
+    setattr(FeedParserTest, 'test_%s' % mem.replace(".", ""), _test)
+
 class FeedParserValuesTest(unittest.TestCase):
     def test_empty(self):
         fpv = FeedParserValues({})
@@ -104,9 +132,7 @@ class FeedParserValuesTest(unittest.TestCase):
             fpv = FeedParserValues(d.entries[i])
             self.assertEquals(fpv.data["thumbnail_url"], url)
 
-        fn = os.path.join(
-            FPTESTINPUT, "http___feeds_miroguide_com_miroguide_new.xml")
-        d = feedparserutil.parse(fn)
+        d = _parse_feed("http___feeds_miroguide_com_miroguide_new.xml")
 
         for i, url in (
             (0, u"http://s3.miroguide.com/static/media/thumbnails/200x134/14280.jpeg"),
@@ -117,9 +143,7 @@ class FeedParserValuesTest(unittest.TestCase):
 
         # this feed has no thumbnails in the enclosures or in the
         # items.
-        fn = os.path.join(
-            FPTESTINPUT, "http___feeds_feedburner_com_earth-touch_podcast_720p.xml")
-        d = feedparserutil.parse(fn)
+        d = _parse_feed("http___feeds_feedburner_com_earth-touch_podcast_720p.xml")
 
         for i, url in (
             (0, None),
@@ -130,9 +154,7 @@ class FeedParserValuesTest(unittest.TestCase):
 
         # this feed has no thumbnails in the enclosures or in the
         # items.  it's an audio podcast.
-        fn = os.path.join(
-            FPTESTINPUT, "http___feeds_thisamericanlife_org_talpodcast.xml")
-        d = feedparserutil.parse(fn)
+        d = _parse_feed("http___feeds_thisamericanlife_org_talpodcast.xml")
 
         for i, url in (
             (0, None),):
@@ -144,9 +166,7 @@ class FeedParserValuesTest(unittest.TestCase):
         # <enclosure>
         #    <thumbnail>
         #        <href>http...</href>
-        fn = os.path.join(
-            FPTESTINPUT, "http___vodo_net_feeds_promoted.xml")
-        d = feedparserutil.parse(fn)
+        d = _parse_feed("http___vodo_net_feeds_promoted.xml")
 
         for i, url in (
             (0, u'http://vodo.net/media/thumbnails/work_127_pioneerone_uploaded.jpeg'),
@@ -160,10 +180,7 @@ class FeedParserValuesTest(unittest.TestCase):
         # <item>
         #    <thumbnail>
         #       <href>http:...</href>
-        fn = os.path.join(
-            FPTESTINPUT, "http___www_linktv_org_rss_hq_globalpulse.xml")
-
-        d = feedparserutil.parse(fn)
+        d = _parse_feed("http___www_linktv_org_rss_hq_globalpulse.xml")
 
         for i, url in (
             (0, u'http://www.linktv.org/sitecontent/videothumbs/globalpulse20100709.jpg'),
---
+++ tv/resources/testdata/feedparsertests/testgenerator.py
@@ -0,0 +1,83 @@
+# Miro - an RSS based video player application
+# Copyright (C) 2011
+# Participatory Culture Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# In addition, as a special exception, the copyright holders give
+# permission to link the code of portions of this program with the OpenSSL
+# library.
+#
+# You must obey the GNU General Public License in all respects for all of
+# the code used other than OpenSSL. If you modify file(s) with this
+# exception, you may extend this exception to your version of the file(s),
+# but you are not obligated to do so. If you do not wish to do so, delete
+# this exception statement from your version. If you delete this exception
+# statement from all source files in the program, then also delete it here.
+
+"""
+Generates feedparser tests.
+"""
+import sys
+import os
+import pprint
+
+USAGE = "testgenerator.py <input-dir> <output-dir>"
+
+def run_parser(feedparser, inputdir, outputdir, mem):
+    output = feedparser.parse(os.path.join(inputdir, mem))
+    f = open(os.path.join(outputdir, "%s.output" % mem), "w")
+    f.write(pprint.pformat(output))
+    f.close()
+
+def main(argv):
+    print "Feedparser test generator of awesome: version 1.0"
+    if len(argv) < 2:
+        print USAGE
+        return 1
+
+    inputdir = os.path.abspath(argv[0])
+    outputdir = os.path.abspath(argv[1])
+
+    sys.path.insert(
+        0,
+        os.path.join(
+            os.pardir,
+            os.pardir,
+            os.pardir,
+            "lib"))
+
+    import feedparser
+
+    inputfiles = os.listdir(inputdir)
+    print "%s total input files" % len(inputfiles)
+    outputfiles = os.listdir(outputdir)
+    print "%s total output files" % len(outputfiles)
+
+    print ""
+
+    to_generate = [m for m in inputfiles if "%s.output" % m not in outputfiles]
+    if not to_generate:
+        print "No files to generate."
+    else:
+        print "Generating %s files:" % len(to_generate)
+        for i, mem in enumerate(to_generate):
+            print "%s: Generating output for %s" % (i, mem)
+            run_parser(feedparser, inputdir, outputdir, mem)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
