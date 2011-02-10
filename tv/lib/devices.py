pculture/miro

commit d715d52a4c20c5bbfa569affc8251f9ed5b4d254

Catch datetime.fromtimestamp exceptions. Resolves most of #16220.

tv/lib/item.py
@@ -2093,7 +2093,7 @@ class FileItem(Item):
         try:
             self.releaseDateObj = datetime.fromtimestamp(
                 fileutil.getmtime(self.filename))
-        except OSError:
+        except OSError, ValueError:
             logging.warn("Error setting release date:\n%s",
                     traceback.format_exc())
             self.releaseDateObj = datetime.now()

tv/lib/itemsource.py
@@ -30,6 +30,7 @@
 import datetime
 import logging
 import os.path
+import traceback
 
 from miro import app
 from miro import database
@@ -399,7 +400,7 @@ class SharingItemSource(ItemSource):
             feed_url = None,
             description = item.description,
             state = u'saved',
-            release_date = datetime.datetime.fromtimestamp(item.release_date),
+            release_date = item.get_release_date_obj(),
             size = item.size,
             duration = (item.duration not in (-1, None) and
                         item.duration / 1000 or 0),
@@ -533,7 +534,7 @@ class DeviceItemSource(ItemSource):
             feed_url = None,
             description = item.description,
             state = u'saved',
-            release_date = datetime.datetime.fromtimestamp(item.release_date),
+            release_date = item.get_release_date_obj(),
             size = item.size,
             duration = (item.duration not in (-1, None) and
                         item.duration / 1000 or 0),

commit 3e0828f9573439711827d9db4be4c9dc2f95b166

I think this fixes the sharing and devices breakage from d715d52

tv/lib/devices.py
@@ -656,6 +656,9 @@ class DeviceItem(object):
     def id_exists():
         return True
 
+    def get_release_date(self):
+        return self.release_date
+
     @returns_filename
     def get_filename(self):
         return os.path.join(self.device.mount, self.video_path)

tv/lib/item.py
@@ -1641,9 +1641,9 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
     def get_pub_date_parsed(self):
         """Returns the published date of the item as a datetime object.
         """
-        return self.get_release_date_obj()
+        return self.get_release_date()
 
-    def get_release_date_obj(self):
+    def get_release_date(self):
         """Returns the date this video was released or when it was
         published.
         """
@@ -2098,7 +2098,7 @@ class FileItem(Item):
                     traceback.format_exc())
             self.releaseDateObj = datetime.now()
 
-    def get_release_date_obj(self):
+    def get_release_date(self):
         if self.parent_id:
             return self.get_parent().releaseDateObj
         else:

tv/lib/itemsource.py
@@ -157,7 +157,7 @@ class DatabaseItemSource(ItemSource):
             'feed_url': item.get_feed_url(),
             'description': item.get_description(),
             'state': item.get_state(),
-            'release_date': item.get_release_date_obj(),
+            'release_date': item.get_release_date(),
             'size': item.get_size(),
             'duration': item.get_duration_value(),
             'resume_time': item.resumeTime,
@@ -400,7 +400,7 @@ class SharingItemSource(ItemSource):
             feed_url = None,
             description = item.description,
             state = u'saved',
-            release_date = item.get_release_date_obj(),
+            release_date = item.get_release_date(),
             size = item.size,
             duration = (item.duration not in (-1, None) and
                         item.duration / 1000 or 0),
@@ -534,7 +534,7 @@ class DeviceItemSource(ItemSource):
             feed_url = None,
             description = item.description,
             state = u'saved',
-            release_date = item.get_release_date_obj(),
+            release_date = item.get_release_date(),
             size = item.size,
             duration = (item.duration not in (-1, None) and
                         item.duration / 1000 or 0),

tv/lib/sharing.py
@@ -166,6 +166,9 @@ class SharingItem(object):
     def id_exists():
         return True
 
+    def get_release_date(self):
+        return self.release_date
+
     @returns_filename
     def get_filename(self):
         # For daap, sent it to be the same as http as it is basically