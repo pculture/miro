f37e029a9ff6314545572b2782ff65bf0e56d26e
Ben Dean-Kawamura <ben@pculture.org>
2011-02-15 16:58:20 -0500

    Fix for upgrade 143
    I used the wrong column type.  It's a bit messy changing a db upgrade after
    it's been released, but I don't think this one should cause many problems.
    Especially since the upgrade was pushed out today.

--- tv/lib/databaseupgrade.py
+++ tv/lib/databaseupgrade.py
@@ -3136,4 +3136,4 @@ def upgrade142(cursor):
 def upgrade143(cursor):
     """Move sort_state from view_state to display_state"""
     remove_column(cursor, 'view_state', ['sort_state'])
-    cursor.execute("ALTER TABLE display_state ADD COLUMN sort_state pythonrepr")
+    cursor.execute("ALTER TABLE display_state ADD COLUMN sort_state text")
