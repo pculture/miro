pculture/miro

commit 2b2160473287d3fd55cdbf639f7c0c771a3ab648

shared API for metadata-owning objects

tv/lib/devices.py
@@ -47,6 +47,7 @@ from miro import messages
 from miro import signals
 from miro import conversions
 from miro import moviedata
+from miro import metadata
 from miro.util import returns_filename
 
 from miro.plat import resources
@@ -616,7 +617,7 @@ class DeviceSyncManager(object):
         for key in self.waiting:
             conversions.conversion_manager.cancel(key)
 
-class DeviceItem(object):
+class DeviceItem(metadata.Store):
     """
     An item which lives on a device.  There's a separate, per-device JSON
     database, so this implements the necessary Item logic for those files.
@@ -626,7 +627,7 @@ class DeviceItem(object):
             if required not in kwargs:
                 raise TypeError('DeviceItem must be given a "%s" argument'
                                 % required)
-        self.name = self.file_format = self.size = None
+        self.file_format = self.size = None
         self.release_date = self.feed_name = self.feed_id = None
         self.keep = self.media_type_checked = True
         self.isContainerItem = False
@@ -636,19 +637,10 @@ class DeviceItem(object):
         self.duration = self.screenshot = self.thumbnail_url = None
         self.resumeTime = 0
         self.subtitle_encoding = self.enclosure_type = None
-        self.description = u''
-        self.album = None
-        self.artist = None
-        self.title_tag = None
-        self.track = None
-        self.year = None
-        self.genre = None
-        self.rating = None
         self.file_type = None
         self.creation_time = None
         self.is_playing = False
-        self.has_drm = None
-        self.metadata_version = 0
+        metadata.Store.setup_new(self)
         self.__dict__.update(kwargs)
 
         if isinstance(self.video_path, unicode):
@@ -740,12 +732,6 @@ class DeviceItem(object):
                     self.screenshot = os.path.join('.miro', basename)
             elif screenshot.startswith(resources.root()):
                 self.screenshot = None # don't save a default thumbnail
-
-    def drm_description(self):
-        if self.has_drm:
-            return _("Locked")
-        else:
-            return u""
 
     def remove(self, save=True):
         file_types = [self.file_type]

tv/lib/databaseupgrade.py
@@ -3144,3 +3144,6 @@ def upgrade144(cursor):
 def upgrade145(cursor):
     cursor.execute("ALTER TABLE display_state "
             "ADD COLUMN last_played_item_id integer")
+
+def upgrade146(cursor):
+    cursor.execute("ALTER TABLE item ADD COLUMN album_tracks integer")

tv/lib/frontends/widgets/style.py
@@ -1034,7 +1034,7 @@ class LastPlayedRenderer(ListViewRendererText):
     attr_name = 'display_last_played'
 
 class DRMRenderer(ListViewRendererText):
-    attr_name = 'drm_description'
+    attr_name = 'display_drm'
 
 class ListViewRenderer(widgetset.InfoListRenderer):
     """Renderer for more complex list view columns.

tv/lib/item.py
@@ -62,6 +62,7 @@ from miro import searchengines
 from miro import fileutil
 from miro import search
 from miro import models
+from miro import metadata
 
 _charset = locale.getpreferredencoding()
 
@@ -313,7 +314,7 @@ class FeedParserValues(object):
 
         return datetime.min
 
-class Item(DDBObject, iconcache.IconCacheOwnerMixin):
+class Item(DDBObject, iconcache.IconCacheOwnerMixin, metadata.Store):
     """An item corresponds to a single entry in a feed.  It has a
     single url associated with it.
     """
@@ -322,6 +323,7 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
 
     def setup_new(self, fp_values, linkNumber=0, feed_id=None, parent_id=None,
             eligibleForAutoDownload=True, channel_title=None):
+        metadata.Store.setup_new(self)
         self.is_file_item = False
         self.feed_id = feed_id
         self.parent_id = parent_id
@@ -333,12 +335,10 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
         self.downloadedTime = None
         self.watchedTime = None
         self.pendingReason = u""
-        self.title = u""
-        self.description = u""
         fp_values.update_item(self)
         self.expired = False
         self.keep = False
-        self.filename = self.file_type = None
+        self.filename = None
         self.eligibleForAutoDownload = eligibleForAutoDownload
         self.duration = None
         self.screenshot = None
@@ -349,19 +349,8 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
         self.was_downloaded = False
         self.subtitle_encoding = None
         self.setup_new_icon_cache()
-        self.album = None
-        self.album_artist = None
-        self.artist = None
-        self.title_tag = None
-        self.track = None
-        self.year = None
-        self.genre = None
-        self.rating = None
         self.play_count = 0
         self.skip_count = 0
-        self.cover_art = None
-        self.has_drm = None
-        self.metadata_version = 0
         # Initalize FileItem attributes to None
         self.deleted = self.shortFilename = self.offsetPath = None
 
@@ -780,10 +769,6 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
         if not self.media_type_checked:
             self.file_type = self._file_type_for_filename(filename)
 
-    def set_file_type(self, file_type):
-        self.file_type = file_type
-        self.signal_change()
-
     def _file_type_for_filename(self, filename):
         filename = filename.lower()
         for ext in filetypes.VIDEO_EXTENSIONS:
@@ -950,12 +935,6 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
             return Item.media_children_view(self.id).count() > 0
         else:
             return self.file_type in ('audio', 'video') and not self.has_drm
-
-    def drm_description(self):
-        if self.has_drm:
-            return _("Locked")
-        else:
-            return u""
 
     def set_feed(self, feed_id):
         """Moves this item to another feed.
@@ -1170,11 +1149,6 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
         self.skip_count += 1
         self.signal_change()
 
-    def set_rating(self, rating):
-        self.confirm_db_thread()
-        self.rating = rating
-        self.signal_change()
-
     @returns_unicode
     def get_rss_id(self):
         self.confirm_db_thread()
@@ -1281,7 +1255,10 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
         to signal the right set of items.
         """
         self.confirm_db_thread()
-        if self.icon_cache is not None and self.icon_cache.is_valid():
+        if self.cover_art:
+            path = self.cover_art
+            return resources.path(fileutil.expand_filename(path))
+        elif self.icon_cache is not None and self.icon_cache.is_valid():
             path = self.icon_cache.get_filename()
             return resources.path(fileutil.expand_filename(path))
         elif self.screenshot:
@@ -1289,9 +1266,6 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
             return resources.path(fileutil.expand_filename(path))
         elif self.isContainerItem:
             return resources.path("images/thumb-default-folder.png")
-        elif self.cover_art:
-            path = self.cover_art
-            return resources.path(fileutil.expand_filename(path))
         else:
             feed = self.get_feed()
             if feed.thumbnail_valid():
@@ -1302,37 +1276,6 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
             else:
                 return resources.path("images/thumb-default-video.png")
 
-    @returns_unicode
-    def get_album_artist(self):
-        return self.album_artist
-
-    @returns_unicode
-    def get_artist(self):
-        return self.artist
-
-    @returns_unicode
-    def get_album(self):
-        return self.album
-
-    def get_track(self):
-        return self.track
-
-    def get_year(self):
-        return self.year
-
-    @returns_unicode
-    def get_genre(self):
-        return self.genre
-
-    def get_rating(self):
-        return self.rating
-
-    def get_title_tag(self):
-        return self.title_tag
-
-    def get_cover_art(self):
-        return self.cover_art
-
     def is_downloaded_torrent(self):
         return (self.isContainerItem and self.has_downloader() and
                 self.downloader.is_finished())
@@ -1341,10 +1284,9 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
     def get_title(self):
         """Returns the title of the item.
         """
-        if self.title:
-            return self.title
-        if self.title_tag:
-            return self.title_tag
+        stored = metadata.Store.get_title(self)
+        if stored:
+            return stored
         if self.entry_title is not None:
             return self.entry_title
         if self.get_filename() is not None:
@@ -1362,16 +1304,6 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
             t2.append(word.capitalize())
         title = u' '.join(t2)
         return title
-
-    def set_title(self, title):
-        self.confirm_db_thread()
-        self.title = title
-        self.signal_change()
-
-    def set_description(self, desc):
-        self.confirm_db_thread()
-        self.description = desc
-        self.signal_change()
 
     def set_channel_title(self, title):
         check_u(title)
@@ -1400,8 +1332,9 @@ class Item(DDBObject, iconcache.IconCacheOwnerMixin):
 
         If the item is a torrent, then it adds some additional text.
         """
-        if self.description:
-            return unicode(self.description)
+        stored = metadata.Store.get_description(self)
+        if stored:
+            return stored
 
         if self.entry_description:
             return unicode(self.entry_description)

tv/lib/itemsource.py
@@ -149,11 +149,9 @@ class DatabaseItemSource(ItemSource):
     @staticmethod
     def _item_info_for(item):
         info = {
-            'name': item.get_title(),
             'feed_id': item.feed_id,
             'feed_name': item.get_source(),
             'feed_url': item.get_feed_url(),
-            'description': item.get_description(),
             'state': item.get_state(),
             'release_date': item.get_release_date(),
             'size': item.get_size(),
@@ -183,14 +181,6 @@ class DatabaseItemSource(ItemSource):
             'media_type_checked': item.media_type_checked,
             'seeding_status': item.torrent_seeding_status(),
             'mime_type': item.enclosure_type,
-            'album_artist': item.get_album_artist(),
-            'artist': item.get_artist(),
-            'album': item.get_album(),
-            'track': item.get_track(),
-            'year': item.get_year(),
-            'genre': item.get_genre(),
-            'title_tag': item.get_title_tag(),
-            'rating': item.get_rating(),
             'date_added': item.get_creation_time(),
             'last_played': item.get_watched_time(),
             'children': [],
@@ -208,12 +198,10 @@ class DatabaseItemSource(ItemSource):
             'source_type': 'database',
             'play_count': item.play_count,
             'skip_count': item.skip_count,
-            'cover_art': item.get_cover_art(),
             'auto_rating': item.get_auto_rating(),
             'is_playing': item.is_playing(),
-            'has_drm': item.has_drm,
-            'drm_description': item.drm_description(),
             }
+        info.update(item.get_iteminfo_metadata())
         if item.isContainerItem:
             info['children'] = [DatabaseItemSource._item_info_for(i) for i in
                                 item.get_children()]
@@ -391,14 +379,12 @@ class SharingItemSource(ItemSource):
         ]
 
     def _item_info_for(self, item):
-        return messages.ItemInfo(
+        info = dict(
             item.id,
             source_type='sharing',
-            name = item.name,
             feed_id = item.feed_id,
             feed_name = None,
             feed_url = None,
-            description = item.description,
             state = u'saved',
             release_date = item.get_release_date(),
             size = item.size,
@@ -432,12 +418,6 @@ class SharingItemSource(ItemSource):
             media_type_checked = True,
             mime_type = item.enclosure_type,
             artist = item.artist,
-            album = item.album,
-            track = item.track,
-            year = item.year,
-            genre = item.genre,
-            title_tag = item.title_tag,
-            rating = item.rating,
             auto_rating = None,
             date_added = item.get_creation_time(),
             last_played = item.get_creation_time(),
@@ -453,12 +433,11 @@ class SharingItemSource(ItemSource):
             up_down_ratio = 0,
             play_count=0,
             skip_count=0,
-            cover_art=None,
             host=item.host,
             port=item.port,
-            has_drm=item.has_drm,
-            drm_description=item.drm_description(),
             is_playing=False)
+        info.update(item.get_iteminfo_metadata())
+        return messages.ItemInfo(*info)
 
     def _ensure_info(self, obj):
         if not isinstance(obj, messages.ItemInfo):
@@ -523,15 +502,13 @@ class DeviceItemSource(ItemSource):
         self.emit("removed", item.id)
 
     def _item_info_for(self, item):
-        return messages.ItemInfo(
+        info = dict(
             item.id,
             source_type='device',
-            name = item.name,
             feed_id = item.feed_id,
             feed_name = (item.feed_name is None and item.feed_name or
                          self.device.name),
             feed_url = None,
-            description = item.description,
             state = u'saved',
             release_date = item.get_release_date(),
             size = item.size,
@@ -565,12 +542,6 @@ class DeviceItemSource(ItemSource):
             seeding_status = None,
             mime_type = item.enclosure_type,
             artist = item.artist,
-            album = item.album,
-            track = item.track,
-            year = item.year,
-            genre = item.genre,
-            title_tag = item.title_tag,
-            rating = item.rating,
             date_added = item.get_creation_time(),
             last_played = item.get_creation_time(),
             download_info = None,
@@ -585,11 +556,10 @@ class DeviceItemSource(ItemSource):
             up_down_ratio = 0,
             play_count=0,
             skip_count=0,
-            cover_art=None,
             auto_rating=0,
-            has_drm=item.has_drm,
-            drm_description=item.drm_description(),
             is_playing=item.is_playing)
+        info.update(item.get_iteminfo_metadata())
+        return messages.ItemInfo(info)
 
     def fetch_all(self):
         return [self._item_info_for(devices.DeviceItem(

tv/lib/messages.py
@@ -1196,6 +1196,7 @@ class ItemInfo(object):
         self.display_track = displaytext.integer(self.track)
         self.display_year = displaytext.integer(self.year)
         self.display_torrent_details = self.calc_torrent_details()
+        self.display_drm = self.has_drm and _("Locked") or u""
 
         if self.state == 'downloading':
             dl_info = self.download_info

tv/lib/metadata.py
@@ -1,0 +1,181 @@
+# Miro - an RSS based video player application
+# Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011
+# Participatory Culture Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# In addition, as a special exception, the copyright holders give
+# permission to link the code of portions of this program with the OpenSSL
+# library.
+#
+# You must obey the GNU General Public License in all respects for all of
+# the code used other than OpenSSL. If you modify file(s) with this
+# exception, you may extend this exception to your version of the file(s),
+# but you are not obligated to do so. If you do not wish to do so, delete
+# this exception statement from your version. If you delete this exception
+# statement from all source files in the program, then also delete it here.
+
+"""``miro.metadata`` -- Handle metadata properties for *Items. Generally the
+frontend cares about these and the backend doesn't.
+"""
+
+import logging
+import fileutil
+
+from miro.gtcache import gettext as _
+from miro.util import returns_unicode
+from miro import coverart
+
+class Source(object):
+    """Object with readable metadata properties."""
+
+    def get_iteminfo_metadata(self):
+        return dict(
+            name = self.get_title(),
+            title_tag = self.title_tag,
+            description = self.description,
+            album = self.album,
+            album_artist = self.album_artist,
+            artist = self.artist,
+            track = self.track,
+            album_tracks = self.album_tracks,
+            year = self.year,
+            genre = self.genre,
+            rating = self.rating,
+            cover_art = self.cover_art,
+            has_drm = self.has_drm,
+        )
+
+    def setup_new(self):
+        self.title = u""
+        self.title_tag = None
+        self.description = u""
+        self.album = None
+        self.album_artist = None
+        self.artist = None
+        self.track = None
+        self.album_tracks = None
+        self.year = None
+        self.genre = None
+        self.rating = None
+        self.cover_art = None
+        self.has_drm = None
+        self.file_type = None
+        self.metadata_version = 0
+
+    @returns_unicode
+    def get_title(self):
+        if self.title:
+            return self.title
+        else:
+            return self.title_tag
+
+def metadata_setter(attribute, type_=None):
+    def set_metadata(self, value, _bulk=False):
+        if value is not None and type_ is not None:
+            # None is always an acceptable value for metadata properties
+            value = type_(value)
+        if not _bulk:
+            self.confirm_db_thread()
+        self.__dict__[attribute] = value
+        if not _bulk:
+            self.signal_change()
+            self.write_back((attribute,))
+    return set_metadata
+
+class Store(Source):
+    """Object with read/write metadata properties."""
+
+    set_title = metadata_setter('title', unicode)
+    set_title_tag = metadata_setter('title_tag', unicode)
+    set_description = metadata_setter('description', unicode)
+    set_album = metadata_setter('album', unicode)
+    set_album_artist = metadata_setter('album_artist', unicode)
+    set_artist = metadata_setter('artist', unicode)
+    set_track = metadata_setter('track', int)
+    set_album_tracks = metadata_setter('album_tracks')
+    set_year = metadata_setter('year', int)
+    set_genre = metadata_setter('genre', unicode)
+    set_rating = metadata_setter('rating', int)
+    set_file_type = metadata_setter('file_type', unicode)
+    set_has_drm = metadata_setter('has_drm', bool)
+
+    def set_cover_art(self, new_file, _bulk=False):
+        """Set new cover art. Deletes any old cover art.
+
+        Creates a copy of the image in our cover art directory.
+        """
+        if not _bulk:
+            self.confirm_db_thread()
+        if new_file:
+            new_cover = coverart.Image.from_file(new_file, self.get_filename())
+        self.delete_cover_art()
+        if new_file:
+            self.cover_art = new_cover
+        if not _bulk:
+            self.signal_change()
+            self.write_back(('cover_art',))
+
+    def delete_cover_art(self):
+        """Delete the cover art file and unset cover_art."""
+        try:
+            fileutil.remove(self.cover_art)
+        except (OSError, TypeError):
+            pass
+        self.cover_art = None
+
+    def setup_new(self):
+        Source.setup_new(self)
+        self._deferred_update = {}
+
+    def set_metadata_from_iteminfo(self, changes, _deferrable=True):
+        self.confirm_db_thread()
+        for field, value in changes.iteritems():
+            Store.ITEM_INFO_TO_ITEM[field](self, value, _bulk=True)
+        self.signal_change()
+        self.write_back(changes.keys())
+
+    def write_back(self, _changed):
+        """Write back metadata changes to the original source, if supported. If
+        this method fails because the item is playing, it should add the changed
+        fields to _deferred_update.
+        """
+        logging.debug("%s can't write back changes", self.__class__.__name__)
+
+    def set_is_playing(self, playing):
+        """Hook so that we can defer updating an item's data if we can't change
+        it while it's playing.
+        """
+        if not playing and self._deferred_update:
+            self.set_metadata_from_iteminfo(self._deferred_update, _deferrable=False)
+            self._deferred_update = {}
+        super(Store, self).set_is_playing(playing)
+
+    ITEM_INFO_TO_ITEM = dict(
+        name = set_title,
+        title_tag = set_title_tag,
+        description = set_description,
+        album = set_album,
+        album_artist = set_album_artist,
+        artist = set_artist,
+        track = set_track,
+        album_tracks = set_album_tracks,
+        year = set_year,
+        genre = set_genre,
+        rating = set_rating,
+        file_type = set_file_type,
+        cover_art = set_cover_art,
+        has_drm = set_has_drm,
+    )

tv/lib/moviedata.py
@@ -71,13 +71,15 @@ TAG_MAP = {
     'drm': ('itunmovi',),
     'title': ('tit2', 'title', u'\uFFFDnam'),
     'track': ('trck', 'tracknumber'),
+    'album_tracks': (),
     'year': ('tdrc', 'tyer', 'date', 'year'),
     'genre': ('genre', 'tcon', 'providerstyle', u'\uFFFDgen'),
     'cover-art': ('\uFFFDart', 'apic', 'covr'),
 }
 TAG_TYPES = {
     'album': unicode, 'album_artist': unicode, 'artist': unicode, 'drm': bool,
-    'title': unicode, 'track': int, 'year': int, 'genre': unicode,
+    'title': unicode, 'track': int, 'album_tracks': int, 'year': int,
+    'genre': unicode,
 }
 NOFLATTEN_TAGS = ('cover-art',)
 

commit 2f9d87055d88d8e5ebf546666efcb514cbea4202

2b21604 broke some device item name/title handling

tv/lib/devices.py
@@ -500,9 +500,9 @@ class DeviceSyncManager(object):
             if (item_info.file_url and
                 existing.get('url') == item_info.file_url):
                 return True
-            elif ((item_info.name, item_info.description, item_info.size,
+            elif ((item_info.title, item_info.description, item_info.size,
                    item_info.duration) ==
-                  (existing.get('name'), existing.get('description'),
+                  (existing.get('title'), existing.get('description'),
                    existing.get('size'), existing.get('duration'))):
                 # if a bunch of qualities are the same, we'll call it close
                 # enough
@@ -647,8 +647,8 @@ class DeviceItem(metadata.Store):
             self.video_path = utf8_to_filename(self.video_path.encode('utf8'))
         if isinstance(self.screenshot, unicode):
             self.screenshot = utf8_to_filename(self.screenshot.encode('utf8'))
-        if self.name is None:
-            self.name = filename_to_unicode(os.path.basename(self.video_path))
+        if self.title is None:
+            self.title = filename_to_unicode(os.path.basename(self.video_path))
         if self.file_format is None:
             self.file_format = filename_to_unicode(
                 os.path.splitext(self.video_path)[1])