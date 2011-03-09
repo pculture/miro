c5354f19899e0f2f8865377e4cb7dd1beb186bd3
Geoffrey Lee <glee@pculture.org>
2011-03-09 22:09:41 +0900

    Make sure in resume policy we pick up manual feed too.

--- tv/lib/frontends/widgets/playback.py
+++ tv/lib/frontends/widgets/playback.py
@@ -246,7 +246,8 @@ class PlaybackManager (signals.SignalEmitter):
         currently_playing = self.playlist.currently_playing
         if (currently_playing.remote or 
           (currently_playing.feed_url and 
-          currently_playing.feed_url.startswith('dtv:directoryfeed'))):
+          (currently_playing.feed_url.startswith('dtv:manualFeed') or
+           currently_playing.feed_url.startswith('dtv:directoryfeed')))):
             if(self.playlist.currently_playing.file_type == u'video'):
                 resume = app.config.get(prefs.RESUME_VIDEOS_MODE)
             else:
