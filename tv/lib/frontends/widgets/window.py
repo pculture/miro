cda0fbec251e5a835725d3d4a9b06232c8b70f7b
Geoffrey Lee <glee@pculture.org>
2011-03-19 12:06:17 -0400

    #16285 prettier colors for border on sidebar.

--- tv/lib/frontends/widgets/window.py
+++ tv/lib/frontends/widgets/window.py
@@ -46,14 +46,24 @@ class MiroWindow(widgetset.MainWindow):
     def __init__(self, title, rect):
         widgetset.MainWindow.__init__(self, title, rect)
 
+        GREY = (0.85, 0.85, 0.85)
         vbox = widgetset.VBox()
-        vbox.pack_start(separator.HSeparator(widgetutil.BLACK))
+        vbox.pack_start(separator.HSeparator(GREY))
         vbox.pack_start(tablist.TabListBox(), expand=True)
 
+        hbox = widgetset.HBox()
+        hbox.pack_start(vbox, expand=True)
+        hbox.pack_start(separator.VSeparator(GREY))
+
         self.main_area_holder = widgetutil.WidgetHolder()
+
+        main_hbox = widgetset.HBox()
+        main_hbox.pack_start(separator.VSeparator(GREY))
+        main_hbox.pack_start(self.main_area_holder, expand=True)
+        
         self.splitter = widgetset.Splitter()
-        self.splitter.set_left(vbox)
-        self.splitter.set_right(self.main_area_holder)
+        self.splitter.set_left(hbox)
+        self.splitter.set_right(main_hbox)
 
         hbox = widgetset.HBox()
         self.videobox = videobox.VideoBox()
