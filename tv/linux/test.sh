7d45bf5ba2960fa7605ec064f8d0626285bfdbb9
Will Kahn-Greene <will.guaraldi@pculture.org>
2011-04-15 19:33:45 -0400

    Hacky fix for test.sh not taking multiple args
    I didn't feel like chasing this down, but test.sh doesn't pass all the
    command line arguments to run.sh.  So doing something like:
    
        ./test.sh -v moviedatatest
    
    passes the -v to run.sh and totally loses moviedatatest.
    
    This fixes that in a hacky way by explicitly passing arguments, but it
    only passes the first three because that's as many as I pass.
    
    It should get fixed for real at some point.

--- tv/linux/test.sh
+++ tv/linux/test.sh
@@ -35,4 +35,4 @@
 
 # this comes from http://www.estamos.de/blog/2009/05/08/running-syncevolution-as-cron-job/
 # env `dbus-launch` sh -c 'trap "kill $DBUS_SESSION_BUS_PID" EXIT; ./run.sh --unittest utiltest > /home/pcf/test_output.txt 1>&2' || true
-env `dbus-launch` bash -c "trap \"kill $DBUS_SESSION_BUS_PID\" EXIT; ./run.sh --unittest $@"
+env `dbus-launch` bash -c "trap \"kill $DBUS_SESSION_BUS_PID\" EXIT; ./run.sh --unittest $1 $2 $3"
