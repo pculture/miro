2d2ac36c02e5fcd5b17e13a56220a6e2f4691413
Will Kahn-Greene <will.guaraldi@pculture.org>
2011-05-15 15:11:55 -0400

    17391. Reworks search page in first time dialog
    Overhauls search page in first time dialog so that it immediately
    starts searching when the user clicks "Next >" to the page and also
    has a cleaner ui for users to understand the different searching
    states (searching, finished searching, cancelled search).
    
    This also cleans up some odd behavior when going back and forth between
    pages.  Now if you click on the No radio button in the Find Files page,
    it clears the gather_media_files list.
    
    If the user cancels the search, then it now clears the
    gathered_media_files list.

--- tv/lib/frontends/widgets/firsttimedialog.py
+++ tv/lib/frontends/widgets/firsttimedialog.py
@@ -83,8 +83,12 @@ class FirstTimeDialog(widgetset.DialogWindow):
         self.finder = None
 
         self.cancelled = False
-
         self.gathered_media_files = None
+        self.progress_bar = None
+        self.progress_label = None
+        self.search_cancel_button = None
+        self.search_prev_button = None
+        self.search_next_button = None
 
         self._done_firsttime_callback = done_firsttime_callback
 
@@ -109,7 +113,6 @@ class FirstTimeDialog(widgetset.DialogWindow):
         if self._has_media_player:
             pages.append(self.build_media_player_import_page())
 
-        pages = [widgetutil.pad(page) for page in pages]
         for page in pages:
             page.set_size_request(WIDTH - 40, HEIGHT - 40)
         return pages
@@ -135,6 +138,14 @@ class FirstTimeDialog(widgetset.DialogWindow):
             self._pages = self.build_pages()
         self._page_box.pack_start(self._pages[i], expand=True)
         self._page_index = i
+
+        # when we switch to the search page, it needs to kick off a
+        # search.  since we build the pages when the dialog is
+        # created, i needed a way to specify a function that runs when
+        # we switch to that page.  so that's why i'm doing this
+        # totally goofy thing.
+        if hasattr(self._pages[i], "run_me_on_switch"):
+            self._pages[i].run_me_on_switch()
 
     def this_page(self, rebuild=False):
         self._switch_page(self._page_index, rebuild)
@@ -211,6 +222,8 @@ class FirstTimeDialog(widgetset.DialogWindow):
                     widgetutil.build_hbox((next_button,)))),
             expand=True)
 
+        vbox = widgetutil.pad(vbox)
+
         return vbox
 
     def build_startup_page(self):
@@ -257,13 +270,11 @@ class FirstTimeDialog(widgetset.DialogWindow):
                     widgetutil.build_hbox((prev_button, next_button)))),
             expand=True)
 
+        vbox = widgetutil.pad(vbox)
+
         return vbox
 
     def build_import_page(self):
-        # NOTE: This is essentially duplicated in importmediadialog.
-        # There are minor differences, so it couldn't really be reused
-        # (which is annoying).
-
         vbox = widgetset.VBox(spacing=5)
 
         vbox.pack_start(_build_title(_("Finding Files")))
@@ -335,7 +346,7 @@ class FirstTimeDialog(widgetset.DialogWindow):
 
         search_button = widgetset.Button(_("Search"))
         search_button.connect('clicked', handle_search_finish_clicked)
-        search_button.text_faces = {"search": _("Search")}
+        search_button.text_faces = {"search": _("Next >")}
         if not self._has_media_player:
             search_button.text_faces["next"] = _("Finish")
         else:
@@ -361,6 +372,7 @@ class FirstTimeDialog(widgetset.DialogWindow):
                 search_entry.disable()
                 change_button.disable()
                 switch_mode("next")
+                self.gathered_media_files = None
 
             elif widget is yes_rb:
                 group_box.enable()
@@ -392,105 +404,124 @@ class FirstTimeDialog(widgetset.DialogWindow):
         handle_radio_button_clicked(restrict_rb)
         handle_radio_button_clicked(no_rb)
 
+        vbox = widgetutil.pad(vbox)
+
         return vbox
 
-    def build_search_page(self):
-        # NOTE: This is essentially duplicated in importmediadialog.
-        # There are minor differences, so it couldn't really be reused
-        # (which is annoying).
+    def stop_search_progress(self):
+        self.progress_bar.stop_pulsing()
+        self.progress_bar.set_progress(1.0)
+        self.cancel_search_button.disable()
+        self.cancelled = True
+        self.search_prev_button.enable()
+        self.search_next_button.enable()
 
+    def handle_search_cancel_clicked(self, widget):
+        self.stop_search_progress()
+        self.gathered_media_files = None
+        self.progress_label.set_text(_("Cancelled"))
+
+    def search_complete(self, text):
+        self.stop_search_progress()
+        self.progress_label.set_text(text)
+
+    def make_search_progress(self):
+        if self.cancelled:
+            self.finder = None
+            return
+
+        try:
+            num_parsed, found = self.finder.next()
+            self.gathered_media_files = found
+
+            num_found = len(found)
+            num_files = ngettext("parsed %(count)s file",
+                    "parsed %(count)s files",
+                    num_parsed,
+                    {"count": num_parsed})
+
+            num_media_files = ngettext("found %(count)s media file",
+                    "found %(count)s media files",
+                    num_found,
+                    {"count": num_found})
+            self.progress_label.set_text(u"%s - %s" % (
+                    num_files, num_media_files))
+
+            threads.call_on_ui_thread(self.make_search_progress)
+
+        except StopIteration:
+            num_found = len(self.gathered_media_files)
+            self.search_complete(
+                ngettext(
+                    "found %(count)s media file",
+                    "found %(count)s media files",
+                    num_found,
+                    {"count": num_found}))
+            self.finder = None
+
+    def start_search(self):
+        # only start a search if we haven't gathered anything, yet.
+        if self.gathered_media_files is not None:
+            return
+
+        # this starts the search as soon as the dialog is built
+        self.cancelled = False
+        self.cancel_search_button.enable()
+        self.search_prev_button.disable()
+        self.search_next_button.disable()
+
+        search_directory = FilenameType(self.search_directory)
+        self.finder = util.gather_media_files(search_directory)
+        self.progress_bar.start_pulsing()
+        threads.call_on_ui_thread(self.make_search_progress)
+
+    def build_search_page(self):
         vbox = widgetset.VBox(spacing=5)
 
         vbox.pack_start(_build_title(_("Searching for media files")))
 
-        progress_bar = widgetset.ProgressBar()
-        vbox.pack_start(progress_bar)
+        self.progress_bar = widgetset.ProgressBar()
+        self.progress_bar.set_size_request(400, -1)
+        vbox.pack_start(widgetutil.align_center(
+                self.progress_bar, top_pad=50))
 
-        progress_label = widgetset.Label("")
-        progress_label.set_size_request(WIDTH - 40, -1)
-        vbox.pack_start(widgetutil.align_left(progress_label))
+        self.progress_label = widgetset.Label("")
+        vbox.pack_start(
+            widgetutil.align_top(
+                widgetutil.align_center(self.progress_label),
+                top_pad=10))
 
-        search_button = widgetset.Button(_("Search"))
-        cancel_button = widgetset.Button(_("Cancel"))
+        self.cancel_search_button = widgetset.Button(_("Cancel Search"))
+        self.cancel_search_button.connect(
+            'clicked', self.handle_search_cancel_clicked)
 
-        hbox = widgetutil.build_hbox((search_button, cancel_button))
-        vbox.pack_start(widgetutil.align_left(hbox))
+        vbox.pack_start(widgetutil.align_right(self.cancel_search_button))
 
-        vbox.pack_start(self._force_space_label())
+        vbox.pack_start(self._force_space_label(), expand=True)
 
-        prev_button = widgetset.Button(_("< Previous"))
-        prev_button.connect('clicked', lambda x: self.prev_page())
+        self.search_prev_button = widgetset.Button(_("< Previous"))
+        self.search_prev_button.connect('clicked', lambda x: self.prev_page())
 
         if self._has_media_player:
-            next_button = widgetset.Button(_("Next >"))
-            next_button.connect('clicked', lambda x: self.next_page())
+            self.search_next_button = widgetset.Button(_("Next >"))
+            self.search_next_button.connect('clicked',
+                                            lambda x: self.next_page())
         else:
-            next_button = widgetset.Button(_("Finish"))
-            next_button.connect('clicked', lambda x: self.destroy())
+            self.search_next_button = widgetset.Button(_("Finish"))
+            self.search_next_button.connect('clicked',
+                                            lambda x: self.destroy())
 
         vbox.pack_start(
             widgetutil.align_bottom(widgetutil.align_right(
-                    widgetutil.build_hbox((prev_button, next_button)))),
+                    widgetutil.build_hbox((self.search_prev_button,
+                                           self.search_next_button)))),
             expand=True)
 
-        def handle_cancel_clicked(widget):
-            progress_bar.stop_pulsing()
-            progress_bar.set_progress(1.0)
-            search_button.enable()
-            cancel_button.disable()
+        vbox = widgetutil.pad(vbox)
+        vbox.run_me_on_switch = self.start_search
 
-            prev_button.enable()
-            next_button.enable()
-            self.cancelled = True
-
-        def make_progress():
-            if self.cancelled:
-                self.gathered_media_files = []
-                self.finder = None
-                progress_label.set_text("")
-                return
-
-            try:
-                num_parsed, found = self.finder.next()
-                self.gathered_media_files = found
-
-                num_found = len(found)
-                num_files = ngettext("parsed %(count)s file",
-                        "parsed %(count)s files",
-                        num_parsed,
-                        {"count": num_parsed})
-
-                num_media_files = ngettext("found %(count)s media file",
-                        "found %(count)s media files",
-                        num_found,
-                        {"count": num_found})
-                progress_label.set_text(u"%s - %s" % (num_files,
-                                                      num_media_files))
-
-                threads.call_on_ui_thread(make_progress)
-
-            except StopIteration:
-                handle_cancel_clicked(None)
-                self.finder = None
-
-        def handle_search_clicked(widget):
-            self.cancelled = False
-            search_button.disable()
-            cancel_button.enable()
-
-            prev_button.disable()
-            next_button.disable()
-
-            search_directory = FilenameType(self.search_directory)
-            self.finder = util.gather_media_files(search_directory)
-            progress_bar.start_pulsing()
-            threads.call_on_ui_thread(make_progress)
-
-        search_button.connect('clicked', handle_search_clicked)
-        cancel_button.connect('clicked', handle_cancel_clicked)
-
-        cancel_button.disable()
         return vbox
+
 
     def build_media_player_import_page(self):
         vbox = widgetset.VBox(spacing=5)
@@ -538,5 +569,7 @@ class FirstTimeDialog(widgetset.DialogWindow):
             widgetutil.align_bottom(widgetutil.align_right(
                     widgetutil.build_hbox((prev_button, finish_button)))),
             expand=True)
+
+        vbox = widgetutil.pad(vbox)
 
         return vbox
