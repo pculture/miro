7a4297b45a12286fbf39f47af08e2cbb10c3bf4b
Geoffrey Lee <glee@pculture.org>
2011-05-22 05:00:25 -0700

    Fix #17524.  Force conversion to str type before packing a string.
    Struct.pack only works on str type, so if get a unicode the message will
    fail to pack and in our case a HTTP reply will fail to eventuate from it.

--- tv/lib/libdaap/subr.py
+++ tv/lib/libdaap/subr.py
@@ -277,6 +277,9 @@ def encode_response(reply):
             if typ == DMAP_TYPE_STRING:
                 fmt = str(len(value)) + fmt
                 size = len(value)
+                # This ensures we always get a string type even if we are lame
+                # and passed a unicode in.
+                value = str(buffer(value))
             # code (4 bytes), length (4 bytes), data (variable), network byte
             # order
             fmt = '!4sI' + fmt
