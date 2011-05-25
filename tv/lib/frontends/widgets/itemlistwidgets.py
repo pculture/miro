pculture/miro

commit 1dc3fe33c35341adbe8719f8e1a7ba74c0e94021

#17559 Move get_search_text() to superclass, and filter out directory feeds from New Search Podcast.

tv/lib/frontends/widgets/itemlistwidgets.py
@@ -598,9 +598,6 @@ class ItemListTitlebar(Titlebar):
     def set_search_text(self, text):
         self.searchbox.set_text(text)
 
-    def get_search_text(self):
-        return self.searchbox.get_text()
-
     def start_editing_search(self, text):
         self.searchbox.start_editing(text)
 
@@ -724,6 +721,9 @@ class SearchTitlebar(ItemListTitlebar):
 
     def save_search_icon(self):
         return 'save-as-podcast'
+
+    def get_search_text(self):
+        return self.searchbox.get_text()
 
     def _on_save_search(self, button):
         self.emit('save-search', self.get_search_text())

tv/lib/frontends/widgets/newsearchfeed.py
@@ -76,7 +76,7 @@ class NewSearchFeedDialogRunner(object):
 
     def get_channel_info(self):
         self.channels = [ci for ci in app.tabs['feed'].get_feeds()
-                if not ci.is_folder]
+                if not (ci.is_folder or ci.is_directory_feed)]
         self.channels.sort(key=lambda x: util.name_sort_key(x.name))
 
     def get_search_engine_info(self):

commit 80b11905c77c69b1bb04d87b99ddc549ee871edf

Bug #17559.  1dc3fe33c3534 accidentally re-reverted the correct fix, it should be okay now.

tv/lib/frontends/widgets/itemlistwidgets.py
@@ -598,6 +598,9 @@ class ItemListTitlebar(Titlebar):
     def set_search_text(self, text):
         self.searchbox.set_text(text)
 
+    def get_search_text(self):
+        return self.searchbox.get_text()
+
     def start_editing_search(self, text):
         self.searchbox.start_editing(text)
 
@@ -721,9 +724,6 @@ class SearchTitlebar(ItemListTitlebar):
 
     def save_search_icon(self):
         return 'save-as-podcast'
-
-    def get_search_text(self):
-        return self.searchbox.get_text()
 
     def _on_save_search(self, button):
         self.emit('save-search', self.get_search_text())