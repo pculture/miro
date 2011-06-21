b78ae7de63c7e0677c1bd9897c7205f7a5511e01
Will Kahn-Greene <will.guaraldi@pculture.org>
2011-06-21 14:56:43 -0400

    Tweaks the find call so it's stable
    * Paul pointed out that the messages.pot file gets built in a different
      order on his system than mine because the find call isn't stable.
      This fixes that so the file list is sorted.

--- tv/resources/locale/export-strings.sh
+++ tv/resources/locale/export-strings.sh
@@ -30,7 +30,7 @@
 # statement from all source files in the program, then also delete it here.
 
 
-xgettext --package-name="Miro" --msgid-bugs-address="http://bugzilla.pculture.org/" --copyright-holder="Participatory Culture Foundation" -o messages.pot `find ../../ -name '*.py' -and -not -path '*testdata*' -and -not -path '*.git*' -and -not -path '*.svn*' -and -not -path '*build*' -and -not -path '*dist*' -and -not -path '*feedparser.py' -and -not -path '*amazon.py' -and -not -path '*lib/test/*'`
+xgettext --package-name="Miro" --msgid-bugs-address="http://bugzilla.pculture.org/" --copyright-holder="Participatory Culture Foundation" -o messages.pot `find ../../ -name '*.py' -and -not -path '*testdata*' -and -not -path '*.git*' -and -not -path '*.svn*' -and -not -path '*build*' -and -not -path '*dist*' -and -not -path '*feedparser.py' -and -not -path '*amazon.py' -and -not -path '*lib/test/*' | sort`
 
 # generates faux translation
 cp messages.pot swch.po
