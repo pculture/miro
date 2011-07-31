ce751eff5861365dc8eaa96157c48162cd8f7a0a
Geoffrey Lee <glee@pculture.org>
2011-07-31 23:12:45 +0900

    Conflicts:
    	tv/lib/libdaap/libdaap.py

--- tv/lib/libdaap/libdaap.py
+++ tv/lib/libdaap/libdaap.py
@@ -43,6 +43,11 @@ import BaseHTTPServer
 import SocketServer
 import threading
 import httplib
+import gzip
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 
 # Where do I get this guy in Python?
 # NB: equivalent to INT32_MAX.
@@ -229,8 +234,8 @@ class DaapHttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             pass
 
     def do_send_reply(self, rcode, reply, content_type=DEFAULT_CONTENT_TYPE,
-                      extra_headers=[]):
-        blob = encode_response(reply)
+                      content_encoding=None, extra_headers=[]):
+        blob = encode_response(reply, content_encoding=content_encoding)
         try:
             self.send_response(rcode)
             self.send_header('Content-type', content_type)
@@ -678,10 +683,50 @@ class DaapHttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             rcode = DAAP_BADREQUEST
             reply = []
             extra_headers = []
+        content_encoding = self.reply_encoding()
+        self.do_send_reply(rcode, reply, extra_headers=extra_headers,
+                           content_encoding=content_encoding)
         if endconn:
             self.wfile.close()
         else:
             self.do_send_reply(rcode, reply, extra_headers=extra_headers)
+
+    def reply_encoding(self):
+        supported = ['gzip']
+        acceptable = self.headers.getheader('Accept-encoding').split(',')
+        candidates = []
+        prohibited = []
+        for x in acceptable:
+            try:
+                encoding = x.split(',')
+                encoding, qvalue = x.split(',')
+                qvalue = float(qvalue)
+            except ValueError:
+                encoding = encoding[0]
+                qvalue = None
+            encoding = encoding.strip()
+            if qvalue == 0:
+                prohibited.append(encoding)
+            else:
+                candidates.append((encoding, qvalue))
+        def sort(x, y):
+           # NB: None > 0 == False, None < 0 == False
+           _, q1 = x
+           _, q2 = y
+           if x > y:
+               return 1
+           elif x < y:
+               return -1
+           else:
+               return 0
+        candidates.sort(cmp=sort, reverse=True)
+        for e, _ in candidates:
+            if e in supported:
+                return e
+        # XXX what to do if the 'identity' encoding or the wildcard was
+        # was administratively prohibited?  We can check for '*' in the
+        # prohibited list.
+        return None
 
 def mdns_init():
     return mdns.mdns_init()
@@ -784,11 +829,15 @@ def make_daap_server(backend, debug=False, name='pydaap', port=DEFAULT_PORT,
 # HTTP/1.1.
 class DaapClient(object):
     HEARTBEAT = 60    # seconds
-    def __init__(self, host, port):
+    def __init__(self, host, port, gzip=False):
         self.conn = None
         self.host = host
         self.port = port
+        self.gzip = gzip
         self.session = None
+        self.headers = dict()
+        if self.gzip:
+           self.headers['Accept-encoding'] = 'gzip, identity'
 
     def heartbeat_callback(self):
         try:
@@ -817,6 +866,9 @@ class DaapClient(object):
         # XXX Broken - don't do an unbounded read here, this is stupid,
         # server can crash the client
         data = response.read()
+        encoding = response.getheader('Content-encoding')
+        if encoding is not None and encoding.strip() == 'gzip':
+            data = gzip.GzipFile(fileobj=StringIO(data)).read()
         if callback:
             callback(data, *args)
 
@@ -877,11 +929,11 @@ class DaapClient(object):
     def connect(self):
         try:
             self.conn = httplib.HTTPConnection(self.host, self.port)
-            self.conn.request('GET', '/server-info')
+            self.conn.request('GET', '/server-info', headers=self.headers)
             self.check_reply(self.conn.getresponse())            
-            self.conn.request('GET', '/content-codes')
+            self.conn.request('GET', '/content-codes', headers=self.headers)
             self.check_reply(self.conn.getresponse())
-            self.conn.request('GET', '/login')
+            self.conn.request('GET', '/login', headers=self.headers)
             self.check_reply(self.conn.getresponse(),
                              callback=self.handle_login)
             # Finally, if this all works, start the heartbeat timer.
@@ -901,7 +953,8 @@ class DaapClient(object):
     # XXX Right now, there is only one db_id.
     def databases(self):
         try:
-            self.conn.request('GET', self.sessionize('/databases', []))
+            self.conn.request('GET', self.sessionize('/databases', []),
+                              headers=self.headers)
             self.check_reply(self.conn.getresponse(),
                              callback=self.handle_db)
             return self.db_id
@@ -917,7 +970,7 @@ class DaapClient(object):
         try:
             self.conn.request('GET', self.sessionize(
                               '/databases/%d/containers' % self.db_id,
-                              [('meta', meta)]))
+                              [('meta', meta)]), headers=self.headers)
             self.check_reply(self.conn.getresponse(),
                              callback=self.handle_playlist,
                              args=[meta])
@@ -940,12 +993,12 @@ class DaapClient(object):
             if playlist_id is None:
                 self.conn.request('GET', self.sessionize(
                     '/databases/%d/items' % self.db_id,
-                    [('meta', meta)]))
+                    [('meta', meta)]), headers=self.headers)
             else:
                 self.conn.request('GET', self.sessionize(
                     ('/databases/%d/containers/%d/items' % 
                      (self.db_id, playlist_id)),
-                    [('meta', meta)]))
+                    [('meta', meta)]), headers=self.headers)
             self.check_reply(self.conn.getresponse(),
                              callback=self.handle_items,
                              args=[playlist_id, meta])
@@ -995,8 +1048,8 @@ class DaapClient(object):
         fn += '?session-id=%s' % self.session
         return fn
 
-def make_daap_client(host, port=DEFAULT_PORT):
-    return DaapClient(host, port)
+def make_daap_client(host, port=DEFAULT_PORT, gzip=True):
+    return DaapClient(host, port, gzip=gzip)
 
 def register_meta(meta, code, typ):
     dmap_consts[code] = (meta, typ)
