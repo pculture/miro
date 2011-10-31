5614dc1f428ea9486e7b328f3c7a0b71f69f04e1
Christopher Schuster <cs@livoris.de>
2011-10-31 16:57:33 +0100

    minor adjustments for fedoara 15 requirements

---
+++ tv/linux/helperscripts/fedora_15.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script installs dependencies for building and running Miro on
+# Fedora 15.
+#
+# You run this sript AT YOUR OWN RISK.  Read through the whole thing
+# before running it!
+#
+# This script must be run as root.
+
+# Last updated:    10/31/2011
+# Last updated by: Christopher Schuster
+
+yum groupinstall "Development Tools" "Development Libraries"
+
+yum install \
+   ffmpeg-devel \
+   avahi-compat-libdns_sd \
+   Pyrex \
+   python-devel \
+   pygtk2-devel \
+   gtk2-devel \
+   pygtk2 \
+   pygobject2 \
+   gtk2 \
+   gnome-python2-gconf \
+   dbus-python \
+   rb_libtorrent \
+   rb_libtorrent-python \
+   webkitgtk-devel \
+   pywebkitgtk \
+   gstreamer-python \
+   python-mutagen \
+   ffmpeg \
+   ffmpeg2theora
