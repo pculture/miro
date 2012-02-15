fdf9fbe400b1bdd5c81bc5470adeaa8694b01669
Ben Dean-Kawamura <ben@pculture.org>
2012-02-15 16:25:55 -0500

    Updated clean.sh script on linux.
    Added a bunch of pyrex files that should be cleaned out

--- tv/linux/clean.sh
+++ tv/linux/clean.sh
@@ -2,6 +2,14 @@
 
 rm -rf build dist
 rm -rf miro.1.gz miro.real.1.gz
-rm plat/xlibhelper.c 
-rm ../lib/frontends/widgets/gtk/pygtkhacks.c
+rm -f plat/xlibhelper.c
+rm -f ../lib/frontends/widgets/gtk/pygtkhacks.c
+rm -f ../lib/frontends/widgets/gtk/webkitgtkhacks.c
+rm -f ../lib/frontends/widgets/infolist/infolist.c
 rm -rf tmp
+rm -f plat/frontends/widgets/windowcreator.cpp
+rm -f plat/frontends/widgets/pluginsdir.cpp
+rm -f plat/frontends/widgets/mozprompt.c
+rm -f plat/frontends/widgets/mozprompt.h
+rm -f plat/frontends/widgets/httpobserver.c
+
