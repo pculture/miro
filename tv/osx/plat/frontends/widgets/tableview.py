pculture/miro

commit b83ac03a5b2e89d112436b805f2aba686fb348a1

Fixed alternating row drawing on OSX
Now we draw alternating rows for the normal cells, but not for the album cell.
Added ability for any renderer to stop cocoa from drawing its background.

Didn't change anything for GTK.  GTK doesn't draw the alternating background
below the last cell anyways.  We could use DRAWS_BACKGROUND to avoid painting
the background below cells with content, but that's quite tricky.

tv/osx/plat/frontends/widgets/tableview.py
@@ -63,6 +63,10 @@ EXPANDER_PADDING = 6
 HEADER_HEIGHT = 17
 CUSTOM_HEADER_HEIGHT = 25
 
+def iter_range(ns_range):
+    """Iterate over an NSRange object"""
+    return xrange(ns_range.location, ns_range.location + ns_range.length)
+
 Rect = namedtuple('Rect', 'x y width height') 
 def NSRectToRect(nsrect):
     origin, size = nsrect.origin, nsrect.size
@@ -271,6 +275,8 @@ class MiroCheckboxCell(NSButtonCell):
                 _calc_interior_frame(frame, view), view)
 
 class CellRendererBase(object):
+    DRAW_BACKGROUND = True
+
     def set_index(self, index):
         self.index = index
     
@@ -645,6 +651,56 @@ class TableViewCommon(object):
         gradient.set_end_color(end_color)
         gradient.draw()
         context.restoreGraphicsState()
+
+    def drawBackgroundInClipRect_(self, clip_rect):
+        # save our graphics state, since we are about to modify the clip path
+        graphics_context = NSGraphicsContext.currentContext()
+        graphics_context.saveGraphicsState()
+        # create a NSBezierPath that contains the rects of the columns with
+        # DRAW_BACKGROUND True.
+        clip_path = NSBezierPath.bezierPath()
+        for col_index in iter_range(self.columnsInRect_(clip_rect)):
+            column = wrappermap.wrapper(self.tableColumns()[col_index])
+            column_rect = self.rectOfColumn_(col_index)
+            if not column.renderer.DRAW_BACKGROUND:
+                self.drawBackgroundOutsideContent_clipRect_(column_rect,
+                        clip_rect)
+                continue
+            clip_path.appendBezierPathWithRect_(column_rect)
+        # clip to that path
+        clip_path.addClip()
+        # do the default drawing
+        self.SuperClass.drawBackgroundInClipRect_(self, clip_rect)
+        # restore graphics state
+        graphics_context.restoreGraphicsState()
+
+    def drawBackgroundOutsideContent_clipRect_(self, column_rect, clip_rect):
+        """Draw our background outside the rows with content
+
+        We call this for cells with DRAW_BACKGROUND set to False.  For those,
+        we let the cell draw their own background, but we still need to draw
+        the background before the first cell and after the last cell.
+        """
+
+        self.backgroundColor().set()
+
+        # calculate x and width for both the top and bottom rect from the
+        # overlap between the column rect and the clip rect
+        intersection = NSIntersectionRect(clip_rect, column_rect)
+        x = intersection.origin.x
+        width = intersection.size.width
+
+        # draw part of clip_rect above column_rect
+        height_above_top = column_rect.origin.y - clip_rect.origin.y
+        if height_above_top > 0:
+            y = clip_rect.origin.y
+            NSRectFill(NSMakeRect(x, y, width, height_above_top))
+
+        # draw part of clip_rect below column_rect
+        height_below_bottom = NSMaxY(clip_rect) - NSMaxY(column_rect)
+        if NSMaxY(clip_rect) > NSMaxY(column_rect):
+            y = NSMaxY(column_rect) + 1
+            NSRectFill(NSMakeRect(x, y, width, height_below_bottom))
 
     def canDragRowsWithIndexes_atPoint_(self, indexes, point):
         return YES

tv/lib/frontends/widgets/style.py
@@ -737,6 +737,8 @@ class MultiRowAlbumRenderer(widgetset.InfoListRenderer):
     """Renderer for album view."""
 
     IGNORE_PADDING = True
+    DRAW_BACKGROUND = False
+
     IMAGE_SIZE = (138, 138)
     IMAGE_MARGIN_TOP = 5
     IMAGE_MARGIN_BOTTOM = 0

commit dd7a890b89e512aee8121851643c1c7c8122add8

include clipping on the right side (OS X)
b83ac03a introduced a bug where the clipping rectangle no longer included the
space on the right side if the columns didn't cover that space.  This happened
for me during a column resize.  The change is to include the space to the right
if it isn't currently part of the right-most column.

tv/osx/plat/frontends/widgets/tableview.py
@@ -675,8 +675,10 @@ class TableViewCommon(object):
         # create a NSBezierPath that contains the rects of the columns with
         # DRAW_BACKGROUND True.
         clip_path = NSBezierPath.bezierPath()
+        number_of_columns = len(self.tableColumns())
         for col_index in iter_range(self.columnsInRect_(clip_rect)):
             column = wrappermap.wrapper(self.tableColumns()[col_index])
+            column_rect = None
             if column.renderer.DRAW_BACKGROUND:
                 # We should draw the background for this column, add it's rect
                 # to our clip rect.
@@ -688,6 +690,17 @@ class TableViewCommon(object):
                 # first row and after the last row.
                 self.drawBackgroundOutsideContent_clipRect_(col_index,
                         clip_rect)
+            if col_index == number_of_columns - 1: # last column
+                if not column_rect:
+                    column_rect = self.rectOfColumn_(col_index)
+                column_right = column_rect.origin.x + column_rect.size.width
+                clip_right = clip_rect.origin.x + clip_rect.size.width
+                if column_right < clip_right:
+                    # there's space to the right, so add that to the clip_rect
+                    remaining = clip_right - column_right
+                    left_rect = NSMakeRect(column_right, clip_rect.origin.y,
+                                           remaining, clip_rect.size.height)
+                    clip_path.appendBezierPathWithRect_(left_rect)
         # clip to that path
         clip_path.addClip()
         # do the default drawing