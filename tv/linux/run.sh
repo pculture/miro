e8c83151b32cc69e2d6d50d42d264b6e50936b52
Paul Swartz <pswartz@pculture.org>
2012-05-11 13:16:46 -0400

    exit 1 if we can't build Miro

--- tv/linux/run.sh
+++ tv/linux/run.sh
@@ -57,4 +57,5 @@ then
     PATH=dist/$PREFIX/bin:$PATH PYTHONPATH=$MIROPYTHONPATH dist/$PREFIX/bin/miro "$@"
 else
     echo "Compile failed.  Review the console text and make necessary changes."
+    exit 1
 fi
