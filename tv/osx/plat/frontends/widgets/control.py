3353969662680573e1b1165af1fa595ee0cb4596
Ben Dean-Kawamura <ben@pculture.org>
2012-08-15 11:40:45 -0400

    bz19450: don't create the focus-out signal twice on OSX
    It's already created in the base Widget class, so we don't need to also create
    it in MultiLineTextEntry.

--- tv/osx/plat/frontends/widgets/control.py
+++ tv/osx/plat/frontends/widgets/control.py
@@ -210,7 +210,6 @@ class MultilineTextEntry(Widget):
         self.view.setVerticallyResizable_(YES)
         self.notifications = NotificationForwarder.create(self.view)
         self.create_signal('changed')
-        self.create_signal('focus-out')
         if initial_text is not None:
             self.set_text(initial_text)
         self.set_size(widgetconst.SIZE_NORMAL)
